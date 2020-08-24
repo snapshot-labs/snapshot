52b650322b8f0ce0b5381176a541d2dacf3e060e
brockelmore <31553173+brockelmore@users.noreply.github.com>
2020-08-24 17:33:44 -0400

    Add Community and Core Devs Tabs (#1)
    * add coreDevs param
    
    * Add community and core dev tab
    
    * Update src/views/Proposals.vue
    
    Co-authored-by: Fabien <bonustrack@users.noreply.github.com>
    
    * Update matching
    
    Co-authored-by: Fabien <bonustrack@users.noreply.github.com>

--- src/views/Proposals.vue
+++ src/views/Proposals.vue
@@ -29,7 +29,7 @@
       <Block :slim="true">
         <div class="px-4 py-3 bg-gray-dark">
           <a
-            v-for="state in ['All', 'Active', 'Pending', 'Closed']"
+            v-for="state in ['All', 'Core devs', 'Community', 'Active', 'Pending', 'Closed']"
             :key="state"
             v-text="state"
             @click="selectedState = state"
@@ -97,6 +97,18 @@ export default {
               this.selectedState === 'Active' &&
               proposal[1].msg.payload.start <= ts &&
               proposal[1].msg.payload.end > ts
+            ) {
+              return true;
+            }
+            if (
+              this.selectedState === 'Core devs' &&
+              proposal[1].address.includes(this.namespace.coreDevs)
+            ) {
+              return true;
+            }
+            if (
+              this.selectedState === 'Community' &&
+              !proposal[1].address.includes(this.namespace.coreDevs)
             ) {
               return true;
             }
