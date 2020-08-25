51803d24cd8e979cae9c7501d7e9cfecccf93733
bonustrack <fabien@bonustrack.co>
2020-08-25 18:34:31 +0700

    Update lock

--- src/helpers/config.ts
+++ src/helpers/config.ts
@@ -2,7 +2,7 @@ import config from '@/config.json';
 
 let id = 'master';
 const domainName = window.location.hostname;
-if (domainName.includes('localhost:')) id = 'local';
-if (domainName === 'beta.vote.balancer.finance') id = 'develop';
+if (domainName.includes('localhost')) id = 'local';
+if (domainName === 'beta.snapshot.page') id = 'develop';
 
 export default config[id];
--- src/helpers/lock.ts
+++ src/helpers/lock.ts
@@ -2,9 +2,11 @@ import { Lock } from '@bonustrack/lock/dist/lock.cjs';
 import config from '@/helpers/config';
 import injected from '@bonustrack/lock/connectors/injected';
 import portis from '@bonustrack/lock/connectors/portis';
-import magic from '@bonustrack/lock/connectors/fortmatic';
+import fortmatic from '@bonustrack/lock/connectors/fortmatic';
+import walletlink from '@bonustrack/lock/connectors/walletlink';
+import walletconnect from '@bonustrack/lock/connectors/walletconnect';
 
-const connectors = { injected, portis, magic };
+const connectors = { injected, portis, fortmatic, walletconnect, walletlink };
 const lock = new Lock();
 Object.entries(config.connectors).forEach((connector: any) => {
   lock.addConnector({
