03facff53ffdb6256244a3f40e8c4f61fdca2c4e
zashton <ashton@havence.com.sg>
2020-08-27 02:05:47 +0800

    lint fix

--- src/views/Create.vue
+++ src/views/Create.vue
@@ -2,8 +2,8 @@
   <Container :slim="true">
     <div class="px-4 px-md-0 mb-3">
       <router-link
-          :to="{ name: 'proposals', params: { key } }"
-          class="text-gray"
+        :to="{ name: 'proposals', params: { key } }"
+        class="text-gray"
       >
         <Icon name="back" size="22" class="v-align-middle" />
         {{ namespace.name || _shorten(namespace.address) }}
@@ -14,17 +14,17 @@
         <div class="px-4 px-md-0">
           <div class="d-flex flex-column mb-6">
             <input
-                v-autofocus
-                v-model="form.name"
-                maxlength="128"
-                class="h1 mb-2 input"
-                placeholder="Question"
+              v-autofocus
+              v-model="form.name"
+              maxlength="128"
+              class="h1 mb-2 input"
+              placeholder="Question"
             />
             <textarea-autosize
-                v-model="form.body"
-                maxlength="10240"
-                class="input mb-6"
-                placeholder="What is your proposal?"
+              v-model="form.body"
+              maxlength="10240"
+              class="input mb-6"
+              placeholder="What is your proposal?"
             />
             <div v-if="form.body">
               <h4 class="mb-4">Preview</h4>
@@ -37,24 +37,23 @@
             <draggable v-model="choices">
               <transition-group name="list">
                 <div
-                    v-for="(choice, i) in choices"
-                    :key="choice.key"
-                    class="d-flex mb-2"
+                  v-for="(choice, i) in choices"
+                  :key="choice.key"
+                  class="d-flex mb-2"
                 >
                   <UiButton class="d-flex width-full">
                     <span class="mr-4">{{ i + 1 }}</span>
                     <input
-                        v-model="choices[i].text"
-                        class="input height-full flex-auto text-center"
+                      v-model="choices[i].text"
+                      class="input height-full flex-auto text-center"
                     />
                     <span @click="removeChoice(i)" class="ml-4">
-                  <Icon name="close" size="12" />
-                </span>
+                      <Icon name="close" size="12" />
+                    </span>
                   </UiButton>
                 </div>
               </transition-group>
             </draggable>
-
           </div>
           <UiButton @click="addChoice(1)" class="d-block width-full">
             Add choice
@@ -65,33 +64,33 @@
         <Block title="Actions">
           <div class="mb-2">
             <UiButton
-                @click="[(modalOpen = true), (selectedDate = 'start')]"
-                class="width-full mb-2"
+              @click="[(modalOpen = true), (selectedDate = 'start')]"
+              class="width-full mb-2"
             >
               <span v-if="!form.start">Select start date</span>
               <span v-else v-text="$d(form.start * 1e3, 'long')" />
             </UiButton>
             <UiButton
-                @click="[(modalOpen = true), (selectedDate = 'end')]"
-                class="width-full mb-2"
+              @click="[(modalOpen = true), (selectedDate = 'end')]"
+              class="width-full mb-2"
             >
               <span v-if="!form.end">Select end date</span>
               <span v-else v-text="$d(form.end * 1e3, 'long')" />
             </UiButton>
             <UiButton class="width-full mb-2">
               <input
-                  v-model="form.snapshot"
-                  type="number"
-                  class="input width-full text-center"
-                  placeholder="Snapshot block number"
+                v-model="form.snapshot"
+                type="number"
+                class="input width-full text-center"
+                placeholder="Snapshot block number"
               />
             </UiButton>
           </div>
           <UiButton
-              @click="handleSubmit"
-              :disabled="!isValid"
-              :loading="loading"
-              class="d-block width-full button--submit"
+            @click="handleSubmit"
+            :disabled="!isValid"
+            :loading="loading"
+            class="d-block width-full button--submit"
           >
             Publish
           </UiButton>
@@ -99,11 +98,11 @@
       </div>
     </div>
     <ModalSelectDate
-        :value="form[selectedDate]"
-        :selectedDate="selectedDate"
-        :open="modalOpen"
-        @close="modalOpen = false"
-        @input="setDate"
+      :value="form[selectedDate]"
+      :selectedDate="selectedDate"
+      :open="modalOpen"
+      @close="modalOpen = false"
+      @input="setDate"
     />
   </Container>
 </template>
@@ -115,13 +114,13 @@ import draggable from 'vuedraggable';
 
 export default {
   components: {
-    draggable,
+    draggable
   },
   data() {
     return {
       key: this.$route.params.key,
       loading: false,
-      choices:[],
+      choices: [],
       form: {
         name: '',
         body: '',
@@ -132,46 +131,45 @@ export default {
       },
       modalOpen: false,
       selectedDate: '',
-      counter:0
+      counter: 0
     };
   },
   computed: {
     namespace() {
       return namespaces[this.key]
-          ? namespaces[this.key]
-          : { token: this.key, verified: [] };
+        ? namespaces[this.key]
+        : { token: this.key, verified: [] };
     },
     isValid() {
       // const ts = (Date.now() / 1e3).toFixed();
 
       return (
-          !this.loading &&
-          this.web3.account &&
-          this.form.name &&
-          this.form.body &&
-          this.form.start &&
-          // this.form.start >= ts &&
-          this.form.end &&
-          this.form.end > this.form.start &&
-          this.form.choices.length >= 2 &&
-          this.form.choices.reduce((a, b) => (!a ? false : b), true)
+        !this.loading &&
+        this.web3.account &&
+        this.form.name &&
+        this.form.body &&
+        this.form.start &&
+        // this.form.start >= ts &&
+        this.form.end &&
+        this.form.end > this.form.start &&
+        this.form.choices.length >= 2 &&
+        this.form.choices.reduce((a, b) => (!a ? false : b), true)
       );
     }
   },
-  mounted(){
+  mounted() {
     this.addChoice(2);
   },
   methods: {
     ...mapActions(['send']),
     addChoice(num) {
-      for(let i=1; i<=num; i++)
-      {
+      for (let i = 1; i <= num; i++) {
         this.counter++;
-        this.choices.push({key:this.counter,text:''});
+        this.choices.push({ key: this.counter, text: '' });
       }
     },
     removeChoice(i) {
-      this.choices.splice(i,1);
+      this.choices.splice(i, 1);
     },
     setDate(ts) {
       if (this.selectedDate) {
@@ -180,7 +178,7 @@ export default {
     },
     async handleSubmit() {
       this.loading = true;
-      this.form.choices = this.choices.map( function (choice) {
+      this.form.choices = this.choices.map(function(choice) {
         return choice.text;
       });
 
@@ -207,13 +205,15 @@ export default {
 </script>
 
 <style>
-.list-leave-active,.list-enter-active {
+.list-leave-active,
+.list-enter-active {
   transition: all 0.3s;
 }
 .list-move {
   transition: transform 0.3s;
 }
-.list-enter, .list-leave-to {
+.list-enter,
+.list-leave-to {
   opacity: 0;
 }
 </style>
