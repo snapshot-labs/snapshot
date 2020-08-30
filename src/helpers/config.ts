6e51dbb686600c712161007ab292a5c185852ada
bonustrack <fabien@bonustrack.co>
2020-08-30 16:23:40 +0700

    Add Synthetix demo

--- src/helpers/config.ts
+++ src/helpers/config.ts
@@ -3,7 +3,8 @@ import config from '@/config.json';
 config.env = 'master';
 const domainName = window.location.hostname;
 if (domainName.includes('localhost')) config.env = 'local';
-if (domainName === 'beta.snapshot.page') config.env = 'develop';
+if (domainName === 'demo.snapshot.page' || domainName === 'beta.snapshot.page')
+  config.env = 'develop';
 if (domainName === 'snapshot.page') {
   delete config.connectors.walletconnect;
   delete config.connectors.walletlink;
