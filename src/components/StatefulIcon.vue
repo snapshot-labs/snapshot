0b084337d9f05dc222be5c8ca03d4ae7c405ac34
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2020-09-06 21:45:12 +0200

    feat: add ability to favourite spaces (#69)
    * feat: add favoriteSpaces module
    
    * feat: add Add/Remove buttons
    
    * feat: sort spaces by favourite
    
    * feat: only show Add button on hover
    
    * feat: use star icons for favourites
    
    * refactor: simplify favourites state access
    
    * feat: add colors to the buttons

---
+++ src/components/StatefulIcon.vue
@@ -0,0 +1,47 @@
+<template>
+  <button
+    :class="{
+      'extra-icon': true,
+      'extra-icon-off': !this.on,
+      'text-primary': this.on,
+      'text-gray': !this.on
+    }"
+    @click="handleClick($event)"
+  >
+    <Icon :name="name" size="20" />
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    onName: { type: String },
+    offName: { type: String },
+    on: { type: Boolean, default: true }
+  },
+  computed: {
+    name() {
+      return this.on ? 'star' : 'star1';
+    }
+  },
+  methods: {
+    handleClick(e) {
+      e.preventDefault();
+
+      this.$emit('click');
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.extra-icon {
+  position: absolute;
+  top: 12px;
+  right: 12px;
+  padding: 8px;
+  line-height: 16px;
+  border: none;
+  background-color: transparent;
+}
+</style>
