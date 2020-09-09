12792d05352b0f73aa1c5a300bbb7d3ed3d8b205
Fabien <bonustrack@users.noreply.github.com>
2020-09-10 04:09:44 +0700

    Move Snapshot.js to another repo (#89)

--- src/store/modules/web3.ts
+++ src/store/modules/web3.ts
@@ -3,7 +3,7 @@ import { getInstance } from '@bonustrack/lock/plugins/vue';
 import { Web3Provider } from '@ethersproject/providers';
 import { Contract } from '@ethersproject/contracts';
 import { getAddress } from '@ethersproject/address';
-import { multicall } from '@/_snapshot/utils';
+import { multicall } from '@bonustrack/snapshot.js/src/utils';
 import spaces from '@/spaces';
 import store from '@/store';
 import abi from '@/helpers/abi';
