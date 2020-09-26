e4094da9730307d1f94ecd40b377ea1edf87e65a
zashton <17747090+zashton@users.noreply.github.com>
2020-09-26 22:09:29 +0800

    run lints

--- src/views/Proposals.vue
+++ src/views/Proposals.vue
@@ -32,23 +32,7 @@
             />
           </div>
           <transition name="bounce">
-            <div
-                v-if="showMobileFilter"
-                class="col-12 hide-lg hide-xl pt-2 "
-            >
-              <input
-                  class="form-control height-full input-block ml-lg-2 "
-                  v-model="search"
-                  type="text"
-                  placeholder="Search"
-                  aria-label="Search"
-              />
-            </div>
-          </transition>
-
-          <div
-              class="col-5 float-right hide-sm hide-md "
-            >
+            <div v-if="showMobileFilter" class="col-12 hide-lg hide-xl pt-2 ">
               <input
                 class="form-control height-full input-block ml-lg-2 "
                 v-model="search"
@@ -57,6 +41,17 @@
                 aria-label="Search"
               />
             </div>
+          </transition>
+
+          <div class="col-5 float-right hide-sm hide-md ">
+            <input
+              class="form-control height-full input-block ml-lg-2 "
+              v-model="search"
+              type="text"
+              placeholder="Search"
+              aria-label="Search"
+            />
+          </div>
         </div>
 
         <RowLoading v-if="loading" />
@@ -84,18 +79,30 @@
       class="floater-btn d-md-none"
       @click="showMobileFilter = !showMobileFilter"
     >
-        <svg v-if="!showMobileFilter" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 14" width="24" height="24"><path fill-rule="evenodd" d="M14.53 15.59a8.25 8.25 0 111.06-1.06l5.69 5.69a.75.75 0 11-1.06 1.06l-5.69-5.69zM2.5 9.25a6.75 6.75 0 1111.74 4.547.746.746 0 00-.443.442A6.75 6.75 0 012.5 9.25z"></path></svg>        <svg
-          v-else
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 14"
-          width="24"
-          height="24"
-        >
-          <path
-            fill-rule="evenodd"
-            d="M5.72 5.72a.75.75 0 011.06 0L12 10.94l5.22-5.22a.75.75 0 111.06 1.06L13.06 12l5.22 5.22a.75.75 0 11-1.06 1.06L12 13.06l-5.22 5.22a.75.75 0 01-1.06-1.06L10.94 12 5.72 6.78a.75.75 0 010-1.06z"
-          ></path>
-        </svg>
+      <svg
+        v-if="!showMobileFilter"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 14"
+        width="24"
+        height="24"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M14.53 15.59a8.25 8.25 0 111.06-1.06l5.69 5.69a.75.75 0 11-1.06 1.06l-5.69-5.69zM2.5 9.25a6.75 6.75 0 1111.74 4.547.746.746 0 00-.443.442A6.75 6.75 0 012.5 9.25z"
+        ></path>
+      </svg>
+      <svg
+        v-else
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 14"
+        width="24"
+        height="24"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M5.72 5.72a.75.75 0 011.06 0L12 10.94l5.22-5.22a.75.75 0 111.06 1.06L13.06 12l5.22 5.22a.75.75 0 11-1.06 1.06L12 13.06l-5.22 5.22a.75.75 0 01-1.06-1.06L10.94 12 5.72 6.78a.75.75 0 010-1.06z"
+        ></path>
+      </svg>
     </button>
   </div>
 </template>
