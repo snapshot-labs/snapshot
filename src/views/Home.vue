a2655b30c309547860414609eef5d38683a399ae
bonustrack <fabien@bonustrack.co>
2020-10-13 18:32:08 +0700

    Remove key on custom domain

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -63,14 +63,12 @@
 import { mapActions } from 'vuex';
 import orderBy from 'lodash/orderBy';
 import spotlight from '@bonustrack/snapshot-spaces/spaces/spotlight.json';
-import domains from '@bonustrack/snapshot-spaces/spaces/domains.json';
 
 export default {
   data() {
     return {
       q: '',
-      limit: 16,
-      domains
+      limit: 16
     };
   },
   computed: {
@@ -109,14 +107,6 @@ export default {
     }
   },
   created() {
-    const domainName = window.location.hostname;
-    if (domains[domainName])
-      return this.$router.push({
-        name: 'proposals',
-        params: {
-          key: domains[domainName]
-        }
-      });
     this.loadFavoriteSpaces();
   }
 };
