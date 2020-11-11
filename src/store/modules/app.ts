e1bb0b07a128766171b615892245df12d0e2fb7d
bonustrack <fabien@bonustrack.co>
2020-11-11 19:35:46 +0700

    Fix for minScore

--- src/store/modules/app.ts
+++ src/store/modules/app.ts
@@ -126,10 +126,11 @@ const actions = {
           getProvider(space.network),
           Object.values(proposals).map((proposal: any) => proposal.address)
         );
+        console.log('Scores', scores);
         proposals = Object.fromEntries(
           Object.entries(proposals).map((proposal: any) => {
             proposal[1].score = scores.reduce(
-              (a, b) => a + b[proposal[1].address] || 0,
+              (a, b) => a + (b[proposal[1].address] || 0),
               0
             );
             return [proposal[0], proposal[1]];
