fb89e4e8220a4188e1e1c83a4682d014d534f833
Fabien <bonustrack@users.noreply.github.com>
2020-12-06 20:16:48 +0700

    Update src/components/Topnav.vue

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -47,7 +47,7 @@
                   class="mr-0 mr-sm-2 mr-md-2 mr-lg-2 mr-xl-2 ml-n1"
                 />
                 <span
-                  v-if="web3.profile.name"
+                  v-if="web3.profile && web3.profile.name"
                   v-text="web3.profile.name"
                   class="hide-sm"
                 />
