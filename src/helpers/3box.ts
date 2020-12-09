60e201c3720723ec39f92ba3e94d0bf98eca231e
Chaitanya S <yourchaitu@gmail.com>
2020-12-09 16:34:27 +0530

    Lint

--- src/helpers/3box.ts
+++ src/helpers/3box.ts
@@ -10,48 +10,63 @@ function get3BoxProfiles(addresses) {
       eth_address: true,
       image: true
     }
-  })
+  });
 }
 
 function lookupAddresses(addresses) {
-  return subgraphRequest('https://api.thegraph.com/subgraphs/name/ensdomains/ens', {
-    accounts: {
-      __args: {
-        first: 1000,
-        where: {
-          id_in: addresses.map(addresses => addresses.toLowerCase())
-        }
-      },
-      id: true,
-      domains: {
+  return subgraphRequest(
+    'https://api.thegraph.com/subgraphs/name/ensdomains/ens',
+    {
+      accounts: {
         __args: {
-          first: 1
+          first: 1000,
+          where: {
+            id_in: addresses.map(addresses => addresses.toLowerCase())
+          }
         },
-        name: true
+        id: true,
+        domains: {
+          __args: {
+            first: 1
+          },
+          name: true
+        }
       }
     }
-  })
+  );
 }
 
 export async function getProfiles(addresses) {
-  let ensNames: any = {accounts: []}
-  let _3BoxProfiles: any = {profiles: []}
+  let ensNames: any = { accounts: [] };
+  let _3BoxProfiles: any = { profiles: [] };
   try {
     [ensNames, _3BoxProfiles] = await Promise.all([
       lookupAddresses(addresses),
       get3BoxProfiles(addresses)
-    ])
+    ]);
   } catch (e) {
     console.log(e);
   }
 
-  console.log(ensNames, _3BoxProfiles)
+  console.log(ensNames, _3BoxProfiles);
   const profiles = Object.fromEntries(addresses.map(address => [address, {}]));
-  return Object.fromEntries(Object.entries(profiles).map(([address, profile]) => {
-    profile = _3BoxProfiles.profiles.find(profile => profile.eth_address === address.toLowerCase()) || {};
-    const ensAccount = ensNames.accounts.find(profile => profile.id === address.toLowerCase());
-    profile.ens = (ensAccount && ensAccount.domains && ensAccount.domains.length > 0) && ensAccount.domains[0].name || "";
-    return [address, profile];
-  }));
-  return profiles
+  return Object.fromEntries(
+    Object.entries(profiles).map(([address, profile]) => {
+      profile =
+        _3BoxProfiles.profiles.find(
+          profile => profile.eth_address === address.toLowerCase()
+        ) || {};
+      const ensAccount = ensNames.accounts.find(
+        profile => profile.id === address.toLowerCase()
+      );
+      profile.ens =
+        (ensAccount &&
+          ensAccount.domains &&
+          ensAccount.domains.length > 0 &&
+          ensAccount.domains[0].name) ||
+        '';
+      return [address, profile];
+    })
+  );
+  return profiles;
 }
