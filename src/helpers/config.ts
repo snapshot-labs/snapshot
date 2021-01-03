885501931a3f537826f00e8f449e0e4387dff0de
bonustrack <fabien@bonustrack.co>
2021-01-03 17:25:32 +0700

    Update WalletConnect

--- src/helpers/config.ts
+++ src/helpers/config.ts
@@ -9,7 +9,7 @@ const domainName = window.location.hostname;
 if (domainName.includes('localhost')) config.env = 'local';
 if (domainName === 'demo.snapshot.page' || domainName === 'beta.snapshot.page')
   config.env = 'develop';
-if (domainName === 'snapshot.page') delete config.connectors.torus;
+// if (domainName === 'snapshot.page') delete config.connectors.torus;
 delete config.connectors.portis;
 
 export default config;
