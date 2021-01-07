a941339d9ac771c3cc49313d7867e5455a2ee13a
Sam <samuv.eth@gmail.com>
2021-01-07 18:14:21 +0700

    fix: Typo and refactor

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,48 +1,50 @@
 import { createI18n } from 'vue-i18n';
 
-const locale = 'en-US';
-
-const i18n = createI18n({
-  locale,
-  messages: {
-    en: {
-      messages: {
-        EMPTY_STATE: 'No results found'
-      }
-    }
-  },
-  numberFormats: {
-    en: {
-      currency: {
-        style: 'currency',
-        currency: 'USD',
-        minimumFractionDigits: 0,
-        maximumFractionDigits: 0
-      },
-      price: {
-        style: 'currency',
-        currency: 'USD',
-        minimumFractionDigits: 2,
-        maximumFractionDigits: 6
-      },
-      percent: {
-        style: 'percent',
-        minimumFractionDigits: 0,
-        maximumFractionDigits: 2
-      }
-    }
-  },
-  dateTimeFormats: {
-    'en-US': {
-      short: {
-        year: 'numeric',
-        month: 'short',
-        day: 'numeric',
-        hour: 'numeric',
-        minute: 'numeric'
-      }
+const messages = {
+  en: {
+    message: {
+      EMPTY_STATE: 'No results found'
     }
   }
+};
+const numberFormats = {
+  en: {
+    currency: {
+      style: 'currency',
+      currency: 'USD',
+      minimumFractionDigits: 0,
+      maximumFractionDigits: 0
+    },
+    price: {
+      style: 'currency',
+      currency: 'USD',
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 6
+    },
+    percent: {
+      style: 'percent',
+      minimumFractionDigits: 0,
+      maximumFractionDigits: 2
+    }
+  }
+};
+const datetimeFormats = {
+  en: {
+    short: {
+      year: 'numeric',
+      month: 'short',
+      day: 'numeric',
+      hour: 'numeric',
+      minute: 'numeric'
+    }
+  }
+};
+
+const i18n = createI18n({
+  locale: 'en',
+  datetimeFormats,
+  numberFormats,
+  messages
 });
 
 export default i18n;
