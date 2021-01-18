503f4924e094d1d6bb00ffac768654ab06d674b5
Chaitanya <yourchaitu@gmail.com>
2021-01-18 17:08:44 +0530

    Strategy Page (#190)
    * Strategy Page
    
    * Lint
    
    * Fixes for PR comments
    
    * Fix to strategy redirect
    
    * Links fix
    
    * Fixes

--- src/components/Block/Strategy.vue
+++ src/components/Block/Strategy.vue
@@ -2,29 +2,15 @@
   <Block>
     <div class="d-flex flex-items-baseline">
       <h3>
-        <a
-          v-text="strategy.key"
-          :href="
-            `https://github.com/snapshot-labs/snapshot.js/tree/master/src/strategies/${strategy.key}`
-          "
-          target="_blank"
-        />
+        {{ strategy.key }}
       </h3>
       <div class="ml-1">v{{ strategy.version }}</div>
     </div>
-    <div>
-      <div>
-        <a
-          :href="`https://github.com/${strategy.author}`"
-          target="_blank"
-          class="text-gray"
-        >
-          <Icon name="github" class="mr-1" />
-          {{ strategy.author }}
-        </a>
-      </div>
-      <div>In {{ _numeral(strategy.spaces.length) }} space(s)</div>
+    <div class="text-gray">
+      <Icon name="github" class="mr-1" />
+      {{ strategy.author }}
     </div>
+    <div>In {{ _numeral(strategy.spaces.length) }} space(s)</div>
   </Block>
 </template>
 
