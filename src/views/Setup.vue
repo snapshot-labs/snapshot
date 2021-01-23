f4f1c12fd3060d7d595e86afe38b4ab4f9e9ab0c
Sam <samuv.eth@gmail.com>
2021-01-23 14:01:06 +0700

    feat: Layout component

--- src/views/Setup.vue
+++ src/views/Setup.vue
@@ -7,37 +7,39 @@
       </router-link>
     </div>
     <div>
-      <div class="col-12 col-lg-8 float-left pr-0 pr-lg-5">
-        <div class="px-4 px-md-0">
-          <h1 v-text="'Create a space'" class="mb-4" />
-        </div>
-        <Block>
-          <div class="mb-3">
-            Use an existing ENS name to create your space on.
+      <Layout>
+        <template #content-left>
+          <div class="px-4 px-md-0">
+            <h1 v-text="'Create a space'" class="mb-4" />
           </div>
-          <UiButton class="text-left width-full mb-3 d-flex px-3">
-            <input
-              v-model="id"
-              class="input flex-auto"
-              placeholder="e.g. yam.eth"
-            />
-            <a
-              class="d-block py-1 mr-n2"
-              target="_blank"
-              href="https://docs.snapshot.page/guides/create-a-space"
+          <Block>
+            <div class="mb-3">
+              Use an existing ENS name to create your space on.
+            </div>
+            <UiButton class="text-left width-full mb-3 d-flex px-3">
+              <input
+                v-model="id"
+                class="input flex-auto"
+                placeholder="e.g. yam.eth"
+              />
+              <a
+                class="d-block py-1 mr-n2"
+                target="_blank"
+                href="https://docs.snapshot.page/guides/create-a-space"
+              >
+                <Icon name="info" size="24" class="text-gray p-1" />
+              </a>
+            </UiButton>
+            <UiButton
+              :disabled="!id.includes('.eth') && !id.includes('.xyz')"
+              @click="handleSubmit"
+              class="button--submit width-full"
             >
-              <Icon name="info" size="24" class="text-gray p-1" />
-            </a>
-          </UiButton>
-          <UiButton
-            :disabled="!id.includes('.eth') && !id.includes('.xyz')"
-            @click="handleSubmit"
-            class="button--submit width-full"
-          >
-            Next
-          </UiButton>
-        </Block>
-      </div>
+              Next
+            </UiButton>
+          </Block>
+        </template>
+      </Layout>
     </div>
   </Container>
 </template>
--- src/views/Strategy.vue
+++ src/views/Strategy.vue
@@ -6,49 +6,50 @@
       </router-link>
     </div>
     <div>
-      <div class="col-12 col-lg-8 float-left pr-0 pr-lg-5">
-        <div class="px-4 px-md-0">
-          <h1 class="mb-2">
-            {{ strategy.key }}
-          </h1>
-          <span
-            v-text="`In ${strategy.spaces.length} space(s)`"
-            class="text-gray"
-          />
-          <UiMarkdown :body="strategy.about" class="mb-6 mt-4" />
-        </div>
-      </div>
-    </div>
-
-    <div class="col-12 col-lg-4 float-left">
-      <Block title="Information">
-        <div class="mb-1">
-          <b>Author</b>
-          <a
-            target="_blank"
-            class="float-right"
-            :href="`https://github.com/${strategy.author}`"
-          >
-            <Icon name="github" class="ml-1" />
-            {{ strategy.author }}
-          </a>
-        </div>
-        <div>
-          <div class="mb-1">
-            <b>Version</b>
-            <a
-              target="_blank"
-              class="float-right"
-              :href="
-                `https://github.com/snapshot-labs/snapshot.js/tree/master/src/strategies/${strategy.key}`
-              "
-            >
-              {{ strategy.version }}
-              <Icon name="external-link" class="ml-1" />
-            </a>
+      <Layout>
+        <template name="content-left">
+          <div class="px-4 px-md-0">
+            <h1 class="mb-2">
+              {{ strategy.key }}
+            </h1>
+            <span
+              v-text="`In ${strategy.spaces.length} space(s)`"
+              class="text-gray"
+            />
+            <UiMarkdown :body="strategy.about" class="mb-6 mt-4" />
           </div>
-        </div>
-      </Block>
+        </template>
+        <template name="sidebar-right">
+          <Block title="Information">
+            <div class="mb-1">
+              <b>Author</b>
+              <a
+                target="_blank"
+                class="float-right"
+                :href="`https://github.com/${strategy.author}`"
+              >
+                <Icon name="github" class="ml-1" />
+                {{ strategy.author }}
+              </a>
+            </div>
+            <div>
+              <div class="mb-1">
+                <b>Version</b>
+                <a
+                  target="_blank"
+                  class="float-right"
+                  :href="
+                    `https://github.com/snapshot-labs/snapshot.js/tree/master/src/strategies/${strategy.key}`
+                  "
+                >
+                  {{ strategy.version }}
+                  <Icon name="external-link" class="ml-1" />
+                </a>
+              </div>
+            </div>
+          </Block>
+        </template>
+      </Layout>
     </div>
   </Container>
 </template>
