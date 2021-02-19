c9983fd197e9100e1ab225b079be3905d3b36e78
bonustrack <fabien@bonustrack.co>
2021-02-20 02:12:08 +0700

    Update Portis

--- src/helpers/config.ts
+++ src/helpers/config.ts
@@ -9,7 +9,6 @@ const domainName = window.location.hostname;
 if (domainName.includes('localhost')) config.env = 'local';
 if (domainName === 'demo.snapshot.page' || domainName === 'demo.snapshot.vote')
   config.env = 'develop';
-// if (domainName === 'snapshot.page') delete config.connectors.torus;
-delete config.connectors.portis;
+if (domainName === 'snapshot.page') delete config.connectors.portis;
 
 export default config;
