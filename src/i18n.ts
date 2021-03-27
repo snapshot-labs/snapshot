76d9a638d3e1a90fdff365a586ef9dfec1af5e57
bonustrack <fabien@bonustrack.co>
2021-03-27 21:58:37 +0700

    Add default browser locale

--- src/i18n.ts
+++ src/i18n.ts
@@ -4,6 +4,24 @@ import messages from '@/locales';
 messages['en-US'] = messages.default;
 delete messages.default;
 
+export function getBrowserLocale() {
+  if (typeof navigator !== 'undefined') {
+    return (
+      navigator['userLanguage'] ||
+      navigator['language'] ||
+      (navigator.languages?.[0] ? navigator.languages[0] : undefined)
+    );
+  }
+  return undefined;
+}
+
+export let defaultLocale = 'en-US';
+
+const browserLocale = getBrowserLocale();
+Object.keys(messages).forEach(locale => {
+  if (locale.slice(0, 2) === browserLocale.slice(0, 2)) defaultLocale = locale;
+});
+
 const numberFormats = {
   en: {
     currency: {
@@ -25,6 +43,7 @@ const numberFormats = {
     }
   }
 };
+
 const datetimeFormats = {
   en: {
     short: {
@@ -38,7 +57,7 @@ const datetimeFormats = {
 };
 
 const i18n = createI18n({
-  locale: 'en-US',
+  locale: defaultLocale,
   datetimeFormats,
   numberFormats,
   messages
