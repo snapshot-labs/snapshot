5d98f5e60f88531ffc454f4b779ba6d04dbba3f7
Sam <samuv.eth@gmail.com>
2021-04-17 15:31:56 +0700

    Fix comments

--- src/i18n.ts
+++ src/i18n.ts
@@ -56,17 +56,18 @@ export async function loadLocaleMessages(i18n, locale) {
     lsRemove('locale');
     locale = 'zh-CN';
   }
-  // load locale messages with dynamic import
+
   try {
+    // load locale messages with dynamic import
     const messages = await import(
       /* webpackChunkName: "locale-[request]" */ `./locales/${locale}.json`
     );
+
+    // set locale and locale message
     i18n.global.setLocaleMessage(locale, messages.default);
   } catch (e) {
     console.log(e);
   }
-
-  // set locale and locale message
 
   return nextTick();
 }
