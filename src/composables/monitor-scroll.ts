cd76ad5e390f4b0975031b5d1da97ac4d9e39eb6
Sam <samuv.eth@gmail.com>
2021-04-22 17:03:04 +0700

    Move scroll monitor into vue-3 composable for composition api

---
+++ src/composables/monitor-scroll.ts
@@ -0,0 +1,18 @@
+import scrollMonitor from 'scrollmonitor';
+
+export function monitorScroll(fn) {
+  let canRunAgain = true;
+
+  const el = document.getElementById('endpage');
+  const elementWatcher = scrollMonitor.create(el);
+  elementWatcher.enterViewport(() => {
+    if (canRunAgain) {
+      canRunAgain = false;
+      fn();
+
+      setTimeout(function () {
+        canRunAgain = true;
+      }, 100);
+    }
+  });
+}
