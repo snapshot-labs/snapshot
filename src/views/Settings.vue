00914b7893daf9c87c3b9f71ebd88aa51ca28540
Sam <51686767+samuveth@users.noreply.github.com>
2021-04-28 00:55:45 +0700

    Add terms field and uri format validation (#331)
    * Add terms field and format validation
    
    * Remove console log
    
    * Add ajv format
    
    * Delete empty terms so that validation can pass
    
    Co-authored-by: Fabien <bonustrack@users.noreply.github.com>

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -62,6 +62,13 @@
                 <template v-slot:label> {{ $t(`settings.about`) }} </template>
               </UiInput>
               <UiInput
+                v-model="form.terms"
+                placeholder="e.g. https://example.com/terms"
+                :error="inputError('terms')"
+              >
+                <template v-slot:label> {{ $t(`settings.terms`) }} </template>
+              </UiInput>
+              <UiInput
                 @click="modalNetworksOpen = true"
                 :error="inputError('network')"
               >
@@ -364,6 +371,7 @@ export default {
   },
   computed: {
     validate() {
+      if (this.form.terms === '') delete this.form.terms;
       return validateSchema(schemas.space, this.form);
     },
     isValid() {
