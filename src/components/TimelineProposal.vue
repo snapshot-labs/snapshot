9e3e1dbe17595401d2e7c011db6041b231e14c4d
bonustrack <fabien@bonustrack.co>
2021-04-28 00:43:27 +0700

    Add preview body on timeline proposal

--- src/components/TimelineProposal.vue
+++ src/components/TimelineProposal.vue
@@ -1,6 +1,6 @@
 <template>
   <router-link
-    class="px-4 py-3 d-block text-gray"
+    class="px-4 py-3 pt-4 d-block text-gray"
     :to="{
       name: 'proposal',
       params: { key: proposal.space.id, id: proposal.id }
@@ -12,7 +12,8 @@
         <span class="ml-2" v-text="proposal.space.name" />
       </div>
       <h3 v-text="_shorten(proposal.name, 124)" />
-      <div class="mb-1">
+      <div class="mb-2">
+        <UiState :state="proposal.state" class="d-inline-block mr-1" />
         {{ $tc('proposalBy', [author]) }}
         <Badges
           :address="proposal.author.address"
@@ -20,17 +21,22 @@
         />
         {{ $tc(period, [_ms(proposal.start), _ms(proposal.end)]) }}
       </div>
-      <UiState :state="proposal.state" class="mb-2" />
+      <p v-text="_shorten(body, 140)" class="break-word" style="font-size: 20px;" />
     </div>
   </router-link>
 </template>
 
 <script>
+import removeMd from 'remove-markdown';
+
 export default {
   props: {
     proposal: Object
   },
   computed: {
+    body() {
+      return removeMd(this.proposal.body);
+    },
     period() {
       if (this.proposal.state === 'closed') return 'endedAgo';
       if (this.proposal.state === 'active') return 'endIn';
