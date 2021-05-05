5edc7f69f133c5440b2d7e785139895f21778658
Sam <samuv.eth@gmail.com>
2021-05-06 00:00:08 +0700

    Add network images

--- src/components/Block/Network.vue
+++ src/components/Block/Network.vue
@@ -1,6 +1,20 @@
 <template>
   <Block>
-    <div class="d-flex flex-items-baseline">
+    <div class="d-flex flex-items-center mb-1">
+      <img
+        v-if="imgValid"
+        class="circle border mr-2 mb-2"
+        :src="getLogoUrl(network.key)"
+        @error="imgValid = false"
+        width="28"
+        height="28"
+      />
+      <jazzicon
+        v-else
+        :seed="network.key"
+        :diameter="26"
+        class="d-inline-block line-height-0 mr-2 mb-2"
+      />
       <h3 v-text="network.name" />
       <div v-text="network.key" class="ml-1 text-gray" />
     </div>
@@ -12,6 +26,16 @@
 
 <script>
 export default {
-  props: ['network']
+  props: ['network'],
+  data() {
+    return {
+      imgValid: true
+    };
+  },
+  methods: {
+    getLogoUrl() {
+      return `https://raw.githubusercontent.com/snapshot-labs/snapshot.js/master/src/networks/${this.network.key}.png`;
+    }
+  }
 };
 </script>
