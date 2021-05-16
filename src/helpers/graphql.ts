c51ea2352573e1df47d31c26440b2214c786ee29
Sam <samuv.eth@gmail.com>
2021-05-17 01:09:11 +0700

    Refactor query

--- src/helpers/graphql.ts
+++ src/helpers/graphql.ts
@@ -1,47 +1,43 @@
 import { subgraphRequest } from '@snapshot-labs/snapshot.js/src/utils';
 
 export async function getProposalData(id) {
-  const response = await subgraphRequest(
-    `${process.env.VUE_APP_HUB_URL}/graphql`,
-    {
-      proposal: {
-        __args: {
-          id
-        },
+  return await subgraphRequest(`${process.env.VUE_APP_HUB_URL}/graphql`, {
+    proposal: {
+      __args: {
+        id
+      },
+      id: true,
+      title: true,
+      body: true,
+      choices: true,
+      start: true,
+      end: true,
+      snapshot: true,
+      state: true,
+      author: true,
+      created: true,
+      plugins: true,
+      network: true,
+      strategies: {
+        name: true,
+        params: true
+      },
+      space: {
         id: true,
-        title: true,
-        body: true,
-        choices: true,
-        start: true,
-        end: true,
-        snapshot: true,
-        state: true,
-        author: true,
-        created: true,
-        plugins: true,
-        network: true,
-        strategies: {
-          name: true,
-          params: true
-        },
-        space: {
-          id: true,
-          name: true
+        name: true
+      }
+    },
+    votes: {
+      __args: {
+        first: 10000,
+        where: {
+          proposal: id
         }
       },
-      votes: {
-        __args: {
-          first: 10000,
-          where: {
-            proposal: id
-          }
-        },
-        id: true,
-        voter: true,
-        created: true,
-        choice: true
-      }
+      id: true,
+      voter: true,
+      created: true,
+      choice: true
     }
-  );
-  return { proposal: response.proposal, votes: response.votes };
+  });
 }
