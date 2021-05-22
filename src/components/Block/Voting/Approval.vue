d1985e3ca2661b14fdf3b26974715e2f7a23a4f1
Sam <samuv.eth@gmail.com>
2021-05-22 16:43:14 +0700

    Refactor aragan govern voting into component

--- src/components/Block/Voting/Approval.vue
+++ src/components/Block/Voting/Approval.vue
@@ -9,22 +9,8 @@
         :class="selectedChoices.includes(i + 1) && 'button--active'"
       >
         {{ _shorten(choice, 32) }}
-        <!-- Will these plugins work here? -->
 
-        <!-- <a
-          v-if="proposal.plugins?.aragon?.[`choice${[i + 1]}`]"
-          @click="$emit('open')"
-          :aria-label="`Target address: ${
-            proposal.plugins.aragon[`choice${i + 1}`].actions[0].to
-          }\nValue: ${
-            proposal.plugins.aragon[`choice${i + 1}`].actions[0].value
-          }\nData: ${
-            proposal.plugins.aragon[`choice${i + 1}`].actions[0].data
-          }`"
-          class="tooltipped tooltipped-n break-word"
-        >
-          <Icon name="warning" class="v-align-middle ml-1" />
-        </a> -->
+        <PluginAragonGovern :proposal="proposal" />
       </UiButton>
     </div>
     <UiButton
