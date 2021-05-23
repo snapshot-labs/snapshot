ae6c7fce62e203dca3e6a46bcf8138db5c3eac8a
Sam <samuv.eth@gmail.com>
2021-05-24 00:17:54 +0700

    Fix case sensitively for castvote

---
+++ src/components/Block/CastVote.vue
@@ -0,0 +1,63 @@
+<template>
+  <Block class="mb-4" :title="$t('proposal.castVote')">
+    <div class="mb-3">
+      Testing Testing Testing Testing Testing Testing Testing Testing Testing
+      Testing Testing Testing
+      <!-- <BlockVotingSingleChoice
+        v-if="canVote('single-choice')"
+        :proposal="proposal"
+        @selectChoice="emitChoice"
+      />
+      <BlockVotingApproval
+        v-if="canVote('approval')"
+        :proposal="proposal"
+        @selectChoice="emitChoice"
+      /> -->
+    </div>
+    <UiButton
+      :disabled="app.authLoading || selectedChoices < 1"
+      @click="$emit('clickVote')"
+      class="d-block width-full button--submit"
+    >
+      {{ $t('proposal.vote') }}
+    </UiButton>
+  </Block>
+</template>
+
+<script>
+import { toRefs, computed } from 'vue';
+export default {
+  props: {
+    proposal: {
+      type: Object,
+      required: true
+    },
+    modelValue: [Array, Number],
+    loaded: Boolean,
+    ts: String
+  },
+  setup(props, { emit }) {
+    const { ts, loaded, proposal, modelValue } = toRefs(props);
+
+    const selectedChoices = computed(() => {
+      if (Array.isArray(modelValue.value)) return modelValue.value.length;
+      return modelValue.value;
+    });
+
+    function canVote(t) {
+      return (
+        proposal.value.type === t &&
+        loaded.value &&
+        ts.value >= proposal.value.start &&
+        ts.value < proposal.value.end
+      );
+    }
+
+    function emitChoice(c) {
+      emit('update:modelValue', c);
+    }
+
+    return { canVote, selectedChoices, emitChoice };
+  }
+};
+</script>
