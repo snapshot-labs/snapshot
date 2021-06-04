7b9b46c54ce868a50b40822db49a361cf2c9e089
Chaitanya <yourchaitu@gmail.com>
2021-06-04 10:02:54 +0530

    Show not supported message in modal when connected to Gnosis safe wallet (#397)
    * Show not supported message in modal when connected to Gnosis safe wallet
    
    * Apply suggestions from code review
    
    Co-authored-by: Fabien <bonustrack@users.noreply.github.com>
    
    * Change in Text
    
    Co-authored-by: Sam <51686767+samuveth@users.noreply.github.com>
    Co-authored-by: Fabien <bonustrack@users.noreply.github.com>

---
+++ src/components/Modal/WalletNotice.vue
@@ -0,0 +1,36 @@
+<template>
+  <UiModal :open="open" @close="$emit('close')">
+    <template v-slot:header>
+      <h3>{{ $t('walletNotice') }}</h3>
+    </template>
+    <div class="text-center my-2 p-4">
+      <h4>{{ $t('gnosisSafeWalletNotice') }}</h4>
+      <a
+        href="https://help.gnosis-safe.io/en/articles/4820197-how-to-participate-in-a-snapshot-poll"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <UiText :text="$t('learnMore')" />
+        <Icon name="external-link" class="ml-1" />
+      </a>
+    </div>
+    <template v-slot:footer>
+      <div>
+        <UiButton
+          @click="$emit('close')"
+          type="submit"
+          class="width-full button--submit"
+        >
+          {{ $t('continue') }}
+        </UiButton>
+      </div>
+    </template>
+  </UiModal>
+</template>
+
+<script>
+export default {
+  props: ['open'],
+  emits: ['close']
+};
+</script>
