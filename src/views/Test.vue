e633ee5dcd9457d18eb0738ebbd83e58eedce228
bonustrack <fabien@bonustrack.co>
2021-06-12 17:06:37 +0700

    Add text field

--- src/views/Test.vue
+++ src/views/Test.vue
@@ -5,6 +5,9 @@
         <h1 v-text="'Test'" class="mb-4" />
       </div>
       <Block>
+        <UiButton class="d-block width-full px-3 mb-3" style="height: auto">
+          <TextareaAutosize v-model="body" class="input width-full text-left" />
+        </UiButton>
         <UiButton @click="submit" class="button--submit width-full">
           {{ $t('submit') }}
         </UiButton>
@@ -15,22 +18,24 @@
 
 <script>
 import { send, signMessage } from '@/sign';
-import { computed } from 'vue';
+import { computed, ref } from 'vue';
 import { useStore } from 'vuex';
 import { getInstance } from '@snapshot-labs/lock/plugins/vue3';
 
 export default {
   setup() {
     const store = useStore();
-    const web3Account = computed(() => store.state.web3.account);
     const auth = getInstance();
+
+    const body = ref('');
+    const web3Account = computed(() => store.state.web3.account);
 
     async function submit() {
       try {
         const envelop = await signMessage(auth.web3, web3Account.value, {
           space: 'fabien.eth',
           type: 'post',
-          payload: 'Hi!'
+          payload: body.value
         });
         const result = await send(envelop);
         console.log('Result', result);
@@ -39,7 +44,7 @@ export default {
         console.log(e);
       }
     }
-    return { submit };
+    return { body, submit };
   }
 };
 </script>
