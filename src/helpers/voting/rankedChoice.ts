d94fb35c9fed25a4657e83e617deda7f0cf42e1b
Sam <samuv.eth@gmail.com>
2021-06-14 18:23:42 +0700

    Fix strategy scores

--- src/helpers/voting/rankedChoice.ts
+++ src/helpers/voting/rankedChoice.ts
@@ -4,19 +4,28 @@ function irv(ballots, rounds) {
   const candidates: any[] = [...new Set(ballots.map(vote => vote[0]).flat())];
   const votes = Object.entries(
     ballots.reduce((votes, [v], i, src) => {
-      votes[v[0]] += src[i][1];
+      votes[v[0]][0] += src[i][1];
+      votes[v[0]][1].length > 1
+        ? (votes[v[0]][1] = votes[v[0]][1].map(
+            (score, sI) => score + src[i][2][sI]
+          ))
+        : (votes[v[0]][1] = src[i][2]);
       return votes;
-    }, Object.assign({}, ...candidates.map(c => ({ [c]: 0 }))))
+    }, Object.assign({}, ...candidates.map(c => ({ [c]: [0, []] }))))
   );
-  const [topCand, topCount] = votes.reduce(
-    ([n, m]: (number | string)[], [v, c]: any[]) => (c > m ? [v, c] : [n, m]),
+
+  const votesWithoutScore = votes.map((vote: any) => [vote[0], vote[1][0]]);
+  const [topCand, topCount] = votesWithoutScore.reduce(
+    ([n, m]: any[], [v, c]: any[]) => (c > m ? [v, c] : [n, m]),
     ['?', -Infinity]
   );
-  const [bottomCand, bottomCount] = votes.reduce(
+  const [bottomCand, bottomCount] = votesWithoutScore.reduce(
     ([n, m]: any, [v, c]: any) => (c < m ? [v, c] : [n, m]),
     ['?', Infinity]
   );
-  const sortedByHighest = votes.sort((a: any, b: any) => b[1] - a[1]);
+
+  const sortedByHighest = votes.sort((a: any, b: any) => b[1][0] - a[1][0]);
+
   const totalPowerOfVotes = ballots
     .map(bal => bal[1])
     .reduce((a, b: any) => a + b, 0);
@@ -30,7 +39,11 @@ function irv(ballots, rounds) {
     ? rounds
     : irv(
         ballots
-          .map(ballot => [ballot[0].filter(c => c != bottomCand), ballot[1]])
+          .map(ballot => [
+            ballot[0].filter(c => c != bottomCand),
+            ballot[1],
+            ballot[2]
+          ])
           .filter(b => b[0].length > 0),
         rounds
       );
@@ -38,7 +51,7 @@ function irv(ballots, rounds) {
 
 function getFinalRound(i, votes) {
   const results = irv(
-    votes.map((vote: any) => [vote.choice, vote.balance]),
+    votes.map((vote: any) => [vote.choice, vote.balance, vote.scores]),
     []
   );
   const finalRound = results[results.length - 1];
@@ -60,15 +73,18 @@ export default class ApprovalVoting {
 
   resultsByVoteBalance() {
     return this.proposal.choices.map((choice, i) =>
-      getFinalRound(i, this.votes).reduce((a, b: any) => a + b[1], 0)
+      getFinalRound(i, this.votes).reduce((a, b: any) => a + b[1][0], 0)
     );
   }
 
   resultsByStrategyScore() {
     return this.proposal.choices.map((choice, i) =>
-      this.strategies.map((strategy, sI) =>
-        getFinalRound(i, this.votes).reduce((a, b: any) => a + b[1], 0)
-      )
+      this.strategies.map((strategy, sI) => {
+        return getFinalRound(i, this.votes).reduce(
+          (a, b: any) => a + b[1][1][sI],
+          0
+        );
+      })
     );
   }
 
