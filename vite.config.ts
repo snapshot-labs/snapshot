9057253b53bd1f54c956ede03cb720fdadbd3afb
bonustrack <fabien@bonustrack.co>
2021-06-29 23:47:25 +0700

    Fix component import

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import Vue from '@vitejs/plugin-vue';
 import ViteComponents from 'vite-plugin-components';
 
 export default defineConfig({
-  plugins: [Vue(), ViteComponents()],
+  plugins: [Vue(), ViteComponents({ deep: true, directoryAsNamespace: true })],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
