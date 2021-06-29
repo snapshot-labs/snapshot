d9ea1484ba3b4b3f2bef1af02c3e0e8271198c04
Sam <samuv.eth@gmail.com>
2021-06-29 10:09:07 +0700

    Remove link when selecting validation in modal

--- src/components/Block/Validation.vue
+++ src/components/Block/Validation.vue
@@ -1,13 +1,7 @@
 <template>
   <Block>
     <div class="d-flex flex-items-center mb-1">
-      <a
-        :href="`https://github.com/snapshot-labs/snapshot.js/tree/master/src/validations/${validation.name}/index.ts`"
-        target="_blank"
-        class="d-flex"
-      >
-        <h3 v-text="validation.name" />
-      </a>
+      <h3 v-text="validation.name" />
     </div>
     <div class="text-gray">
       {{ $tc('inSpaces', [_n(validation.spaces.length)]) }}
