0e79e6af696b84453ce47b60e1b5aefd76b1e460
Chaitanya <yourchaitu@gmail.com>
2021-06-29 07:55:03 +0530

    lint fix

--- src/helpers/ens.ts
+++ src/helpers/ens.ts
@@ -37,24 +37,26 @@ export async function getSpaceUriFromContentHash(id) {
 }
 
 export async function getSpaceUriFromTextRecord(id) {
-  const REGISTRAR_ABI = 'function resolver(bytes32 node) external view returns (address)';
-  const RESOLVER_ABI = 'function text(bytes32 node, string calldata key) external view returns (string memory)'
+  const REGISTRAR_ABI =
+    'function resolver(bytes32 node) external view returns (address)';
+  const RESOLVER_ABI =
+    'function text(bytes32 node, string calldata key) external view returns (string memory)';
 
   const REGISTRAR_ADDRESS = '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e';
   let uri: any = false;
   try {
     const hash = namehash(id);
     const provider = getProvider('1');
-    const resolverAddress = await utils.call(provider, [REGISTRAR_ABI], [
-      REGISTRAR_ADDRESS,
-      'resolver',
-      [hash]
-    ]);
-    uri = await utils.call(provider, [RESOLVER_ABI], [
-      resolverAddress,
-      'text',
-      [hash, 'snapshot']
-    ]);
+    const resolverAddress = await utils.call(
+      provider,
+      [REGISTRAR_ABI],
+      [REGISTRAR_ADDRESS, 'resolver', [hash]]
+    );
+    uri = await utils.call(
+      provider,
+      [RESOLVER_ABI],
+      [resolverAddress, 'text', [hash, 'snapshot']]
+    );
   } catch (e) {
     console.log('getSpaceUriFromTextRecord failed', id, e);
   }
