3cf7cbe2c5bc7c683e749b6a3206c5761f637108
Sam <51686767+samuveth@users.noreply.github.com>
2021-07-02 15:23:56 +0700

    Add validation input and modal (#436)
    * Add validation input and modal
    
    * Remove link when selecting validation in modal
    
    * Fis placeholder translation path
    
    * Requested changes and remove vue warnings about root component
    
    Co-authored-by: Fabien <bonustrack@users.noreply.github.com>

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,5 +1,5 @@
 <template>
-  <Layout>
+  <Layout v-bind="$attrs">
     <template #content-left>
       <div class="px-4 px-md-0 mb-3">
         <router-link :to="{ name: 'home' }" class="text-gray">
@@ -229,23 +229,41 @@
               />
             </UiButton>
           </Block>
-          <Block :title="$t('settings.filters')">
+          <Block :title="$t('settings.proposalValidation')">
             <div class="mb-2">
               <UiInput
-                v-model="form.filters.minScore"
-                :error="inputError('minScore')"
-                :number="true"
+                @click="modalValidationOpen = true"
+                :error="inputError('settings.validation')"
               >
-                <template v-slot:label>{{
-                  $t('settings.proposalThreshold')
-                }}</template>
+                <template v-slot:selected>
+                  {{ form.validation.name }}
+                </template>
+                <template v-slot:label>
+                  {{ $t(`settings.validation`) }}
+                </template>
+                <template v-slot:info>
+                  <span @click="handleEditValidation">
+                    {{ $t(`edit`) }}
+                  </span>
+                </template>
               </UiInput>
-              <div class="mb-2 d-flex flex-items-center px-2">
-                <Checkbox
-                  v-model="form.filters.onlyMembers"
-                  class="mr-2 mt-1"
-                />
-                {{ $t('settings.allowOnlyMembers') }}
+              <div v-if="form.validation.name === 'basic'">
+                <UiInput
+                  v-model="form.filters.minScore"
+                  :error="inputError('minScore')"
+                  :number="true"
+                >
+                  <template v-slot:label>{{
+                    $t('settings.proposalThreshold')
+                  }}</template>
+                </UiInput>
+                <div class="mb-2 d-flex flex-items-center px-2">
+                  <Checkbox
+                    v-model="form.filters.onlyMembers"
+                    class="mr-2 mt-1"
+                  />
+                  {{ $t('settings.allowOnlyMembers') }}
+                </div>
               </div>
             </div>
           </Block>
@@ -318,6 +336,12 @@
       @add="handleSubmitAddPlugins"
       :plugin="currentPlugin"
     />
+    <ModalValidation
+      :open="modalValidationOpen"
+      @close="(modalValidationOpen = false), (currentValidation = {})"
+      @add="handleSubmitAddValidation"
+      :validation="currentValidation"
+    />
   </teleport>
 </template>
 
@@ -358,11 +382,13 @@ export default {
       currentContenthash: '',
       currentStrategy: {},
       currentPlugin: {},
+      currentValidation: {},
       currentStrategyIndex: false,
       modalNetworksOpen: false,
       modalSkinsOpen: false,
       modalStrategyOpen: false,
       modalPluginsOpen: false,
+      modalValidationOpen: false,
       loaded: false,
       loading: false,
       uploadLoading: false,
@@ -370,7 +396,8 @@ export default {
       form: {
         strategies: [],
         plugins: {},
-        filters: {}
+        filters: {},
+        validation: {}
       },
       networks
     };
@@ -412,6 +439,7 @@ export default {
       delete space._activeProposals;
       space.strategies = space.strategies || [];
       space.plugins = space.plugins || {};
+      space.validation = space.validation || { name: 'basic', params: {} };
       space.filters = space.filters || {};
       this.currentSettings = clone(space);
       this.form = space;
@@ -514,6 +542,14 @@ export default {
     },
     handleSubmitAddPlugins(payload) {
       this.form.plugins[payload.key] = payload.input;
+    },
+    handleEditValidation() {
+      this.currentValidation = {};
+      this.currentValidation = clone(this.form.validation);
+      this.modalValidationOpen = true;
+    },
+    handleSubmitAddValidation(validation) {
+      this.form.validation = validation;
     },
     setUploadLoading(s) {
       this.uploadLoading = s;
