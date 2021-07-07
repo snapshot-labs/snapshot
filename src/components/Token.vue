4ed8b285e38fd3693b652678320a59c802952795
Sam <samuv.eth@gmail.com>
2021-07-07 16:43:18 +0700

    Refactor main components using Comp API part 2

--- src/components/Token.vue
+++ src/components/Token.vue
@@ -5,37 +5,35 @@
 </template>
 
 <script>
+import { computed } from 'vue';
 import { formatBytes32String } from '@ethersproject/strings';
 import { getUrl } from '@snapshot-labs/snapshot.js/src/utils.ts';
 
 export default {
-  props: ['space', 'size', 'symbolIndex'],
-  data() {
-    return {
-      error: false
-    };
-  },
-  computed: {
-    url() {
-      const file = this.symbolIndex
-        ? this.symbolIndex === 'space'
+  props: { space: Object, size: String, symbolIndex: [String, Number] },
+  setup(props) {
+    const spaceId = computed(() => props.space.id ?? props.space.key);
+
+    const url = computed(() => {
+      const file = props.symbolIndex
+        ? props.symbolIndex === 'space'
           ? 'space'
-          : `logo${this.symbolIndex}`
+          : `logo${props.symbolIndex}`
         : 'logo';
-      const url = this.space.avatar
-        ? this.space.avatar
-        : `https://raw.githubusercontent.com/snapshot-labs/snapshot-spaces/master/spaces/${this.spaceId}/${file}.png`;
+      const url = props.space.avatar
+        ? props.space.avatar
+        : `https://raw.githubusercontent.com/snapshot-labs/snapshot-spaces/master/spaces/${spaceId.value}/${file}.png`;
       return `https://worker.snapshot.org/mirror?img=${encodeURIComponent(
         getUrl(url)
       )}`;
-    },
-    spaceAddress() {
-      if (this.spaceId) return formatBytes32String(this.spaceId.slice(0, 24));
+    });
+
+    const spaceAddress = computed(() => {
+      if (spaceId.value) return formatBytes32String(spaceId.value.slice(0, 24));
       return '';
-    },
-    spaceId() {
-      return this.space.id ?? this.space.key;
-    }
+    });
+
+    return { url, spaceAddress };
   }
 };
 </script>
