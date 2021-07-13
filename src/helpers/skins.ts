fd148e80848cba0c3596418cd03b8652218a4098
Sam <samuv.eth@gmail.com>
2021-07-13 11:04:03 +0700

    stash

--- src/helpers/skins.ts
+++ src/helpers/skins.ts
@@ -1,1 +1,13 @@
+const requireFile = import.meta.glob(
+  '/@snapshot-labs/snapshot-spaces/skins/*.scss'
+);
+
+console.log(requireFile);
+
+// requireFile.keys().map(file => requireFile(file));
+
+// export default requireFile
+//   .keys()
+//   .map(file => file.replace('./', '').replace('.scss', ''));
+
 export default [];
