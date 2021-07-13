1398e5f0e805325aeee5568f0292fa9c7f5c1661
Sam <samuv.eth@gmail.com>
2021-07-13 12:39:21 +0700

    Fix abi files import

--- src/helpers/abi/index.ts
+++ src/helpers/abi/index.ts
@@ -1,10 +1,11 @@
 const requireFile = import.meta.glob('./*.json');
 
-export default Object.fromEntries(
-  await Promise.all(
-    Object.keys(requireFile).map(async path => {
-      const file = await requireFile[path]();
-      return [path.replace('./', '').replace('.json', ''), file.abi];
-    })
-  )
-);
+const files = {};
+
+for (const path in requireFile) {
+  requireFile[path]().then(file => {
+    files[path.replace('./', '').replace('.json', '')] = file.abi;
+  });
+}
+
+export default files;
