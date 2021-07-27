9e06eca1eeee9c9365e5cbe8438bf014bdb06650
bonustrack <fabien@bonustrack.co>
2021-07-27 22:13:57 +0700

    Add portis

--- src/helpers/auth.ts
+++ src/helpers/auth.ts
@@ -1,13 +1,15 @@
 import injected from '@snapshot-labs/lock/connectors/injected';
 import walletconnect from '@snapshot-labs/lock/connectors/walletconnect';
 import torus from '@snapshot-labs/lock/connectors/torus';
+import portis from '@snapshot-labs/lock/connectors/portis';
 import connectors from '@/helpers/connectors.json';
 
 const options: any = { connectors: [] };
 const lockConnectors = {
   injected,
   walletconnect,
-  torus
+  torus,
+  portis
 };
 
 Object.entries(connectors).forEach((connector: any) => {
