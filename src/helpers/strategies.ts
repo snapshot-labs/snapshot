9a48ee5cf760080eff2e8d1da2654bed799658be
Chaitanya <yourchaitu@gmail.com>
2021-07-29 19:07:58 +0530

    Fix for markdown in strategies

--- src/helpers/strategies.ts
+++ src/helpers/strategies.ts
@@ -1,46 +1,9 @@
-const requireFile = require.context(
-  '@snapshot-labs/snapshot.js/src/strategies',
-  true,
-  /index\.ts$/
-);
-
-const requireReadmeFile = require.context(
-  '@snapshot-labs/snapshot.js/src/strategies',
-  true,
-  /README\.md$/
-);
-
-const requireExamplesJson = require.context(
-  '@snapshot-labs/snapshot.js/src/strategies',
-  true,
-  /examples\.json$/
-);
+import strategies from '@snapshot-labs/snapshot.js/src/strategies';
 
 export default Object.fromEntries(
-  requireFile
-    .keys()
-    .filter(fileName => fileName !== './index.ts')
-    .map(fileName => {
-      const key = fileName.replace('./', '').replace('/index.ts', '');
-      const strategy = requireFile(fileName);
-      strategy.key = key;
-      try {
-        strategy.examples = requireExamplesJson(
-          fileName.replace('index.ts', 'examples.json')
-        ).default;
-        strategy.about = requireReadmeFile(
-          fileName.replace('index.ts', 'README.md')
-        ).default;
-      } catch (error) {
-        strategy.about = '';
-      }
-      try {
-        strategy.examples = requireExamplesJson(
-          fileName.replace('index.ts', 'examples.json')
-        );
-      } catch (error) {
-        strategy.examples = null;
-      }
-      return [key, strategy];
-    })
+  Object.keys(strategies).map(strategyName => {
+    const strategy = strategies[strategyName];
+    strategy.key = strategyName;
+    return [strategyName, strategy];
+  })
 );
