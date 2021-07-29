754a64b72c456a6e2f3eabaa9bb85ceca890bf31
bonustrack <fabien@bonustrack.co>
2021-07-29 23:43:00 +0700

    rename

--- src/helpers/strategies.ts
+++ src/helpers/strategies.ts
@@ -1,8 +1,8 @@
 import strategies from '@snapshot-labs/snapshot.js/src/strategies';
 
 export default Object.fromEntries(
-  Object.keys(strategies).map(strategyName => {
-    const strategy = strategies[strategyName];
+  Object.keys(strategies).map(name => {
+    const strategy = strategies[name];
     strategy.key = name;
     return [name, strategy];
   })
