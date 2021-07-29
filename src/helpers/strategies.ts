05883ad738f8e34628b2338028d734ab330ab939
Fabien <bonustrack@users.noreply.github.com>
2021-07-29 23:39:44 +0700

    Update src/helpers/strategies.ts

--- src/helpers/strategies.ts
+++ src/helpers/strategies.ts
@@ -3,7 +3,7 @@ import strategies from '@snapshot-labs/snapshot.js/src/strategies';
 export default Object.fromEntries(
   Object.keys(strategies).map(strategyName => {
     const strategy = strategies[strategyName];
-    strategy.key = strategyName;
+    strategy.key = name;
     return [name, strategy];
   })
 );
