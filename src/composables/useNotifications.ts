e2086a85d510ff9d699319794eaac06c5f9fc3cd
ironman <ironman@ironmans-MacBook-Pro.local>
2021-07-30 10:29:46 -0400

    Fix: reactive to ref

--- src/composables/useNotifications.ts
+++ src/composables/useNotifications.ts
@@ -1,4 +1,4 @@
-import { reactive, toRefs } from 'vue';
+import { ref } from 'vue';
 
 interface Notification {
   message: string;
@@ -6,18 +6,16 @@ interface Notification {
   timestamp: number;
 }
 
-const state = reactive({
-  items: [] as Notification[]
-});
+const items = ref<Notification[]>([]);
 
 export function useNotifications() {
-  const notify = payload => {
+  function notify(payload) {
     const item = Array.isArray(payload)
       ? { message: payload[1], type: payload[0], timestamp: Date.now() }
       : { message: payload, type: 'green', timestamp: Date.now() };
 
-    state.items.push(item);
-  };
+    items.value.push(item);
+  }
 
-  return { notify, ...toRefs(state) };
+  return { notify, items };
 }
