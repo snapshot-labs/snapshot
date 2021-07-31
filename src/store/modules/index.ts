d151d05d057e66a61c32ac3334293400108df82a
Fabien <bonustrack@users.noreply.github.com>
2021-07-31 22:08:15 +0700

    Update index.ts

--- src/store/modules/index.ts
+++ src/store/modules/index.ts
@@ -1,11 +1,7 @@
 import app from './app';
-import favoriteSpaces from './favoriteSpaces';
-import notifications from './notifications';
 import web3 from './web3';
 
 export default {
   app,
-  favoriteSpaces,
-  notifications,
   web3
 };
