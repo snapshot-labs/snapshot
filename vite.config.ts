b251acdef4be21bda5ba68f1ff94b167fa026da6
bonustrack <fabien@bonustrack.co>
2021-08-01 01:03:15 +0700

    Add visualizer

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,21 @@ import path from 'path';
 import { defineConfig } from 'vite';
 import Vue from '@vitejs/plugin-vue';
 import ViteComponents from 'vite-plugin-components';
+import visualizer from 'rollup-plugin-visualizer';
 
 export default defineConfig({
   define: {
     'process.env': process.env
   },
-  plugins: [Vue(), ViteComponents({ deep: true, directoryAsNamespace: true })],
+  plugins: [
+    Vue(),
+    ViteComponents({ deep: true, directoryAsNamespace: true }),
+    visualizer({
+      filename: './dist/stats.html',
+      template: 'sunburst',
+      gzipSize: true
+    })
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
