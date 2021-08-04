e63b53b55dc1fc879cc726beb1473a652b994ca2
Sam <51686767+samuveth@users.noreply.github.com>
2021-08-04 16:51:28 +0700

    Refactor space and timeline apollo query to use new composable (#529)
    Co-authored-by: Fabien <bonustrack@users.noreply.github.com>

--- src/views/Space.vue
+++ src/views/Space.vue
@@ -5,7 +5,7 @@ import { useRoute } from 'vue-router';
 import { useInfiniteLoader } from '@/composables/useInfiniteLoader';
 import { useScrollMonitor } from '@/composables/useScrollMonitor';
 import { useDomain } from '@/composables/useDomain';
-import { apolloClient } from '@/helpers/apollo';
+import { useApolloQuery } from '@/composables/useApolloQuery';
 import { PROPOSALS_QUERY } from '@/helpers/queries';
 import { useProfiles } from '@/composables/useProfiles';
 
@@ -33,9 +33,10 @@ const { endElement } = useScrollMonitor(() =>
 );
 
 // Proposals query
+const { apolloQuery } = useApolloQuery();
 async function loadProposals(skip = 0) {
-  try {
-    const response = await apolloClient.query({
+  const proposalsObj = await apolloQuery(
+    {
       query: PROPOSALS_QUERY,
       variables: {
         first: loadBy,
@@ -44,12 +45,11 @@ async function loadProposals(skip = 0) {
         state: filterBy.value === 'core' ? 'all' : filterBy.value,
         author_in: filterBy.value === 'core' ? spaceMembers.value : []
       }
-    });
-    stopLoadingMore.value = response.data.proposals?.length < loadBy;
-    proposals.value = proposals.value.concat(response.data.proposals);
-  } catch (e) {
-    console.log(e);
-  }
+    },
+    'proposals'
+  );
+  stopLoadingMore.value = proposalsObj?.length < loadBy;
+  proposals.value = proposals.value.concat(proposalsObj);
 }
 
 // Initialize
