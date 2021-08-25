4ce5182ec49b731861f2c0914d06f9dbdbf1dd7e
NTB <nfttop.best@gmail.com>
2021-08-25 16:09:54 +0800

    🐛  fix pendingCount lt 0 bug

--- src/composables/useTxStatus.ts
+++ src/composables/useTxStatus.ts
@@ -3,6 +3,11 @@ import { ref, watch, nextTick } from 'vue';
 const pendingCount = ref(0);
 
 export function useTxStatus() {
+  watch(pendingCount, () => {
+    if (pendingCount.value < 0) {
+      pendingCount.value = 0
+    }
+  })
   return { pendingCount };
 }
 
