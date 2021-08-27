21b9f6ce3434ec4d703c68d1627a8ec4ade03726
febriano <rinoakbr@gmail.com>
2021-08-27 16:39:01 +0700

    kurang submit dengan ipdb

--- src/components/Plugin/CommentBox/CustomBlock.vue
+++ src/components/Plugin/CommentBox/CustomBlock.vue
@@ -13,21 +13,30 @@
     <div></div>
     <UiButton class="mt-2 button--submit"> Submit </UiButton>
     <UiButton class="ml-2 mt-2 button--primary"> Preview </UiButton>
-    <Block :slim="true" class="p-4 d-block text-color">
-<div>
-      <div class="mb-2">
-        <!-- <Token :space="proposal.space" size="28" /> -->
-        <span class="ml-2" v-text="`dsa`" />
-        <span class="ml-2" v-text="`dsa`" />
-        <!-- {{ $tc('proposalBy', [username]) }} -->
-        <!-- <Badges :address="proposal.author" :members="proposal.space.members" /> -->
-        <!-- <UiState :state="proposal.state" class="d-inline-block float-right" /> -->
+    <Block :slim="true" class="p-4 d-block text-color mt-2">
+      <div>
+        <div class="mb-2">
+          <Token :space="{space:{id:'thanku.eth'}}" size="23" />
+          <span class="ml-2" v-text="`dsa`" />
+          <UiDropdown
+                            
+              class="float-right"
+             
+            >
+              <div >
+                <UiLoading v-if="dropdownLoading" />
+                <Icon
+                  v-else
+                  name="threedots"
+                  size="25"
+                  class="v-align-text-bottom"
+                />
+              </div>
+            </UiDropdown>
+        </div>
+         <p v-text="`sadas`" class="break-word mb-1" style="font-size: 20px" />
+        <div class="mt-1">adsad</div>
       </div>
-      <h3 v-text="`dsa`" class="mt-1" />
-      <p v-text="`sadas`" class="break-word mb-1" style="font-size: 20px" />
-      <div class="mt-1">adsad</div>
-    </div>
     </Block>
-    
   </Block>
 </template>
