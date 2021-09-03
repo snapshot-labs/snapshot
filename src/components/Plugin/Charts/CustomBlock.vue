e7dd95370da885e3723a282fb9d3c1e270e4df9f
Sam <samuv.eth@gmail.com>
2021-09-03 09:18:08 +0700

    Fix plugin modal on proposal creation error and votes array mutation error

--- src/components/Plugin/Charts/CustomBlock.vue
+++ src/components/Plugin/Charts/CustomBlock.vue
@@ -7,7 +7,7 @@ Chart.register(...registerables);
 const props = defineProps({
   space: Object,
   proposal: Object,
-  votes: Object,
+  votes: Object
 });
 
 const totalVotesPerDayChart = ref(null);
@@ -16,14 +16,13 @@ const votingPowerPerDay = ref(null);
 
 const space = computed(() => props.space);
 const proposal = computed(() => props.proposal);
-const votes = computed(() => {
-  votes.sort((a, b) => a.created - b.created);
-  return votes;
-});
+const votes = computed(() =>
+  [...props.votes].sort((a, b) => a.created - b.created)
+);
 
 onMounted(() => {
   const canvasRef = totalVotesPerDayChart.value;
-  if (! canvasRef) {
+  if (!canvasRef) {
     return;
   }
 
@@ -44,13 +43,15 @@ onMounted(() => {
   const labels = Array.from(dates.keys());
   const data = {
     labels: labels,
-    datasets: [{
-      label: 'Total votes per day',
-      data: Array.from(dates.values()),
-      backgroundColor: 'rgba(75, 192, 192, 0.2)',
-      borderColor: 'rgb(75, 192, 192)',
-      borderWidth: 1
-    }]
+    datasets: [
+      {
+        label: 'Total votes per day',
+        data: Array.from(dates.values()),
+        backgroundColor: 'rgba(75, 192, 192, 0.2)',
+        borderColor: 'rgb(75, 192, 192)',
+        borderWidth: 1
+      }
+    ]
   };
 
   const config = {
@@ -71,7 +72,7 @@ onMounted(() => {
 
 onMounted(() => {
   const canvasRef = votingPowerPerAddress.value;
-  if (! canvasRef) {
+  if (!canvasRef) {
     return;
   }
 
@@ -84,7 +85,9 @@ onMounted(() => {
   let balances = newVotes.map(vote => vote.balance);
 
   if (newVotes.length > MAX_ADDRESSES + 1) {
-    const extraBalances = balances.slice(MAX_ADDRESSES).reduce((total, current) => total + current);
+    const extraBalances = balances
+      .slice(MAX_ADDRESSES)
+      .reduce((total, current) => total + current);
     labels = labels.slice(0, MAX_ADDRESSES);
     balances = balances.slice(0, MAX_ADDRESSES);
     labels.push('Others');
@@ -93,11 +96,15 @@ onMounted(() => {
 
   const data = {
     labels,
-    datasets: [{
-      label: 'Share of voting power',
-      data: balances,
-      backgroundColor: labels.map(label => label === 'Others' ? '#cccccc' : '#' + label.substr(-6)),
-    }]
+    datasets: [
+      {
+        label: 'Share of voting power',
+        data: balances,
+        backgroundColor: labels.map(label =>
+          label === 'Others' ? '#cccccc' : '#' + label.substr(-6)
+        )
+      }
+    ]
   };
 
   const config = {
@@ -122,20 +129,22 @@ onMounted(() => {
 
 onMounted(() => {
   const canvasRef = votingPowerPerDay.value;
-  if (! canvasRef) {
+  if (!canvasRef) {
     return;
   }
 
   const datasets = proposal.value.choices.map((choice, index) => {
-    const votesForThisChoice = votes.value.filter(vote => vote.choice === index + 1);
+    const votesForThisChoice = votes.value.filter(
+      vote => vote.choice === index + 1
+    );
     const data = votesForThisChoice.reduce((result, current, index) => {
-      const date = new Date(current.created * 1000).toString()
+      const date = new Date(current.created * 1000).toString();
       const balance = current.balance;
 
       if (index === 0) {
         return [{ x: date, y: balance }];
       }
-      
+
       const previous = result[index - 1];
       return result.concat({ x: date, y: previous.y + balance });
     }, []);
@@ -144,9 +153,12 @@ onMounted(() => {
       label: choice,
       data,
       fill: false,
-      borderColor: votesForThisChoice.length > 0 ? '#' + votesForThisChoice[0].voter.substr(-6) : '', // as close to random as it gets
-      tension: .1,
-    }
+      borderColor:
+        votesForThisChoice.length > 0
+          ? '#' + votesForThisChoice[0].voter.substr(-6)
+          : '', // as close to random as it gets
+      tension: 0.1
+    };
   });
 
   const data = { datasets };
@@ -154,7 +166,7 @@ onMounted(() => {
     scales: {
       x: {
         ticks: {
-          display: false,
+          display: false
         }
       }
     },
@@ -168,7 +180,6 @@ onMounted(() => {
 
   new Chart(canvasRef, { type: 'line', data, options });
 });
-
 </script>
 
 <template>
@@ -185,7 +196,7 @@ onMounted(() => {
       </div>
     </div>
     <div v-if="votes.length === 0">
-      {{ $t('charts.noVotesYet' ) }}
+      {{ $t('charts.noVotesYet') }}
     </div>
   </Block>
 </template>
