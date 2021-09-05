2a874a7b5aeaa9ca0c6fe03ef1268cdab2a6b130
Sam <samuv.eth@gmail.com>
2021-09-05 20:05:32 +0700

    Fix placerholder visibility and header spacing

--- tailwind.config.js
+++ tailwind.config.js
@@ -52,7 +52,7 @@ module.exports = {
       '2xl': ['36px', '50px'],
       xl: ['28px', '44px'],
       lg: ['24px', '42px'],
-      md: ['20px', '32px'],
+      md: ['20px', '28px'],
       base: ['18px', '24px'],
       sm: ['16px', '22px'],
       xs: ['14px', '20px']
