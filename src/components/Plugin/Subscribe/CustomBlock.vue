3e073aa9ed92e7b416af6102fc347d0d4df8bbeb
dapiguabc <dapiguabc@163.com>
2021-09-06 14:50:23 +0800

    Add a plugin named Subscribe

---
+++ src/components/Plugin/Subscribe/CustomBlock.vue
@@ -0,0 +1,49 @@
+<template>
+  <Block title="Subscribe">
+    <UiInput :required="false" placeholder="Enter Your Email" v-model="email">
+    </UiInput>
+    <UiButton
+      :loading="loading"
+      :disabled="email === ''"
+      class="width-full mb-2 button--submit"
+      @click="action"
+    >
+      {{ $t('subscribe.action') }}
+    </UiButton>
+  </Block>
+</template>
+<script>
+import Plugin from '@snapshot-labs/snapshot.js/src/plugins/subscribe';
+
+export default {
+  props: ['proposalId'],
+  inject: ['notify'],
+  data() {
+    return {
+      loading: false,
+      email: '',
+      plugin: new Plugin()
+    };
+  },
+  methods: {
+    async action() {
+      const regEmail = /^([a-zA-Z]|[0-9])(\w|-)+@[a-zA-Z0-9]+\.([a-zA-Z]{2,4})$/;
+      if (regEmail.test(this.email)) {
+        this.loading = true;
+        const res = await this.plugin.subscribe(this.proposalId, this.email);
+        this.loading = false;
+        if (res.status === 200){
+          this.notify(['green', this.$t('subscribe.subscribe_success')]);
+        } else {
+          this.notify([
+            'red',
+            this.$t('subscribe.subscribe_fail', [res.message])
+          ]);
+        }
+      } else {
+        this.notify(['red', this.$t('subscribe.email_format_error')]);
+      }
+    }
+  }
+};
+</script>
