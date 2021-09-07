0b124fd444bec02bf204c1b816d8b6fbf8c2aeef
Sam <samuv.eth@gmail.com>
2021-09-07 10:22:30 +0700

    Fix prograss bar

--- src/components/Ui/Progress.vue
+++ src/components/Ui/Progress.vue
@@ -9,16 +9,17 @@ const bars = computed(() =>
 </script>
 
 <template>
-  <div class="h-2 relative overflow-hidden rounded-full flex opacity-90">
+  <div class="h-2 relative overflow-hidden rounded-full flex">
     <div
       class="w-full h-full bg-[color:var(--border-color)] absolute z-5"
     ></div>
+    <!-- TODO: Reword progress bar, for now opcity is just alternating -->
     <div
       v-for="(bar, i) in bars"
       :key="i"
       :style="`width: ${parseFloat((100 / max) * bar).toFixed(3)}%;`"
       class="bg-blue h-full z-10"
-      :class="{ 'opacity-60': i !== 1 && i % 2 !== 0 }"
+      :class="{ 'opacity-50': i % 2 == 1 }"
     />
   </div>
 </template>
