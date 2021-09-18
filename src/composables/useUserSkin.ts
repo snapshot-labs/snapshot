e667f89600ea92864101a5e932b54a30c4a84988
Harsh <harshmaur@gmail.com>
2021-09-18 15:17:01 +0530

    fix(): persist skin across refreshes by setting the skin from localstorage

--- src/composables/useUserSkin.ts
+++ src/composables/useUserSkin.ts
@@ -5,12 +5,13 @@ const NOT_SET = 'not-set';
 const DARK_MODE = 'dark-mode';
 const LIGHT_MODE = 'light-mode';
 
+const currenSkin = lsGet('skin', NOT_SET);
 const osSkin =
   window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches
     ? DARK_MODE
     : LIGHT_MODE;
 
-const userSkin = ref(osSkin);
+const userSkin = ref(currenSkin === NOT_SET ? osSkin : currenSkin);
 const getSkinIcon = () => (userSkin.value === LIGHT_MODE ? 'moon' : 'sun');
 const _toggleSkin = skin => {
   if (skin === LIGHT_MODE) {
