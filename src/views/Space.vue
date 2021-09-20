defbbafa8615a0ad9dcaecd1ed60ec48f50c541e
Harsh Maur <harshmaur@gmail.com>
2021-09-20 19:33:09 +0530

    fix(): routing for custom domains + nested routes (#646)
    * Refactor space with nested routes
    
    * Rename rout names to fit flat folter structure standards
    
    * Rename SpaceIndex to Space
    
    * Change route naming to camelCase and fix custom domain routing guard
    
    * Handle custom domain
    
    * Handle wrong space
    
    * Fix url not working on custom domain when key is defined
    
    * fix(): routing for custom domains + nested routes
    
    * fix(): skin on aliased spaces, redirect to ens if aliased, redirect to home if domain specified
    
    * fix(): redirect the user to the without space url when custom domain is configured
    
    * fix(): routing for spaces
    
    * fix(): handle both aliases key and value
    
    * fix(): create back button
    
    Co-authored-by: Sam <samuv.eth@gmail.com>

--- src/views/Space.vue
+++ src/views/Space.vue
@@ -1,144 +1,38 @@
 <script setup>
-import { computed, onMounted, ref, watch } from 'vue';
-import { useRoute } from 'vue-router';
-import { useInfiniteLoader } from '@/composables/useInfiniteLoader';
-import { useScrollMonitor } from '@/composables/useScrollMonitor';
-import { useDomain } from '@/composables/useDomain';
-import { useApolloQuery } from '@/composables/useApolloQuery';
-import { PROPOSALS_QUERY } from '@/helpers/queries';
-import { useProfiles } from '@/composables/useProfiles';
+import { computed } from 'vue';
 import { useApp } from '@/composables/useApp';
-import { useUnseenProposals } from '@/composables/useUnseenProposals';
-import { lsSet } from '@/helpers/utils';
-import { useWeb3 } from '@/composables/useWeb3';
+import { useRoute } from 'vue-router';
+import { useDomain } from '@/composables/useDomain';
+import aliases from '@/../snapshot-spaces/spaces/aliases.json';
+import { useRouter } from 'vue-router';
 
 const route = useRoute();
+const router = useRouter();
 const { domain } = useDomain();
 const { spaces } = useApp();
-const { lastSeenProposals, updateLastSeenProposal } = useUnseenProposals();
-const { web3 } = useWeb3();
+const aliasedSpace = aliases[domain] || aliases[route.params.key];
 
-const spaceId = domain || route.params.key;
-
-const loading = ref(false);
-const proposals = ref([]);
-const filterBy = ref('all');
-
-const space = computed(() => spaces.value[spaceId]);
-const spaceMembers = computed(() =>
-  space.value.members.length < 1 ? ['none'] : space.value.members
-);
-const web3Account = computed(() => web3.value.account);
-
-const { loadBy, limit, loadingMore, stopLoadingMore, loadMore } =
-  useInfiniteLoader();
-
-const { endElement } = useScrollMonitor(() =>
-  loadMore(() => loadProposals(limit.value), loading.value)
-);
-
-const { apolloQuery } = useApolloQuery();
-async function loadProposals(skip = 0) {
-  const proposalsObj = await apolloQuery(
-    {
-      query: PROPOSALS_QUERY,
-      variables: {
-        first: loadBy,
-        skip,
-        space: spaceId,
-        state: filterBy.value === 'core' ? 'all' : filterBy.value,
-        author_in: filterBy.value === 'core' ? spaceMembers.value : []
-      }
-    },
-    'proposals'
+// Redirect the user to the ENS address if the space is aliased.
+if (aliasedSpace) {
+  const updatedPath = route.path.replace(
+    domain || route.params.key,
+    aliasedSpace
   );
-  stopLoadingMore.value = proposalsObj?.length < loadBy;
-  proposals.value = proposals.value.concat(proposalsObj);
+  router.replace({ path: updatedPath });
 }
 
-onMounted(load());
+const spaceId = computed(() => aliasedSpace || domain || route.params.key);
+const space = computed(() => spaces.value[spaceId.value]);
 
-async function load() {
-  loading.value = true;
-  await loadProposals();
-  loading.value = false;
-}
-
-function selectState(e) {
-  filterBy.value = e;
-  proposals.value = [];
-  limit.value = loadBy;
-  load();
-}
-
-const { profiles, addressArray } = useProfiles();
-
-watch(proposals, () => {
-  addressArray.value = proposals.value.map(proposal => proposal.author);
-});
-
-watch([proposals, web3Account], () => {
-  if (web3Account.value) {
-    lsSet(
-      `lastSeenProposals.${web3Account.value.slice(0, 8).toLowerCase()}`,
-      Object.assign(lastSeenProposals.value, {
-        [spaceId]: new Date().getTime()
-      })
-    );
-  }
-  updateLastSeenProposal(web3Account.value);
-});
+const from = computed(() => route.params.from);
+const spaceFrom = computed(() => spaces.value[from.value]);
 </script>
 
 <template>
-  <Layout>
-    <template #sidebar-left>
-      <BlockSpace :space="space" />
-    </template>
-    <template #content-right>
-      <div class="px-4 md:px-0 mb-3 flex">
-        <div class="flex-auto">
-          <div v-text="space.name" />
-          <div class="flex items-center flex-auto">
-            <h2>{{ $t('proposals.header') }}</h2>
-          </div>
-        </div>
-        <UiDropdown
-          top="3.5rem"
-          right="1.25rem"
-          @select="selectState"
-          :items="[
-            { text: $t('proposals.states.all'), action: 'all' },
-            { text: $t('proposals.states.active'), action: 'active' },
-            { text: $t('proposals.states.pending'), action: 'pending' },
-            { text: $t('proposals.states.closed'), action: 'closed' },
-            { text: $t('proposals.states.core'), action: 'core' }
-          ]"
-        >
-          <UiButton class="pr-3">
-            {{ $t(`proposals.states.${filterBy}`) }}
-            <Icon size="14" name="arrow-down" class="mt-1 mr-1" />
-          </UiButton>
-        </UiDropdown>
-      </div>
-
-      <Block v-if="loading" :slim="true">
-        <RowLoading class="my-2" />
-      </Block>
-
-      <NoResults :block="true" v-else-if="proposals.length < 1" />
-      <div v-else>
-        <Block :slim="true" v-for="(proposal, i) in proposals" :key="i">
-          <TimelineProposal :proposal="proposal" :profiles="profiles" />
-        </Block>
-      </div>
-      <div
-        style="height: 10px; width: 10px; position: absolute"
-        ref="endElement"
-      />
-      <Block v-if="loadingMore && !loading" :slim="true">
-        <RowLoading class="my-2" />
-      </Block>
-    </template>
-  </Layout>
+  <router-view
+    :spaceId="spaceId"
+    :space="space"
+    :from="from"
+    :spaceFrom="spaceFrom"
+  ></router-view>
 </template>
