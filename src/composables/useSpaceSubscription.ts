9db6a002ca35ddbdbea066544d0e814ec07d5716
midgerate <midgerate@gmail.com>
2021-10-11 15:44:33 +0530

    fix(): load subscriptions only once

--- src/composables/useSpaceSubscription.ts
+++ src/composables/useSpaceSubscription.ts
@@ -5,44 +5,48 @@ import { SUBSCRIPTIONS_QUERY } from '@/helpers/queries';
 import { useAliasAction } from '@/composables/useAliasAction';
 import client from '@/helpers/EIP712';
 
+const subscriptions = ref<any[] | undefined>(undefined);
+
 export function useSpaceSubscription(spaceId: any) {
   const { web3 } = useWeb3();
   const { apolloQuery } = useApolloQuery();
   const { setAlias, aliasWallet, isValidAlias, checkAlias } = useAliasAction();
 
-  const isSubscribed = ref(false);
   const loading = ref(false);
   const web3Account = computed(() => web3.value.account);
+  const isSubscribed = computed(() => {
+    return subscriptions.value?.some((subscription: any) => {
+      return (
+        subscription.space.id === spaceId &&
+        subscription.address === web3Account.value
+      );
+    });
+  });
 
-  async function loadSubscriptions() {
+  async function loadSubscriptions(force = false) {
     if (!web3Account.value) return;
 
     loading.value = true;
     try {
-      const subscriptions = await apolloQuery(
-        {
-          query: SUBSCRIPTIONS_QUERY,
-          variables: {
-            address: web3Account.value,
-            space: spaceId
-          }
-        },
-        'subscriptions'
-      );
-
-      if (subscriptions) {
-        isSubscribed.value = subscriptions.some((subscription: any) => {
-          return (
-            subscription.space.id === spaceId &&
-            subscription.address === web3Account.value
-          );
-        });
-      } else {
-        isSubscribed.value = false;
+      if (subscriptions.value === undefined || force) {
+        const spaceSubscriptions = await apolloQuery(
+          {
+            query: SUBSCRIPTIONS_QUERY,
+            variables: {
+              address: web3Account.value
+            }
+          },
+          'subscriptions'
+        );
+        if (spaceSubscriptions) {
+          subscriptions.value = spaceSubscriptions;
+        } else {
+          subscriptions.value = undefined;
+        }
       }
     } catch (e) {
       console.error(e);
-      isSubscribed.value = false;
+      subscriptions.value = undefined;
     } finally {
       loading.value = false;
     }
@@ -71,7 +75,7 @@ export function useSpaceSubscription(spaceId: any) {
           space: spaceId
         });
       }
-      await loadSubscriptions();
+      await loadSubscriptions(true);
     } catch (e) {
       console.error(e);
     } finally {
