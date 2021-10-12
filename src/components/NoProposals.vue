361230b6fc4a13698e499d3f554592b4a005d6eb
Alan Nabiev <85078800+AlanNabiev@users.noreply.github.com>
2021-10-12 22:15:28 +0300

    feat: NoProposals component

---
+++ src/components/NoProposals.vue
@@ -0,0 +1,15 @@
+<script setup>
+  defineProps(['space']);
+</script>
+<template>
+  <div class="mb-3 text-center">
+    <Block class="pt-1">
+      <p>{{ $t('createFirstProposal') }}</p>
+      <router-link :to="{ name: 'spaceCreate', params: { key: space.id } }">
+        <UiButton>
+          {{ $t('proposals.createProposal') }}
+        </UiButton>
+      </router-link>
+    </Block>
+  </div>
+</template>
