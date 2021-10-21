76ce3cd6c344bb460f8f4ce0665f5a35d8812ee1
midgerate <midgerate@gmail.com>
2021-10-21 11:53:27 +0530

    feat(): add pusher beams push notification integration

--- src/composables/useSpaceSubscription.ts
+++ src/composables/useSpaceSubscription.ts
@@ -4,6 +4,9 @@ import { useApolloQuery } from '@/composables/useApolloQuery';
 import { SUBSCRIPTIONS_QUERY } from '@/helpers/queries';
 import { useAliasAction } from '@/composables/useAliasAction';
 import client from '@/helpers/EIP712';
+import { beams } from '../helpers/beams';
+import { useNotifications } from './useNotifications';
+import { useI18n } from 'vue-i18n';
 
 const subscriptions = ref<any[] | undefined>(undefined);
 
@@ -11,16 +14,19 @@ export function useSpaceSubscription(spaceId: any) {
   const { web3 } = useWeb3();
   const { apolloQuery } = useApolloQuery();
   const { setAlias, aliasWallet, isValidAlias, checkAlias } = useAliasAction();
-
+  const { notify } = useNotifications();
+  const { t } = useI18n();
   const loading = ref(false);
   const web3Account = computed(() => web3.value.account);
   const isSubscribed = computed(() => {
-    return subscriptions.value?.some((subscription: any) => {
-      return (
-        subscription.space.id === spaceId &&
-        subscription.address === web3Account.value
-      );
-    });
+    return (
+      subscriptions.value?.some((subscription: any) => {
+        return (
+          subscription.space.id === spaceId &&
+          subscription.address === web3Account.value
+        );
+      }) ?? false
+    );
   });
 
   async function loadSubscriptions() {
@@ -49,6 +55,40 @@ export function useSpaceSubscription(spaceId: any) {
       loading.value = false;
     }
   }
+
+  const checkBrowserNotification = () => {
+    if (Notification.permission === 'denied') {
+      Notification.requestPermission().then(function (permission) {
+        if (permission === 'granted') {
+          return true;
+        }
+      });
+      return false;
+    } else if (Notification.permission === 'granted') {
+      return true;
+    } else {
+      Notification.requestPermission().then(function (permission) {
+        if (permission === 'granted') {
+          return true;
+        }
+      });
+      return false;
+    }
+  };
+
+  const configurePush = async () => {
+    try {
+      const isNotificationsAllowed = checkBrowserNotification();
+      if (isNotificationsAllowed) {
+        await beams.start();
+        await beams.addDeviceInterest(web3Account.value);
+      } else {
+        notify(['red', t('pleaseEnableBrowserNotifications')]);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   async function toggleSubscription() {
     if (web3.value.authLoading) {
@@ -81,16 +121,12 @@ export function useSpaceSubscription(spaceId: any) {
     }
   }
 
-  // load subscriptions when the hook is loaded on the page.
-  watch(web3Account, () => {
-    if (subscriptions.value === undefined) {
-      loadSubscriptions();
-    }
-  });
-
   return {
     toggleSubscription,
     loading,
-    isSubscribed
+    isSubscribed,
+    subscriptions,
+    loadSubscriptions,
+    configurePush
   };
 }
