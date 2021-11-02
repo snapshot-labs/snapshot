a4ad2b622b8a54069309fdbde53fc6b74f09ec20
mktcode <kontakt@markus-kottlaender.de>
2021-11-02 19:23:52 +0100

    prevent notification area from blocking ui

--- src/components/Notifications.vue
+++ src/components/Notifications.vue
@@ -5,9 +5,22 @@ const { items } = useNotifications();
 </script>
 
 <template>
-  <div class="fixed left-0 right-0 bottom-0 text-center" style="z-index: 99999">
-    <transition-group name="fade" tag="div" class="mb-4">
-      <div v-for="item in items" :key="item.id" class="mb-2">
+  <div
+    class="
+      fixed
+      left-0
+      right-0
+      bottom-0
+      z-50
+      flex flex-col
+      items-center
+      mb-4
+      space-y-2
+      pointer-events-none
+    "
+  >
+    <transition-group name="fade">
+      <div v-for="item in items" :key="item.id" class="pointer-events-auto">
         <UiButton
           class="notification inline-block !border-none !bg-red"
           :class="`!bg-${item.type}`"
