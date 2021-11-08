a1cb640433c8138526d950fde11754e01937af6e
mktcode <kontakt@markus-kottlaender.de>
2021-11-08 23:30:03 +0100

    removed padding/border in plugin config modal

--- src/components/Modal/Plugins.vue
+++ src/components/Modal/Plugins.vue
@@ -75,7 +75,7 @@ watch(
     <div class="mt-4 mx-0 md:mx-4">
       <div
         v-if="selectedPlugin?.key"
-        class="p-4 mb-4 border rounded-md link-color"
+        class="mb-4 link-color"
       >
         <h4 v-text="selectedPlugin.name" class="mb-3 text-center" />
         <UiButton
