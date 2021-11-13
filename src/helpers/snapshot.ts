82e9e41a32c7facfa7ca1dd073a71bdd9f954259
Keishi Shimmachi <42565705+aiinkiestism@users.noreply.github.com>
2021-11-13 17:11:17 +0900

    Move helpers voting to snapshot.js (#984)
    * Move helpers voting to snapshot.js
    
    * Remove unnecessary import
    
    * Update utils.ts
    
    Co-authored-by: Fabien <bonustrack@users.noreply.github.com>

--- src/helpers/snapshot.ts
+++ src/helpers/snapshot.ts
@@ -1,8 +1,8 @@
 import { getScores } from '@snapshot-labs/snapshot.js/src/utils';
+import voting from '@snapshot-labs/snapshot.js/src/voting';
 import { apolloClient } from '@/helpers/apollo';
 import { PROPOSAL_QUERY, VOTES_QUERY } from '@/helpers/queries';
 import cloneDeep from 'lodash/cloneDeep';
-import voting from '@/helpers/voting';
 
 export async function getProposalVotes(proposalId: string) {
   try {
