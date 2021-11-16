b9d7ba0b5a513d72908fd57916da4621eb807f14
bonustrack <fabien@bonustrack.co>
2021-11-16 19:11:15 +0700

    Load max 100 votes on final proposal

--- src/helpers/queries.ts
+++ src/helpers/queries.ts
@@ -1,9 +1,14 @@
 import gql from 'graphql-tag';
 
 export const VOTES_QUERY = gql`
-  query Votes($id: String!, $orderBy: String, $orderDirection: OrderDirection) {
+  query Votes(
+    $id: String!
+    $first: Int
+    $orderBy: String
+    $orderDirection: OrderDirection
+  ) {
     votes(
-      first: 20000
+      first: $first
       where: { proposal: $id, vp_gt: 0 }
       orderBy: $orderBy
       orderDirection: $orderDirection
