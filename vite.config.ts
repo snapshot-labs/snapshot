1491f3087264b4542bff3648708b5f0a35a92445
mktcode <kontakt@markus-kottlaender.de>
2021-11-20 00:06:51 +0100

    support base bath env var

--- vite.config.ts
+++ vite.config.ts
@@ -1,35 +1,40 @@
 import path from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import ViteComponents from 'unplugin-vue-components/vite';
 import visualizer from 'rollup-plugin-visualizer';
 
-export default defineConfig({
-  define: {
-    'process.env': process.env
-  },
-  plugins: [
-    vue({
-      script: {
-        refSugar: true
-      }
-    }),
-    ViteComponents({ directoryAsNamespace: true }),
-    visualizer({
-      filename: './dist/stats.html',
-      template: 'sunburst',
-      gzipSize: true
-    })
-  ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
+export default defineConfig(({ mode }) => {
+  Object.assign(process.env, loadEnv(mode, process.cwd()));
+  
+  return {
+    base: process.env.VITE_BASE_PATH || '/',
+    define: {
+      'process.env': process.env
     },
-    dedupe: ['@popperjs/core']
-  },
-  optimizeDeps: {
-    include: ['color'],
-    // @ts-ignore
-    allowNodeBuiltins: ['stream']
+    plugins: [
+      vue({
+        script: {
+          refSugar: true
+        }
+      }),
+      ViteComponents({ directoryAsNamespace: true }),
+      visualizer({
+        filename: './dist/stats.html',
+        template: 'sunburst',
+        gzipSize: true
+      })
+    ],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src')
+      },
+      dedupe: ['@popperjs/core']
+    },
+    optimizeDeps: {
+      include: ['color'],
+      // @ts-ignore
+      allowNodeBuiltins: ['stream']
+    }
   }
 });
