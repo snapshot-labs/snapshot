9130fe6f05401862f224e4dde4ca1293cbfef919
mktcode <kontakt@markus-kottlaender.de>
2021-11-22 12:33:08 +0100

    fix registering watcher twice

--- src/composables/useProfiles.ts
+++ src/composables/useProfiles.ts
@@ -4,6 +4,23 @@ import { ref, computed, watch } from 'vue';
 const profiles = ref({});
 const addressArray = ref<string[]>([]);
 
+const filteredArray = computed(() =>
+  addressArray.value.filter(address => {
+    return !Object.keys(profiles.value).includes(address);
+  })
+);
+
+watch(addressArray, async () => {
+  console.time('getProfiles');
+  const response =
+    filteredArray.value.length > 0
+      ? await getProfiles(filteredArray.value)
+      : {};
+  console.timeEnd('getProfiles');
+
+  profiles.value = { ...profiles.value, ...response };
+});
+
 export function useProfiles() {
   const updateAddressArray = (addresses: string[]) => {
     const addressesToAdd = addresses.filter(
@@ -12,23 +29,6 @@ export function useProfiles() {
 
     addressArray.value = addressArray.value.concat(addressesToAdd);
   };
-
-  const filteredArray = computed(() =>
-    addressArray.value.filter(address => {
-      return !Object.keys(profiles.value).includes(address);
-    })
-  );
-
-  watch(addressArray, async () => {
-    console.time('getProfiles');
-    const response =
-      filteredArray.value.length > 0
-        ? await getProfiles(filteredArray.value)
-        : {};
-    console.timeEnd('getProfiles');
-
-    profiles.value = { ...profiles.value, ...response };
-  });
 
   return {
     profiles,
