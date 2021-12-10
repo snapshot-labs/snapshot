0789a2980927cf702f2a845b9cedba51b5c3fe56
bigint <bigint@icloud.com>
2021-12-10 13:42:41 +0530

    chore: use space instead of margin for spacing (#1208)

--- src/components/Modal/Receipt.vue
+++ src/components/Modal/Receipt.vue
@@ -10,23 +10,27 @@ export default {
     <template v-slot:header>
       <h3>{{ $t('receipt') }}</h3>
     </template>
-    <div class="m-4 mb-0 p-4 border rounded-md link-color">
-      <div class="flex">
-        <span v-text="$t('author')" class="flex-auto text-color mr-1" />
-        <a :href="_getUrl(authorIpfsHash)" target="_blank" class="link-color">
-          #{{ authorIpfsHash.slice(0, 7) }}
-          <Icon name="external-link" class="ml-1" />
-        </a>
+    <div class="m-4 space-y-4">
+      <div class="p-4 border rounded-md link-color">
+        <div class="flex">
+          <span v-text="$t('author')" class="flex-auto text-color mr-1" />
+          <a :href="_getUrl(authorIpfsHash)" target="_blank" class="link-color">
+            #{{ authorIpfsHash.slice(0, 7) }}
+            <Icon name="external-link" class="ml-1" />
+          </a>
+        </div>
+        <div v-if="relayerIpfsHash" class="flex">
+          <span v-text="$t('relayer')" class="flex-auto text-color mr-1" />
+          <a
+            :href="_getUrl(relayerIpfsHash)"
+            target="_blank"
+            class="link-color"
+          >
+            #{{ relayerIpfsHash.slice(0, 7) }}
+            <Icon name="external-link" class="ml-1" />
+          </a>
+        </div>
       </div>
-      <div v-if="relayerIpfsHash" class="flex">
-        <span v-text="$t('relayer')" class="flex-auto text-color mr-1" />
-        <a :href="_getUrl(relayerIpfsHash)" target="_blank" class="link-color">
-          #{{ relayerIpfsHash.slice(0, 7) }}
-          <Icon name="external-link" class="ml-1" />
-        </a>
-      </div>
-    </div>
-    <div class="m-4">
       <a
         :href="`https://signator.io/view?ipfs=${authorIpfsHash}`"
         target="_blank"
