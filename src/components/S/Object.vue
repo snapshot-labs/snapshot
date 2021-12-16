4f23fe80cba9b425c96c1503344387cb07eaa20c
Sam <samuv.eth@gmail.com>
2021-12-16 18:16:45 +0700

    Import form components dynamically

--- src/components/S/Object.vue
+++ src/components/S/Object.vue
@@ -1,5 +1,7 @@
 <script setup>
 import { onMounted, ref, watch } from 'vue';
+import { defineAsyncComponent } from 'vue';
+import capitalize from 'lodash/capitalize';
 
 const props = defineProps({
   modelValue: Object,
@@ -10,6 +12,10 @@ const emit = defineEmits(['update:modelValue']);
 
 const input = ref(props.modelValue || props.definition.default || {});
 
+const getComponent = name => {
+  return defineAsyncComponent(() => import(`./${capitalize(name)}.vue`));
+};
+
 onMounted(() => emit('update:modelValue', input.value));
 
 watch(input, () => emit('update:modelValue', input.value));
@@ -17,30 +23,10 @@ watch(input, () => emit('update:modelValue', input.value));
 
 <template>
   <div v-for="(property, i) in definition.properties" :key="i">
-    <SObject
-      v-if="property.type === 'object'"
+    <component
+      :is="getComponent(property.type)"
+      :v-model="input[i]"
       :definition="property"
-      v-model="input[i]"
-    />
-    <SArray
-      v-if="property.type === 'array'"
-      :definition="property"
-      v-model="input[i]"
-    />
-    <SString
-      v-if="property.type === 'string'"
-      :definition="property"
-      v-model="input[i]"
-    />
-    <SNumber
-      v-if="property.type === 'number'"
-      :definition="property"
-      v-model="input[i]"
-    />
-    <SBoolean
-      v-if="property.type === 'boolean'"
-      :definition="property"
-      v-model="input[i]"
     />
   </div>
 </template>
