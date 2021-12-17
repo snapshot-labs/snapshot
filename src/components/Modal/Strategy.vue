527fa904cf1dd55bcbea5cc44f1930f5059caaab
Sam <51686767+samuveth@users.noreply.github.com>
2021-12-18 04:19:22 +0700

    Strategy form (#1273)
    * Add strategy generated form
    
    * Add TextareaJson and refactor
    
    * Update Number.vue
    
    * Add focus border and refactor
    
    * Refactor plugins and validation using TextareaJson
    
    * Fix single root node warning
    
    * Import form components dynamically
    
    * Fix input glitches
    
    Co-authored-by: bonustrack <fabien@bonustrack.co>
    Co-authored-by: Fabien <bonustrack@users.noreply.github.com>

--- src/components/Modal/Strategy.vue
+++ src/components/Modal/Strategy.vue
@@ -2,6 +2,7 @@
 import { ref, computed, toRefs, watch } from 'vue';
 import { useSearchFilters } from '@/composables/useSearchFilters';
 import { clone } from '@snapshot-labs/snapshot.js/src/utils';
+import { useApp } from '../../composables/useApp';
 
 const defaultParams = {
   symbol: 'DAI',
@@ -15,48 +16,42 @@ const emit = defineEmits(['add', 'close']);
 
 const { open } = toRefs(props);
 const searchInput = ref('');
+const isValid = ref(true);
 const input = ref({
   name: '',
-  params: JSON.stringify(defaultParams, null, 2)
+  params: {}
 });
 
-const isValid = computed(() => {
-  try {
-    const params = JSON.parse(input.value.params);
-    return !!params.symbol;
-  } catch (e) {
-    return false;
-  }
+const { strategies } = useApp();
+
+const definition = computed(() => {
+  return input.value.name && strategies.value[input.value.name].schema?.$ref
+    ? strategies.value[input.value.name].schema.definitions.Strategy
+    : false;
 });
 
 const { filteredStrategies } = useSearchFilters();
-const strategies = computed(() => filteredStrategies(searchInput.value));
+const strategiesResults = computed(() => filteredStrategies(searchInput.value));
 
 function handleSubmit() {
   const strategyObj = clone(input.value);
-  strategyObj.params = JSON.parse(strategyObj.params);
   emit('add', strategyObj);
   emit('close');
 }
 
 function handleClick(strategy) {
   const params = strategy.examples[0]?.strategy?.params || defaultParams;
-
-  input.value = {
-    name: strategy.key,
-    params: JSON.stringify(params, null, 2)
-  };
+  input.value.name = strategy.key;
+  input.value.params = definition.value ? {} : params;
 }
 
 watch(open, () => {
   if (props.strategy?.name) {
-    const strategyObj = props.strategy;
-    strategyObj.params = JSON.stringify(strategyObj.params, null, 2);
     input.value = props.strategy;
   } else {
     input.value = {
       name: '',
-      params: JSON.stringify(defaultParams, null, 2)
+      params: defaultParams
     };
   }
 });
@@ -65,9 +60,7 @@ watch(open, () => {
 <template>
   <UiModal :open="open" @close="$emit('close')">
     <template v-slot:header>
-      <h3>
-        {{ strategy.name ? $t('editStrategy') : $t('settings.addStrategy') }}
-      </h3>
+      <h3 v-text="input.name ? input.name : $t('settings.addStrategy')" />
     </template>
     <Search
       v-if="!strategy.name && !input.name"
@@ -75,39 +68,45 @@ watch(open, () => {
       :placeholder="$t('searchPlaceholder')"
       :modal="true"
     />
-    <div class="mt-4 mx-0 md:mx-4">
-      <div v-if="input.name" class="p-4 mb-4 border rounded-md link-color">
-        <h4 v-text="input.name" class="mb-3 text-center" />
-        <UiButton
-          class="block w-full mb-3 overflow-x-auto"
-          style="height: auto"
-        >
-          <TextareaAutosize
-            v-model="input.params"
-            :placeholder="$t('strategyParameters')"
-            class="input text-left no-scrollbar"
-            style="width: 560px"
-          />
-        </UiButton>
-        <UiButton
-          @click="handleSubmit"
-          :disabled="!isValid"
-          class="w-full"
-          primary
-        >
-          {{ strategy.name ? $t('save') : $t('add') }}
-        </UiButton>
-      </div>
-      <div v-if="!input.name">
-        <a
-          v-for="strategy in strategies"
-          :key="strategy.key"
-          @click="handleClick(strategy)"
-        >
-          <BlockStrategy :strategy="strategy" />
-        </a>
-        <NoResults v-if="Object.keys(strategies).length < 1" />
-      </div>
+    <div v-if="input.name" class="m-4">
+      <SDefaultObject
+        v-if="definition"
+        v-model="input.params"
+        :definition="definition"
+      />
+      <UiButton
+        v-else
+        class="block w-full mb-3 overflow-x-auto"
+        style="height: auto"
+      >
+        <TextareaJson
+          v-model="input.params"
+          v-model:is-valid="isValid"
+          :placeholder="$t('strategyParameters')"
+          class="input text-left no-scrollbar"
+          style="width: 560px"
+        />
+      </UiButton>
     </div>
+    <div v-else class="my-4 mx-0 md:mx-4">
+      <a
+        v-for="strategy in strategiesResults"
+        :key="strategy.key"
+        @click="handleClick(strategy)"
+      >
+        <BlockStrategy :strategy="strategy" />
+      </a>
+      <NoResults v-if="Object.keys(strategiesResults).length < 1" />
+    </div>
+    <template v-if="input.name" v-slot:footer>
+      <UiButton
+        @click="handleSubmit"
+        :disabled="!isValid || !input.params.symbol"
+        class="w-full"
+        primary
+      >
+        {{ strategy.name ? $t('save') : $t('add') }}
+      </UiButton>
+    </template>
   </UiModal>
 </template>
