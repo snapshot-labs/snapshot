28470652e2462c82e29fe358ca73c17b01bd0a67
mkt <kontakt@markus-kottlaender.de>
2021-12-22 22:20:52 +0100

    Feature/suggest ens domain (#1281)
    * feat: bump snapshot.js version
    
    * feat[wip]: fetch ENS names by account
    
    * fix: layout & add loading
    
    * Update yarn.lock
    
    * feat: resolve conflicts
    
    * lowercase addresses in ens subgraph query
    
    * watch connected web3 account to update ens names
    
    * load ENS names initially
    
    not only when web3Account changes
    
    * refactoring/redesign
    
    * linting/cleanup
    
    * lowerCase entered ens domain and remove whitespace
    
    * added border around content
    
    * style adjustments for domain buttons
    
    * removed "waiting for new registrations" from UI
    
    * removed tld dropdown
    
    * Update src/locales/default.json
    
    Co-authored-by: Sam <51686767+samuveth@users.noreply.github.com>
    
    * rephrased note about being connected to correct account
    
    * removed unused var
    
    * moved code from useEns composable to component
    (composable won't be reused anyway)
    
    * added comment
    
    * adjusted spacing
    
    * avoid loading animation when periodically checking for new domains
    
    Co-authored-by: sarmat <skhachirov12@gmail.com>
    Co-authored-by: Chaitanya <yourchaitu@gmail.com>
    Co-authored-by: Fabien <bonustrack@users.noreply.github.com>
    Co-authored-by: Sam <51686767+samuveth@users.noreply.github.com>

---
+++ src/components/RegisterENS.vue
@@ -0,0 +1,49 @@
+<script setup>
+/**
+ * An input with ENS TLD validation and a register button, forwarding to app.ens.domains.
+ * Emits waitForRegistration event when Register button is clicked, to trigger domain lookup in background.
+ */
+
+import { computed } from 'vue';
+
+const props = defineProps({
+  modelValue: String,
+  validTlds: Array
+});
+defineEmits(['update:modelValue', 'waitForRegistration']);
+
+const isValidDomain = computed(() => {
+  if (!props.modelValue.includes('.')) return false;
+
+  return props.validTlds.includes(props.modelValue.split('.').pop());
+});
+</script>
+
+<template>
+  <UiButton class="text-left w-full mb-1 flex px-3">
+    <input
+      :value="modelValue"
+      @input="$emit('update:modelValue', $event.target.value.toLowerCase().replaceAll(/\s/g, ''))"
+      class="input flex-auto"
+      :placeholder="$t('setup.example')"
+    />
+    <span
+      class="block py-1 -mr-2"
+      target="_blank"
+      v-tippy="{
+        content: `${$t('setup.supportedEnsTLDs')}: ${validTlds.join(', ')}`
+      }"
+    >
+      <Icon name="info" size="24" class="text-color p-1" />
+    </span>
+  </UiButton>
+  <a
+    :href="`https://app.ens.domains/name/${modelValue}/register`"
+    @click="$emit('waitForRegistration')"
+    target="_blank"
+  >
+    <UiButton :disabled="!isValidDomain" class="w-full mt-2" primary>
+      {{ $t('setup.registerEnsButton') }}
+    </UiButton>
+  </a>
+</template>
