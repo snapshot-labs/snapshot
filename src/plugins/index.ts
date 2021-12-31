f1ec15702edae07a6327838c7136d469965ef2d1
mktcode <kontakt@markus-kottlaender.de>
2021-12-31 19:58:22 +0100

    add plugin index file

---
+++ src/plugins/index.ts
@@ -0,0 +1,15 @@
+export default {
+  Chainlink: {
+    name: "Chainlink Result Oracle",
+    website: 'https://...',
+    icon: 'https://...',
+    defaults: {
+      space: {
+        registry: '0x123'
+      },
+      proposal: {
+        oracles: ['0xAbc']
+      }
+    }
+  }
+}
