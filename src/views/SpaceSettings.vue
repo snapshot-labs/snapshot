c9e93791278398cc84d991a1519fac167fa83f25
mkt <kontakt@markus-kottlaender.de>
2022-01-03 00:30:37 +0100

    just don't update title

--- src/views/SpaceSettings.vue
+++ src/views/SpaceSettings.vue
@@ -272,7 +272,9 @@ watchEffect(async () => {
 });
 
 onMounted(() => {
-  setPageTitle('page.title.space.settings', { space: props.space?.name || props.spaceId });
+  if (props.space) {
+    setPageTitle('page.title.space.settings', { space: props.space.name });
+  }
 });
 </script>
 
