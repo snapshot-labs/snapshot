9ba60dc838afc9dfe47e0b903512edaff4b76542
mktcode <kontakt@markus-kottlaender.de>
2022-01-04 23:23:08 +0100

    code formatting

--- src/plugins/safeSnap/Proposal.vue
+++ src/plugins/safeSnap/Proposal.vue
@@ -9,25 +9,25 @@ const props = defineProps({
 
 const safeSnapInput = computed(() => {
   const input = props.proposal.plugins.safeSnap || {};
-    if (!input.safes && input.txs) {
-      // map legacy format to new format
-      return {
-        ...input,
-        safes: [
-          {
-            network: props.space.network,
-            realityAddress: props.space.plugins?.safeSnap?.address,
-            // Some legacy proposals have a plain array of transactions instead of the current two-dimensional structure for batches.
-            txs:
-              input.txs[0] && !Array.isArray(input.txs[0])
-                ? [input.txs]
-                : input.txs
-          }
-        ]
-      };
-    }
+  if (!input.safes && input.txs) {
+    // map legacy format to new format
+    return {
+      ...input,
+      safes: [
+        {
+          network: props.space.network,
+          realityAddress: props.space.plugins?.safeSnap?.address,
+          // Some legacy proposals have a plain array of transactions instead of the current two-dimensional structure for batches.
+          txs:
+            input.txs[0] && !Array.isArray(input.txs[0])
+              ? [input.txs]
+              : input.txs
+        }
+      ]
+    };
+  }
 
-    return input;
+  return input;
 });
 </script>
 
