30ab2b0d2ab4dfb342a387b674c6da6a1f76b38e
mktcode <kontakt@markus-kottlaender.de>
2022-01-05 01:22:01 +0100

    remove unsused code

--- src/components/ProposalPluginsContent.vue
+++ src/components/ProposalPluginsContent.vue
@@ -1,41 +1,11 @@
 <script setup>
-import { computed } from 'vue';
-
-const props = defineProps({
+defineProps({
   id: String,
   space: Object,
   proposal: Object,
   votes: Object,
   loadedResults: Boolean
 });
-
-const emit = defineEmits(['update:safeSnapInput']);
-
-// Legacy structure of proposal.plugins.safeSnap (safeSnapInput):
-// safeSnap: {
-//   txs: [
-//     [batch0tx0, batch0tx1, ...],
-//     [batch1tx0, batch1tx1, ...],
-//   ],
-// }
-//
-// New structure:
-// (stores reality module contract address and network ID in the proposal to allow
-// updates of the space config without breaking existing proposals)
-// safeSnap: {
-//   safes: [
-//     {
-//       network: networkId,
-//       realityAddress: 0x...,
-//       txs: [
-//         [batch0tx0, batch0tx1, ...],
-//         [batch1tx0, batch1tx1, ...],
-//       ],
-//     },
-//     ...
-//   ]
-// }
-
 </script>
 
 <template>
