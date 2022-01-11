f59b21b72ce6824a7b9d617caddb606faf08d3fe
Sam <51686767+samuveth@users.noreply.github.com>
2022-01-11 17:54:41 +0700

    Continue Pr/1250 (#1342)
    * feat: add cursor-pointer to remove choice input for better a11y
    
    * fix: scrollbar color in dark mode
    
    * fix
    
    * Fix scroll bar not matching the skin color on first load
    
    * Update src/composables/useUserSkin.ts
    
    * Use immediate instead of additional onmounted
    
    * Remove onmounted
    
    * Undo hub url changes
    
    * Update src/composables/useUserSkin.ts
    
    * Update src/composables/useUserSkin.ts
    
    * Remove redundand classes
    
    * Remove duplicate id "app"
    
    * Add viewheight screen
    
    * Fix to min-height
    
    Co-authored-by: Bigint <bigint@icloud.com>
    Co-authored-by: mkt <kontakt@markus-kottlaender.de>

--- src/App.vue
+++ src/App.vue
@@ -49,7 +49,10 @@ watch(modalOpen, val => {
 </script>
 
 <template>
-  <div :class="skin" id="app" class="overflow-hidden pb-6 font-serif text-base">
+  <div
+    :class="skin"
+    class="overflow-hidden pb-6 font-serif text-base min-h-screen"
+  >
     <UiLoading v-if="app.loading || !app.init" class="overlay big" />
     <div v-else>
       <Scroller />
