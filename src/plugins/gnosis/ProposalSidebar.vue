f7af0b64248bcb653cc244ab179a8b9600080ced
mktcode <kontakt@markus-kottlaender.de>
2022-01-28 21:33:43 +0100

    show gnosis impact block only if configured

--- src/plugins/gnosis/ProposalSidebar.vue
+++ src/plugins/gnosis/ProposalSidebar.vue
@@ -7,9 +7,11 @@ defineProps({ proposal: Object });
 </script>
 
 <template>
-  <Block :title="pluginIndex.gnosis.name">
+  <Block
+    v-if="proposal.plugins?.gnosis?.baseTokenAddress"
+    :title="pluginIndex.gnosis.name"
+  >
     <PluginGnosisCustomBlock
-      v-if="proposal.plugins?.gnosis?.baseTokenAddress"
       :proposalConfig="proposal.plugins.gnosis"
       :choices="proposal.choices"
     />
