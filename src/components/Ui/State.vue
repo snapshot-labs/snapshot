0d4d45f6c07adb0628c74a872e25e4d8235acd19
bigint <bigint@icloud.com>
2022-01-29 10:46:10 +0530

    chore: upgrade tailwindcss to v3 (#1540)
    * chore: update tailwind to v3
    
    * chore: update purple to violet
    
    * Update components.d.ts
    
    * chore: remove @variants
    
    * Remove redundant responsive directive
    
    Co-authored-by: Sam <51686767+samuveth@users.noreply.github.com>

--- src/components/Ui/State.vue
+++ src/components/Ui/State.vue
@@ -7,7 +7,7 @@ const props = defineProps({
 });
 
 const stateClass = computed(() => {
-  if (props.state === 'closed') return 'bg-purple-600';
+  if (props.state === 'closed') return 'bg-violet-600';
   if (props.state === 'active') return 'bg-green';
   return 'bg-gray-500';
 });
