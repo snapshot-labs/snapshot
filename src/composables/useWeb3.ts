119f4e125bd0f8612ef0800c0cb3c65b9ad78870
mkt <kontakt@markus-kottlaender.de>
2022-01-31 10:45:48 +0100

    remove unnecessary export
    Co-authored-by: Sam <51686767+samuveth@users.noreply.github.com>

--- src/composables/useWeb3.ts
+++ src/composables/useWeb3.ts
@@ -109,7 +109,6 @@ export function useWeb3() {
     logout,
     loadProvider,
     handleChainChanged,
-    getAuth: getInstance,
     web3: computed(() => state),
     web3Account: computed(() => state.account)
   };
