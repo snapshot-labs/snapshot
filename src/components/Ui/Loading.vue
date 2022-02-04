590d376097edf9ff3990e41d5b1b79210dcaae47
Sam <51686767+samuveth@users.noreply.github.com>
2022-02-04 23:25:25 +0700

    Remove voteId and voteCreated from votes query (#1621)
    * Remove voteId and voteCreated
    
    * Fix loading should be white on primary background

--- src/components/Ui/Loading.vue
+++ src/components/Ui/Loading.vue
@@ -6,6 +6,7 @@ defineProps({
   }
 });
 </script>
+
 <template>
   <span class="loading">
     <span>
@@ -16,7 +17,7 @@ defineProps({
         viewBox="0 0 50 50"
       >
         <path
-          :class="{ 'fill-white': fillWhite }"
+          :class="{ '!fill-white': fillWhite }"
           d="M25,5A20.14,20.14,0,0,1,45,22.88a2.51,2.51,0,0,0,2.49,2.26h0A2.52,2.52,0,0,0,50,22.33a25.14,25.14,0,0,0-50,0,2.52,2.52,0,0,0,2.5,2.81h0A2.51,2.51,0,0,0,5,22.88,20.14,20.14,0,0,1,25,5Z"
         >
           <animateTransform
@@ -54,9 +55,6 @@ defineProps({
 
     path {
       fill: var(--link-color);
-      &.fill-white {
-        fill: white;
-      }
     }
   }
 
