abfb38f06decf59f7d35fdcb8e9ea5d8079d842f
rootial <brightliu77@gmail.com>
2022-02-10 14:57:53 +0800

    update icon size and use language native name

--- src/components/SelectLanguageButton.vue
+++ src/components/SelectLanguageButton.vue
@@ -16,7 +16,7 @@ const modalLangOpen = ref(false);
     @click="modalLangOpen = true"
   >
     <Icon size="24" name="earth" class="hover:opacity-80 mr-2" />
-    {{ languages[$i18n.locale]?.name }}
+    {{ languages[$i18n.locale]?.nativeName ?? languages[$i18n.locale]?.name }}
   </UiButton>
   <teleport to="#modal">
     <ModalSelectLanguage :open="modalLangOpen" @close="modalLangOpen = false" />
