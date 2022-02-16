97ce6c7a0d8dde32d23ddde784ca5f3b34110009
midgerate <midgerate@gmail.com>
2022-02-16 09:57:33 +0530

    added unsupported network modal

---
+++ src/components/Modal/UnsupportedNetwork.vue
@@ -0,0 +1,26 @@
+<script setup>
+defineProps({
+  open: {
+    type: Boolean,
+    required: true
+  }
+});
+</script>
+
+<template>
+  <UiModal :open="open" @close="$emit('close')">
+    <template v-slot:header>
+      <h3>{{ $t('settings.unsupportedNetwork') }}</h3>
+    </template>
+
+    <div class="m-4">
+      Creating a space is only supported on the Ethereum mainnet. To continue,
+      please switch your wallet to the mainnet.
+    </div>
+    <template v-slot:footer>
+      <UiButton class="button-outline w-full mb-2" :primary="true">
+        Switch to mainnet
+      </UiButton>
+    </template>
+  </UiModal>
+</template>
