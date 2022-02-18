16afc8c58ab644d6b0ee4f744976e3a891ecafc8
Fabien <bonustrack@users.noreply.github.com>
2022-02-18 18:14:40 +0700

    Use external server to get avatar image (#1701)
    * Use external server to get avatar image
    
    * Add default bg on avatars
    
    * Remove blockies dep

--- src/components/Ui/Avatar.vue
+++ src/components/Ui/Avatar.vue
@@ -28,22 +28,12 @@ watch(
 <template>
   <span class="flex shrink-0 items-center justify-center">
     <img
-      v-if="showImg"
-      :src="imgUrl"
-      :style="{
-        width: `${parseInt(size) || 22}px`,
-        height: `${parseInt(size) || 22}px`
-      }"
-      :class="[
-        space?.skin ? `${space?.skin} bg-[color:var(--bg-color)]` : 'bg-white'
-      ]"
-      :alt="space?.name"
+      :src="
+        imgUrl ||
+        `https://stamp.fyi/avatar/eth:${address}?s=${parseInt(size) * 2}`
+      "
       class="rounded-full"
-    />
-    <UiBlockie
-      v-else-if="!!address"
-      :seed="address"
-      class="rounded-full"
+      :class="!space && 'bg-[color:var(--border-color)]'"
       :style="{
         width: `${parseInt(size) || 22}px`,
         height: `${parseInt(size) || 22}px`
