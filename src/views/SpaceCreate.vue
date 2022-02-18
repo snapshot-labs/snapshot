5426230d5e81cd8767a7ab934503159ad52a8832
Sam <samuv.eth@gmail.com>
2022-02-18 19:28:53 +0700

    Simplify textarea component and remove max length indicator on creat body

--- src/views/SpaceCreate.vue
+++ src/views/SpaceCreate.vue
@@ -392,19 +392,11 @@ const needsPluginConfigs = computed(() =>
               <TextareaAutosize
                 v-if="!preview"
                 v-model="form.body"
-                class="input pt-0 w-full"
+                class="input w-full"
                 style="font-size: 22px"
                 :placeholder="$t('create.content')"
                 :max-length="bodyLimit"
               />
-
-              <!-- Indicator for number of available characters in body -->
-              <div
-                class="absolute right-0 bottom-2 hidden group-focus-within:block p-1 bg-skin-bg"
-                :class="{ 'text-red': form.body.length === bodyLimit }"
-              >
-                {{ `${form.body.length} / ${bodyLimit}` }}
-              </div>
             </div>
 
             <div v-if="form.body && preview" class="mb-4">
