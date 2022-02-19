2944a9a095057a6b1211badba9019d1c8a995d2a
Sam <samuv.eth@gmail.com>
2022-02-19 20:02:21 +0700

    Add counter to router-link

--- src/components/TheSidebar.vue
+++ src/components/TheSidebar.vue
@@ -120,13 +120,12 @@ onMounted(() => {
                   symbolIndex="space"
                   size="44"
                 />
+                <UiCounter
+                  v-if="explore.spaces[element].activeProposals"
+                  :counter="explore.spaces[element].activeProposals"
+                  class="absolute -top-[1px] right-[9px] !bg-green h-[16px] leading-[17px] min-w-[16px]"
+                />
               </router-link>
-
-              <UiCounter
-                v-if="explore.spaces[element].activeProposals"
-                :counter="explore.spaces[element].activeProposals"
-                class="absolute -top-[1px] right-[9px] !bg-green h-[16px] leading-[17px] min-w-[16px]"
-              />
             </div>
           </template>
         </draggable>
