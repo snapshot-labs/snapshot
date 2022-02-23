026fd96ff4817db3f00e7fc4018a356276817a54
Sam <samuv.eth@gmail.com>
2022-02-23 18:06:39 +0700

    Refactor plugin block

--- src/components/Block/Plugin.vue
+++ src/components/Block/Plugin.vue
@@ -27,29 +27,28 @@ defineProps({
     </div>
     <div class="flex justify-between items-end text-skin-text">
       <div class="flex flex-col">
-        <a
-          :href="`https://github.com/${plugin.author}`"
-          target="_blank"
+        <BaseAnchor
           class="text-skin-text"
+          :link="`https://github.com/${plugin.author}`"
+          hide-external-icon
         >
           <Icon name="github" class="mr-1" />
           {{ plugin.author }}
-        </a>
+        </BaseAnchor>
         {{
           $tc('inSpaces', [
             formatCompactNumber(pluginsSpacesCount?.[plugin.key] ?? 0)
           ])
         }}
       </div>
-      <a
+
+      <BaseAnchor
         class="flex items-center"
         @click.stop
-        target="_blank"
-        :href="`https://github.com/snapshot-labs/snapshot-plugins/tree/master/src/plugins/${plugin.key}`"
+        :link="`https://github.com/snapshot-labs/snapshot-plugins/tree/master/src/plugins/${plugin.key}`"
       >
         {{ $t('learnMore') }}
-        <Icon size="16" name="external-link" class="text-skin-text ml-1" />
-      </a>
+      </BaseAnchor>
     </div>
   </Block>
 </template>
