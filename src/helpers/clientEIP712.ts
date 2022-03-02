ccdb8c61572d5245fff5eeec6d4855e68da6f402
mkt <kontakt@markus-kottlaender.de>
2022-03-02 14:51:20 +0100

    fix: linting/typescript issues (#1861)
    * upgrade typescript eslint plugin and parser
    
    * autofixable linting
    
    * use already existent t function in formatDuration function
    
    * removed unnecessary @ts-ignore
    
    * added param types, removed unused params/code
    
    * simplified code to avoid ts issues
    
    * fixed type

--- src/helpers/clientEIP712.ts
+++ src/helpers/clientEIP712.ts
@@ -1,6 +1,5 @@
 import Client from '@snapshot-labs/snapshot.js/src/sign';
 
-// @ts-ignore
 const hubUrl = import.meta.env.VITE_HUB_URL || 'https://testnet.snapshot.org';
 const client = new Client(hubUrl);
 
