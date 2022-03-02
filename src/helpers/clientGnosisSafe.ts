8cfc3bdcea1655c3c7ca7eea567e0b2fe114542a
mktcode <kontakt@markus-kottlaender.de>
2022-03-02 12:28:09 +0100

    removed unnecessary @ts-ignore

--- src/helpers/clientGnosisSafe.ts
+++ src/helpers/clientGnosisSafe.ts
@@ -1,7 +1,6 @@
 import Client from '@snapshot-labs/snapshot.js/src/client';
 
 const relayerUrl =
-  // @ts-ignore
   import.meta.env.VITE_RELAYER_URL || 'https://testnet.snapshot.org';
 const client = new Client(relayerUrl);
 
