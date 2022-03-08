efefdb8c2074a277fbd0113d8d61dd93198b1efc
midgerate <midgerate@gmail.com>
2022-03-08 12:51:10 +0530

    fix(): add getUrl method to allow urls be accepted without protocol

--- src/components/IconSocial.vue
+++ src/components/IconSocial.vue
@@ -12,10 +12,22 @@ interface SocialProps {
 withDefaults(defineProps<SocialProps>(), {
   size: '24'
 });
+
+const getUrl = (url?: string) => {
+  if (!url) return '';
+  if (url.startsWith('http://') || url.startsWith('https://')) {
+    return url;
+  }
+  return `https://${url}`;
+};
 </script>
 
 <template>
-  <BaseLink :link="social.link" hide-external-icon :disabled="!social.link">
+  <BaseLink
+    :link="getUrl(social.link)"
+    hide-external-icon
+    :disabled="!social.link"
+  >
     <Icon
       :size="size"
       :name="social.icon"
