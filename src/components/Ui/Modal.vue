0d2a12ae928cca4bbf063ecba4b4a2870a6b7a9c
mktcode <kontakt@markus-kottlaender.de>
2022-03-10 21:12:45 +0100

    removed modal header border

--- src/components/Ui/Modal.vue
+++ src/components/Ui/Modal.vue
@@ -27,7 +27,7 @@ watch(open, (val, prev) => {
     <div v-if="open" class="modal mx-auto w-screen">
       <div class="backdrop" @click="$emit('close')" />
       <div class="shell overflow-hidden relative rounded-none md:rounded-3xl">
-        <div v-if="$slots.header" class="border-b pt-3 pb-2 text-center">
+        <div v-if="$slots.header" class="pt-3 text-center">
           <slot name="header" />
         </div>
         <div class="modal-body">
