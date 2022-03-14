fe7bf76da508422a34168d9fd6b7cd2be5551fa7
Sam <samuv.eth@gmail.com>
2022-03-14 14:44:14 +0700

    Fix loading background color across all loaders

--- src/components/PageLoading.vue
+++ src/components/PageLoading.vue
@@ -1,16 +1,7 @@
 <template>
   <div class="space-y-3">
-    <div
-      class="bg-skin-text rounded-md animate-pulse-fast"
-      style="width: 100%; height: 34px"
-    />
-    <div
-      class="bg-skin-text rounded-md animate-pulse-fast"
-      style="width: 40%; height: 34px"
-    />
-    <div
-      class="bg-skin-text rounded-md animate-pulse-fast"
-      style="width: 65px; height: 28px"
-    />
+    <div class="rounded-md lazy-loading" style="width: 100%; height: 34px" />
+    <div class="rounded-md lazy-loading" style="width: 40%; height: 34px" />
+    <div class="rounded-md lazy-loading" style="width: 65px; height: 28px" />
   </div>
 </template>
