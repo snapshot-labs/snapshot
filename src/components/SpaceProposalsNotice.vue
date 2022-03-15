eccb17c70e02ce527ea2551f1dc73a6990d5351d
Sam <51686767+samuveth@users.noreply.github.com>
2022-03-15 20:44:23 +0700

    Fix content from behind showing on space notice (#1955)

--- src/components/SpaceProposalsNotice.vue
+++ src/components/SpaceProposalsNotice.vue
@@ -16,7 +16,7 @@ const createdSpaces = useStorage(
 <template>
   <Block
     v-if="createdSpaces?.[spaceId]?.showMessage"
-    class="absolute z-10 left-0"
+    class="absolute z-10 left-0 !bg-skin-bg"
   >
     <div>
       <div>
