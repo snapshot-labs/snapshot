45de833c0708019c861c9e7aab0f5870af00c677
Sam <samuv.eth@gmail.com>
2022-03-17 16:54:15 +0700

    Add BaseLoading to other loading components

--- src/components/LoadingSpinner.vue
+++ src/components/LoadingSpinner.vue
@@ -8,30 +8,32 @@ defineProps({
 </script>
 
 <template>
-  <span class="loading">
-    <span>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="50"
-        height="50"
-        viewBox="0 0 50 50"
-      >
-        <path
-          :class="{ '!fill-white': fillWhite }"
-          d="M25,5A20.14,20.14,0,0,1,45,22.88a2.51,2.51,0,0,0,2.49,2.26h0A2.52,2.52,0,0,0,50,22.33a25.14,25.14,0,0,0-50,0,2.52,2.52,0,0,0,2.5,2.81h0A2.51,2.51,0,0,0,5,22.88,20.14,20.14,0,0,1,25,5Z"
+  <BaseLoading>
+    <span class="loading">
+      <span>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="50"
+          height="50"
+          viewBox="0 0 50 50"
         >
-          <animateTransform
-            attributeName="transform"
-            type="rotate"
-            from="0 25 25"
-            to="360 25 25"
-            dur="0.5s"
-            repeatCount="indefinite"
-          />
-        </path>
-      </svg>
+          <path
+            :class="{ '!fill-white': fillWhite }"
+            d="M25,5A20.14,20.14,0,0,1,45,22.88a2.51,2.51,0,0,0,2.49,2.26h0A2.52,2.52,0,0,0,50,22.33a25.14,25.14,0,0,0-50,0,2.52,2.52,0,0,0,2.5,2.81h0A2.51,2.51,0,0,0,5,22.88,20.14,20.14,0,0,1,25,5Z"
+          >
+            <animateTransform
+              attributeName="transform"
+              type="rotate"
+              from="0 25 25"
+              to="360 25 25"
+              dur="0.5s"
+              repeatCount="indefinite"
+            />
+          </path>
+        </svg>
+      </span>
     </span>
-  </span>
+  </BaseLoading>
 </template>
 
 <style lang="scss">
