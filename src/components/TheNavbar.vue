969ef4cf5ef9d397af9278126229cb340a4111f8
mktcode <kontakt@markus-kottlaender.de>
2022-03-24 08:33:38 +0100

    fix pending tx indicator

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -86,7 +86,7 @@ async function handleLogin(connector) {
       </div>
     </BaseContainer>
   </nav>
-  <div class="bg-primary text-white text-center py-2" v-if="pendingCount > 0">
+  <div class="flex justify-center bg-primary text-white text-center py-2" v-if="pendingCount > 0">
     <LoadingSpinner fill-white class="mr-2" />
     {{ $tc('delegate.pendingTransaction', pendingCount) }}
   </div>
