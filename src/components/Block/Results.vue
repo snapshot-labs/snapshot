b3a72b88e0f202a8196b0622389f8cd9e0ca98f2
Sam <samuv.eth@gmail.com>
2022-04-12 22:03:35 +0700

    Remove length check for showing tooltip

--- src/components/Block/Results.vue
+++ src/components/Block/Results.vue
@@ -47,7 +47,7 @@ const ts = (Date.now() / 1e3).toFixed();
             <div class="flex overflow-hidden">
               <span
                 v-tippy="{
-                  content: choice.choice.length > 24 ? choice.choice : null
+                  content: choice.choice
                 }"
                 class="mr-1 truncate"
                 v-text="choice.choice"
@@ -123,7 +123,10 @@ const ts = (Date.now() / 1e3).toFixed();
           />
         </template>
       </div>
-      <div v-if="proposal.quorum || space.voting?.quorum" class="text-skin-link">
+      <div
+        v-if="proposal.quorum || space.voting?.quorum"
+        class="text-skin-link"
+      >
         {{ $t('settings.quorum') }}
         <span class="float-right">
           {{ formatCompactNumber(results.sumOfResultsBalance) }} /
