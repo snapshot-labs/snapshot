14a6943a099fb6c756bfe6e1fb465c18c1b49f1f
Sam <samuv.eth@gmail.com>
2022-04-15 17:36:01 +0700

    Refactor AvatarUser popover into separate component

--- src/components/AvatarUser.vue
+++ src/components/AvatarUser.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { watchEffect } from 'vue';
-import { shorten, explorerUrl } from '@/helpers/utils';
 import { useUsername } from '@/composables/useUsername';
 
 const props = defineProps<{
@@ -21,54 +20,28 @@ watchEffect(() => {
 
 <template>
   <span>
-    <BasePopover :options="{ offset: [0, 12], placement: 'bottom-start' }">
-      <template v-slot:item>
-        <router-link :to="{ name: 'profileAbout', params: { address } }">
-          <a class="flex flex-nowrap">
-            <BaseAvatar
-              v-if="!onlyUsername"
-              :address="address"
-              size="18"
-              class="mr-2"
-            />
-            <span class="truncate w-full">{{ username }}</span>
-            <BaseBadge
-              v-if="!onlyUsername"
-              :address="address"
-              :members="space?.members"
-            />
-          </a>
-        </router-link>
-      </template>
-      <template v-slot:content>
-        <div class="m-4 mb-0 text-center">
-          <BaseAvatar :address="address" size="64" class="mb-4" />
-          <h3 v-if="profile?.name" class="mt-3" v-text="profile.name" />
-          <h3 v-else-if="profile?.ens" v-text="profile.ens" class="mt-3" />
-          <h3 v-else v-text="shorten(address)" class="mt-3" />
-        </div>
-        <div class="m-4 space-y-2">
-          <BaseLink
-            :link="
-              explorerUrl(proposal?.network || space?.network || '1', address)
-            "
-            hide-external-icon
-          >
-            <BaseButton class="w-full">
-              {{ $t('seeInExplorer') }}
-              <BaseIcon name="external-link" class="ml-1" />
-            </BaseButton>
-          </BaseLink>
-          <BaseButton
-            @click.prevent="
-              $router.push({ name: 'profileAbout', params: { address } })
-            "
-            class="w-full"
-          >
-            {{ $t('viewProfile') }}
-          </BaseButton>
-        </div>
-      </template>
-    </BasePopover>
+    <PopoverProfile
+      :address="address"
+      :profile="profile"
+      :proposal="proposal"
+      :space="space"
+    >
+      <router-link :to="{ name: 'profileAbout', params: { address } }">
+        <a class="flex flex-nowrap">
+          <BaseAvatar
+            v-if="!onlyUsername"
+            :address="address"
+            size="18"
+            class="mr-2"
+          />
+          <span class="truncate w-full">{{ username }}</span>
+          <BaseBadge
+            v-if="!onlyUsername"
+            :address="address"
+            :members="space?.members"
+          />
+        </a>
+      </router-link>
+    </PopoverProfile>
   </span>
 </template>
---
+++ src/components/PopoverProfile.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+import { shorten, explorerUrl } from '@/helpers/utils';
+
+defineProps<{
+  address: string;
+  profile?: {
+    name: string;
+    ens: string;
+  };
+  proposal?: {
+    network: string;
+  };
+  space?: {
+    members: string[];
+    network: string;
+  };
+}>();
+</script>
+
+<template>
+  <BasePopover :options="{ offset: [0, 12], placement: 'bottom-start' }">
+    <template v-slot:item>
+      <slot />
+    </template>
+    <template v-slot:content>
+      <div class="m-4 mb-0 text-center">
+        <div class="flex justify-center">
+          <BaseAvatar :address="address" size="80" />
+        </div>
+        <h3 v-if="profile?.name" v-text="profile.name" />
+        <h3 v-else-if="profile?.ens" v-text="profile.ens" />
+        <h3 v-else v-text="shorten(address)" />
+      </div>
+      <div class="m-4 space-y-2">
+        <BaseLink
+          :link="
+            explorerUrl(proposal?.network || space?.network || '1', address)
+          "
+          hide-external-icon
+        >
+          <BaseButton class="w-full">
+            {{ $t('seeInExplorer') }}
+            <BaseIcon name="external-link" class="ml-1" />
+          </BaseButton>
+        </BaseLink>
+        <BaseButton
+          @click.prevent="
+            $router.push({ name: 'profileAbout', params: { address } })
+          "
+          class="w-full"
+        >
+          {{ $t('viewProfile') }}
+        </BaseButton>
+      </div>
+    </template>
+  </BasePopover>
+</template>
