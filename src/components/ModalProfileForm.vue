c463fc2188cfa13cc5a49f4db96289d898091f2f
Sam <samuv.eth@gmail.com>
2022-04-21 17:11:02 +0700

    Fix profile edit modal

--- src/components/ModalProfileForm.vue
+++ src/components/ModalProfileForm.vue
@@ -35,43 +35,49 @@ onMounted(() => {
       </div>
     </template>
 
-    <div class="p-4">
-      <SBaseLabel>{{ $t('profile image') }}</SBaseLabel>
-      <InputUploadAvatar @image-uploaded="url => (form.avatar = url)">
-        <template v-slot:avatar="{ uploading, preview }">
-          <BaseAvatar
-            :address="address"
-            :imgsrc="getIpfsUrl(form.avatar)"
-            :preview="preview"
-            size="80"
-          >
-            <template v-slot:overlay>
-              <AvatarOverlayEdit :loading="uploading" />
-            </template>
-          </BaseAvatar>
-        </template>
-      </InputUploadAvatar>
+    <div class="p-4 space-y-3">
+      <div class="flex justify-center">
+        <InputUploadAvatar @image-uploaded="url => (form.avatar = url)">
+          <template v-slot:avatar="{ uploading, preview }">
+            <BaseAvatar
+              :address="address"
+              :imgsrc="getIpfsUrl(form.avatar)"
+              :preview="preview"
+              size="80"
+            >
+              <template v-slot:overlay>
+                <AvatarOverlayEdit :loading="uploading" />
+                <div
+                  class="bg-skin-heading absolute rounded-full p-1 right-0 bottom-[2px]"
+                >
+                  <i-ho-pencil class="text-skin-bg text-[12px]" />
+                </div>
+              </template>
+            </BaseAvatar>
+          </template>
+        </InputUploadAvatar>
+      </div>
 
       <SBaseInput
         v-model="form.name"
-        title="username"
+        :title="$t('profile.settings.name')"
         type="text"
-        placeholder="enter username"
+        :placeholder="$t('profile.settings.namePlaceholder')"
         :maxLength="properties.name.maxLength"
         focusOnMount
       />
-      <SBaseLabel> bio </SBaseLabel>
-      <TextareaAutosize
-        v-model="form.about"
-        class="s-input !rounded-3xl"
-        :maxLength="properties.about.maxLength"
-        placeholder="tell the world your story"
-      />
-    </div>
-    <template v-slot:footer>
-      <BaseButton @click="null" class="w-full" primary>
+      <div>
+        <SBaseLabel> {{ $t('profile.settings.biography') }} </SBaseLabel>
+        <TextareaAutosize
+          v-model="form.about"
+          class="s-input !rounded-3xl"
+          :maxLength="properties.about.maxLength"
+          :placeholder="$t('profile.settings.bioPlaceholder')"
+        />
+      </div>
+      <BaseButton @click="null" class="w-full !mt-5" primary>
         {{ $t('save') }}
       </BaseButton>
-    </template>
+    </div>
   </BaseModal>
 </template>
