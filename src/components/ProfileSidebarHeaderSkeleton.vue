35c552cff88d971b4168ee8af662ae29d18cfc8e
Sam <samuv.eth@gmail.com>
2022-04-23 20:17:02 +0700

    Fix skeleton size

--- src/components/ProfileSidebarHeaderSkeleton.vue
+++ src/components/ProfileSidebarHeaderSkeleton.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="text-center">
     <div class="pt-3">
-      <div class="h-[80px] w-[80px] mx-auto lazy-loading rounded-full" />
+      <div class="h-[69px] w-[69px] mx-auto lazy-loading rounded-full" />
 
       <div
-        class="bg-skin-text h-[22px] rounded-md lazy-loading mb-2 mt-3 w-[130px] mx-auto"
+        class="bg-skin-text h-[24px] rounded-md lazy-loading mb-2 mt-2 w-[130px] mx-auto"
       />
       <div
         class="bg-skin-text h-[22px] rounded-md lazy-loading w-[100px] mx-auto"
