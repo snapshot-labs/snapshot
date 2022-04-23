54e33b518319a5dbd2ae42e0cc1ec685667a68a3
Sam <samuv.eth@gmail.com>
2022-04-23 15:08:49 +0700

    Fix name size and weight

--- src/components/ProfileName.vue
+++ src/components/ProfileName.vue
@@ -3,14 +3,13 @@ defineProps<{
   profile?: {
     ens?: string;
     name?: string;
-    about?: string;
   };
 }>();
 </script>
 
 <template>
   <div
-    class="text-skin-heading text-xl truncate px-3"
+    class="text-skin-heading text-lg leading-10 font-semibold truncate px-3"
     v-tippy="{
       content: profile?.name || profile?.ens.split('.')[0] || 'unnamed'
     }"
