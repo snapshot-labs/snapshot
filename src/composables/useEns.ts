d33918779f9b58bccde6cb5553cda006f887a7e0
Sam <51686767+samuveth@users.noreply.github.com>
2022-04-24 13:23:16 +0700

    Fix delegation page errors (#2228)
    * Fix promise all function undefined error
    
    * Fix watcheffect looping
    
    * Remove WE

--- src/composables/useEns.ts
+++ src/composables/useEns.ts
@@ -15,38 +15,42 @@ export function useEns() {
   const ownedEnsDomains = ref<Record<string, any>[]>([]);
 
   const loadOwnedEnsDomains = async () => {
-    if (web3Account.value) {
-      const res = await ensApolloQuery({
-        query: ENS_DOMAINS_BY_ACCOUNT_QUERY,
-        variables: {
-          id: web3Account.value.toLowerCase()
-        }
-      });
+    if (!web3Account.value) return (ownedEnsDomains.value = []);
 
-      // The ens subgraph returns only the hash for domain TLDs other than .eth, so we
-      // have to make a second request to fetch the actual domain.
-      ownedEnsDomains.value = await Promise.all(res.account?.domains.map(async domain => {
-        const hash = domain.name.match(/\[(.*?)\]/)?.[1];
-        if (hash) {
-          const res = await ensApolloQuery({
-            query: ENS_DOMAIN_BY_HASH_QUERY,
-            variables: {
-              id: `0x${hash}`
+    const res = await ensApolloQuery({
+      query: ENS_DOMAINS_BY_ACCOUNT_QUERY,
+      variables: {
+        id: web3Account.value.toLowerCase()
+      }
+    });
+
+    // The ens subgraph returns only the hash for domain TLDs other than .eth, so we
+    // have to make a second request to fetch the actual domain.
+    ownedEnsDomains.value =
+      (await Promise.all(
+        res.account?.domains.map(async domain => {
+          const hash = domain.name.match(/\[(.*?)\]/)?.[1];
+          if (hash) {
+            const res = await ensApolloQuery({
+              query: ENS_DOMAIN_BY_HASH_QUERY,
+              variables: {
+                id: `0x${hash}`
+              }
+            });
+            if (res.registration?.domain?.labelName) {
+              return {
+                ...domain,
+                name: domain.name.replace(
+                  `[${hash}]`,
+                  res.registration.domain.labelName
+                )
+              };
             }
-          });
-          if (res.registration?.domain?.labelName) {
-            return {
-              ...domain,
-              name: domain.name.replace(`[${hash}]`, res.registration.domain.labelName),
-            };
           }
-        }
 
-        return domain;
-      })) || [];
-    } else {
-      ownedEnsDomains.value = [];
-    }
+          return domain;
+        }) ?? []
+      )) || [];
   };
 
   function isValidEnsDomain(domain) {
