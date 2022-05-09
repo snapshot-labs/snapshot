96a7b348eec8b2bb5e3ffdcb974d4b7fa15ddf1e
Sam <samuv.eth@gmail.com>
2022-05-09 23:44:49 +0700

    Show strategy network and link to block explorer for strategy network

--- src/components/Modal/Strategies.vue
+++ src/components/Modal/Strategies.vue
@@ -2,6 +2,7 @@
 import { isAddress } from '@ethersproject/address';
 import { shorten, explorerUrl } from '@/helpers/utils';
 import { encode } from '@/helpers/b64';
+import networks from '@snapshot-labs/snapshot.js/src/networks.json';
 
 defineProps({
   open: Boolean,
@@ -24,32 +25,38 @@ defineEmits(['close']);
         :key="i"
         class="p-4 mb-3 text-skin-link"
       >
-        <router-link
-          class="float-right mt-2 pt-1"
-          target="_blank"
-          :to="{
-            name: 'playground',
-            params: { name: strategy.name },
-            query: {
-              query: encode(
-                JSON.stringify({
-                  network: proposal.network,
-                  snapshot: proposal.snapshot,
-                  params: strategy.params
-                })
-              )
-            }
-          }"
-        >
-          <BaseIcon name="play" size="18" />
-        </router-link>
-        <h3 v-text="strategy.name" />
+        <div class="flex items-center justify-between">
+          <h3 v-text="strategy.name" />
+          <router-link
+            target="_blank"
+            :to="{
+              name: 'playground',
+              params: { name: strategy.name },
+              query: {
+                query: encode(
+                  JSON.stringify({
+                    network: proposal.network,
+                    snapshot: proposal.snapshot,
+                    params: strategy.params
+                  })
+                )
+              }
+            }"
+          >
+            <BaseIcon name="play" size="18" class="flex" />
+          </router-link>
+        </div>
+
         <div>
+          <div v-if="strategy.network" class="flex justify-between">
+            <span class="flex-auto text-skin-text mr-1"> network </span>
+            <span v-text="networks[strategy.network].name" />
+          </div>
           <div v-for="(option, key) in strategy.params" :key="key" class="flex">
             <span v-text="key" class="flex-auto text-skin-text mr-1" />
             <BaseLink
               v-if="key === 'address' || isAddress(option)"
-              :link="explorerUrl(proposal.network, option)"
+              :link="explorerUrl(strategy.network ?? proposal.network, option)"
               class="block"
             >
               <span v-text="shorten(option)" />
