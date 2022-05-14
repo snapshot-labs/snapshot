70cc5ec2030d44a7b39e426a5278959f9b66e78d
lzalvin <alvin@layerzerolabs.org>
2022-05-14 13:13:05 -0700

    bumped snapshot.js

--- src/plugins/quorum/index.ts
+++ src/plugins/quorum/index.ts
@@ -1,32 +1,7 @@
 import { BigNumber } from '@ethersproject/bignumber';
-import { call, subgraphRequest } from '@snapshot-labs/snapshot.js/src/utils';
+import { call } from '@snapshot-labs/snapshot.js/src/utils';
+import { getSnapshots } from '@snapshot-labs/snapshot.js/src/utils/blockfinder'
 import getProvider from '@snapshot-labs/snapshot.js/src/utils/provider';
-
-export async function getSnapshots(network, snapshot, provider, networks) {
-  const snapshots = {};
-  networks.forEach((n) => (snapshots[n] = 'latest'));
-  if (snapshot === 'latest') return snapshots;
-  snapshots[network] = snapshot;
-  const networkIn = Object.keys(snapshots).filter((s) => network !== s);
-  if (networkIn.length === 0) return snapshots;
-  const block = await provider.getBlock(snapshot);
-  const query = {
-    blocks: {
-      __args: {
-        where: {
-          ts: block.timestamp,
-          network_in: networkIn
-        }
-      },
-      network: true,
-      number: true
-    }
-  };
-  const url = 'https://blockfinder.snapshot.org/graphql';
-  const data = await subgraphRequest(url, query);
-  data.blocks.forEach((block) => (snapshots[block.network] = block.number));
-  return snapshots;
-}
 
 export default class Plugin {
   public author = 'lbeder';
