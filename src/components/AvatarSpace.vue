2c0ab08963ff0dc3243c635aaa402c572f9dde8c
bonustrack <fabien@bonustrack.co>
2022-05-27 04:13:48 +0700

    Add bg border color

--- src/components/AvatarSpace.vue
+++ src/components/AvatarSpace.vue
@@ -12,6 +12,7 @@ const props = defineProps({
   <img
     :src="`https://stamp.fyi/space/${space.id}?s=${size * 2}`"
     class="rounded-full inline-block align-middle leading-none"
+    :class="'bg-[color:var(--border-color)]'"
     :style="{
       width: `${size}px`,
       height: `${size}px`
