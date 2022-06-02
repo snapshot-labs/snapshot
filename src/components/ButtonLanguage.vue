0806a1b801f2bb7191b58380522837e453db8580
Sam <samuv.eth@gmail.com>
2022-06-02 13:19:52 +0700

    Fix language switcher on custom domain

--- src/components/ButtonLanguage.vue
+++ src/components/ButtonLanguage.vue
@@ -1,13 +1,22 @@
-<script setup>
+<script setup lang="ts">
 import { ref } from 'vue';
 import languages from '@/locales/languages.json';
+
+defineProps<{
+  small?: boolean;
+}>();
 
 const modalLangOpen = ref(false);
 </script>
 
 <template>
-  <BaseButton class="flex items-center" @click="modalLangOpen = true">
-    <BaseIcon size="24" name="earth" class="mr-2" />
-    {{ languages[$i18n.locale]?.nativeName ?? languages[$i18n.locale]?.name }}
-  </BaseButton>
+  <DropdownLanguages>
+    <UiSidebarButton v-if="small">
+      <BaseIcon size="20" name="earth" class="text-skin-link" />
+    </UiSidebarButton>
+    <BaseButton v-else class="flex items-center" @click="modalLangOpen = true">
+      <BaseIcon size="24" name="earth" class="mr-2" />
+      {{ languages[$i18n.locale]?.nativeName ?? languages[$i18n.locale]?.name }}
+    </BaseButton>
+  </DropdownLanguages>
 </template>
