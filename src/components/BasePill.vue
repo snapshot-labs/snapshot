1473d1d010006c770d4a6a7f880bc4c28de854e6
Sam <samuv.eth@gmail.com>
2022-06-09 17:03:38 +0700

    Fix pill

--- src/components/BasePill.vue
+++ src/components/BasePill.vue
@@ -2,7 +2,7 @@
 
 <template>
   <span
-    class="h-[20px] rounded-full bg-skin-text px-2 text-center text-xs leading-normal text-white"
+    class="rounded-full bg-skin-text px-2 text-center text-xs leading-5 text-white"
   >
     <slot />
   </span>
