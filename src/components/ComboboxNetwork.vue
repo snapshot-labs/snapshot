f753f9f17bd56b092a72a744139315594c8c603f
Sam <samuv.eth@gmail.com>
2022-06-09 17:05:27 +0700

    Add base combobox and network combobox

---
+++ src/components/ComboboxNetwork.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import { ref, computed } from 'vue';
+import { useNetworksFilter } from '@/composables/useNetworksFilter';
+
+defineProps<{
+  network: string;
+}>();
+
+const emit = defineEmits(['select']);
+
+const query = ref('');
+
+const { filterNetworks } = useNetworksFilter();
+
+const networks = computed((): { id: string; name: string }[] => {
+  const filteredNetworks = filterNetworks(query.value).map(_n => ({
+    id: _n.key,
+    name: _n.name
+  }));
+
+  return filteredNetworks;
+});
+</script>
+
+<template>
+  <BaseCombobox
+    label="Network"
+    :items="networks"
+    :selected-id="network"
+    @select="value => emit('select', value.id)"
+    @search="value => (query = value)"
+  >
+    <template #item="{ item }">
+      <div class="truncate pr-2">
+        {{ item.name }}
+      </div>
+
+      <BasePill class="leading-4"> #{{ item.id }} </BasePill>
+    </template>
+  </BaseCombobox>
+</template>
