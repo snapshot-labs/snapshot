ab03bb7dcc04eed5edf0ac27684897766e1310cb
Sam <samuv.eth@gmail.com>
2022-06-10 18:46:04 +0700

    Add input select

---
+++ src/components/InputSelect.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+defineProps<{
+  modelValue: string;
+  title: string;
+}>();
+
+const emit = defineEmits(['select']);
+</script>
+
+<template>
+  <div class="w-full">
+    <LabelInput>{{ title }}</LabelInput>
+    <BaseButton
+      :class="$attrs.class"
+      class="!h-[42px] w-full pl-3 text-left"
+      @click="emit('select')"
+    >
+      {{ modelValue }}
+    </BaseButton>
+  </div>
+</template>
