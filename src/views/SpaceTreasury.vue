b74b1a83f609a3f972f9cb26713908c022b2a8c6
Sam <samuv.eth@gmail.com>
2022-06-16 14:05:50 +0700

    Add treasuries

--- src/views/SpaceTreasury.vue
+++ src/views/SpaceTreasury.vue
@@ -3,29 +3,14 @@ import { computed } from 'vue';
 import { useRoute } from 'vue-router';
 import { TreasuryWallet } from '@/helpers/interfaces';
 
-defineProps<{
-  space: { id: string };
+const props = defineProps<{
+  space: { id: string; treasuries: TreasuryWallet[] };
 }>();
 
 const route = useRoute();
 
-const wallets = [
-  {
-    address: '0x10A19e7eE7d7F8a52822f6817de8ea18204F2e4f',
-    name: 'Balancer',
-    network: 1,
-    ensAddress: 'balancer.eth'
-  },
-  {
-    address: '0x57a8865cfb1ecef7253c27da6b4bc3daee5be518',
-    name: 'Gitcoin',
-    network: 1,
-    ensAddress: 'gitcoindao.eth'
-  }
-];
-
 const wallet = computed(() =>
-  wallets.find(w => w.address === route.params.wallet)
+  props.space.treasuries.find(w => w.address === route.params.wallet)
 );
 </script>
 
@@ -36,7 +21,7 @@ const wallet = computed(() =>
     </template>
     <template #content-right>
       <TreasuryAssetsList v-if="wallet" :wallet="wallet as TreasuryWallet" />
-      <TreasuryWalletsList v-else :wallets="wallets" />
+      <TreasuryWalletsList v-else :wallets="props.space.treasuries" />
     </template>
   </TheLayout>
 </template>
