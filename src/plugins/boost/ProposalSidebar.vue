180554acd70273e04104fc5ff4886ad69257a7c3
mktcode <kontakt@markus-kottlaender.de>
2022-06-22 16:32:32 +0200

    show message when no boosts

--- src/plugins/boost/ProposalSidebar.vue
+++ src/plugins/boost/ProposalSidebar.vue
@@ -18,8 +18,11 @@ onMounted(() => {
 
 <template>
   <BaseBlock v-if="space.plugins.boost" :title="pluginIndex.boost.name">
-    <div v-for="boost in boosts" :key="boost.id">
-      {{ JSON.stringify(boost) }}
+    <div v-if="boosts.length">
+      <div v-for="boost in boosts" :key="boost.id">
+        {{ JSON.stringify(boost) }}
+      </div>
     </div>
+    <div v-else>No boosts for this proposal.</div>
   </BaseBlock>
 </template>
