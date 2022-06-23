5bf9debeff46f819e8c1225521dd2ac22a154566
mktcode <kontakt@markus-kottlaender.de>
2022-06-23 13:54:39 +0200

    implemented claiming

--- src/plugins/boost/ProposalSidebar.vue
+++ src/plugins/boost/ProposalSidebar.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
-import { onMounted } from 'vue';
+import { onMounted, watch } from 'vue';
 import { formatEther } from '@ethersproject/units';
 import { usePlugins } from '@/composables/usePlugins';
+import { useWeb3 } from '@/composables/useWeb3';
 import { useBoost } from './useBoost';
 
 const props = defineProps<{
@@ -9,18 +10,46 @@ const props = defineProps<{
   proposal: Record<string, any>;
 }>();
 
+const { web3Account } = useWeb3();
 const { pluginIndex } = usePlugins();
-const { loadBoosts, boosts } = useBoost();
+const {
+  loadBoosts,
+  boosts,
+  requestClaimReceipt,
+  hasReceiptForBoost,
+  claimTokens
+} = useBoost();
 
-onMounted(() => {
-  loadBoosts(props.proposal.id);
+onMounted(async () => {
+  await loadBoosts(props.proposal.id);
+
+  if (web3Account.value) {
+    for (const boost of boosts.value) {
+      requestClaimReceipt(boost.id, web3Account.value, '1000000000000000000');
+    }
+  }
+});
+
+watch(web3Account, () => {
+  if (web3Account.value) {
+    requestClaimReceipt(1, web3Account.value, '1000000000000000000');
+  }
 });
 </script>
 
 <template>
   <BaseBlock v-if="boosts.length" :title="pluginIndex.boost.name">
     <div v-for="boost in boosts" :key="boost.id">
-      Boost balance: {{ formatEther(boost.balance) }}
+      Boost balance: {{ formatEther(boost.balance) }}<br />
+      Token: {{ boost.token }}<br />
+      <BaseButton
+        v-if="hasReceiptForBoost(boost.id)"
+        class="w-100"
+        @click="claimTokens(boost.id)"
+      >
+        Claim
+      </BaseButton>
+      <div v-else>Can't claim</div>
     </div>
   </BaseBlock>
 </template>
--- src/plugins/boost/useBoost.ts
+++ src/plugins/boost/useBoost.ts
@@ -1,6 +1,6 @@
 import gql from 'graphql-tag';
 import { pin } from '@snapshot-labs/pineapple';
-import { BigNumberish } from '@ethersproject/bignumber';
+import { BigNumber, BigNumberish } from '@ethersproject/bignumber';
 import { getInstance } from '@snapshot-labs/lock/plugins/vue3';
 import { sendTransaction } from '@snapshot-labs/snapshot.js/src/utils';
 import { useTxStatus } from '@/composables/useTxStatus';
@@ -8,7 +8,8 @@ import { useApolloQuery } from '@/composables/useApolloQuery';
 import ABI from './abi.json';
 import { ref } from 'vue';
 
-const CONTRACT_ADDRESS = '0xebdb154759f9d6d02a1930dd0a240aa66ddb962a';
+const GUARD_URL = 'https://boost-guard.mktcode.uber.space';
+const CONTRACT_ADDRESS = '0xec5ba34cf4a473e0254f9d33ca532930755f9615';
 const BOOSTS_QUERY = gql`
   query Boosts($ref: String!) {
     boosts(ref: $ref) {
@@ -27,7 +28,8 @@ export function useBoost() {
   const { apolloQuery } = useApolloQuery();
   const { pendingCount } = useTxStatus();
 
-  const boosts = ref<Record<string, any>>([]);
+  const boosts = ref<Record<string, any>[]>([]);
+  const receipts = ref<Record<string, any>[]>([]);
   const loadingBoosts = ref(true);
 
   async function loadBoosts(proposalId: string) {
@@ -55,18 +57,14 @@ export function useBoost() {
     pendingCount.value--;
   }
 
-  async function claimTokens(
-    boostId: BigNumberish,
-    recipient: string,
-    amount: BigNumberish,
-    signature: string
-  ) {
+  async function claimTokens(boostId: BigNumberish) {
+    const receipt = getReceiptForBoost(boostId);
     const tx = await sendTransaction(
       getInstance().web3,
       CONTRACT_ADDRESS,
       ABI,
       'claimTokens',
-      [{ boostId, recipient, amount }, signature]
+      [receipt.data.message, receipt.sig]
     );
     pendingCount.value++;
     await tx.wait();
@@ -78,11 +76,53 @@ export function useBoost() {
     console.log(receipt);
   }
 
+  async function requestClaimReceipt(
+    boostId: BigNumberish,
+    recipient: string,
+    amount: BigNumberish
+  ) {
+    const init = {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        jsonrpc: '2.0',
+        method: 'get_receipt',
+        params: {
+          boostId,
+          recipient,
+          amount
+        },
+        id: null
+      })
+    };
+    const res = await fetch(GUARD_URL, init).then(res => res.json());
+    if (res.result.sig) {
+      receipts.value[BigNumber.from(boostId).toString()] = res.result;
+      console.log('Receipt:', receipts.value);
+    }
+  }
+
+  function hasReceiptForBoost(boostId: BigNumberish) {
+    return !!receipts.value[BigNumber.from(boostId).toString()];
+  }
+
+  function getReceiptForBoost(boostId: BigNumberish) {
+    return receipts.value[BigNumber.from(boostId).toString()];
+  }
+
   return {
     loadBoosts,
     boosts,
+    receipts,
+    loadingBoosts,
     depositTokens,
     claimTokens,
-    pinStrategy
+    pinStrategy,
+    requestClaimReceipt,
+    hasReceiptForBoost,
+    getReceiptForBoost
   };
 }
