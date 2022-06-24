1427b8199b2a797521cca568a2c341b49fc984d2
Sam <samuv.eth@gmail.com>
2022-06-24 20:35:29 +0700

    Refactor voting type modal with new selected item component

--- src/components/ModalVotingType.vue
+++ src/components/ModalVotingType.vue
@@ -33,40 +33,19 @@ function select(id) {
     <template #header>
       <h3>{{ $t('voting.selectVoting') }}</h3>
     </template>
-    <div class="mx-0 mt-4 flex flex-col space-y-3 md:mx-4">
+    <div class="mx-0 my-4 flex flex-col space-y-3 md:mx-4">
       <a v-if="allowAny" @click="select(undefined)">
-        <BaseBlock
-          :class="[
-            'voting-type relative transition-colors hover:border-skin-text',
-            { '!border-skin-link': !selected }
-          ]"
-        >
-          <i
-            v-if="!selected"
-            class="iconfont iconcheck1 absolute top-2 right-2 text-lg"
-          />
-          <h3>
-            {{ $t('settings.anyType') }}
-          </h3>
-        </BaseBlock>
+        <BaseModalSelectItem
+          :selected="!selected"
+          :title="$t('settings.anyType')"
+        />
       </a>
       <a v-for="(type, key) in types" :key="key" @click="select(type)">
-        <BaseBlock
-          :class="[
-            'voting-type relative transition-colors hover:border-skin-text',
-            { '!border-skin-link': type === selected }
-          ]"
-        >
-          <i
-            v-if="type === selected"
-            class="iconfont iconcheck1 absolute top-2 right-2 text-lg"
-          />
-          <h3 class="mt-0" v-text="$t(`voting.${type}`)" />
-          <div
-            class="text-skin-text"
-            v-text="$t(`voting.description.${type}`)"
-          />
-        </BaseBlock>
+        <BaseModalSelectItem
+          :selected="type === selected"
+          :title="$t(`voting.${type}`)"
+          :description="$t(`voting.description.${type}`)"
+        />
       </a>
     </div>
   </BaseModal>
