e121428623ad7cc6f3e4eccb53ede2835c05837d
Sam <samuv.eth@gmail.com>
2022-06-29 17:18:35 +0700

    Fix align

--- src/components/ProposalResultsShutter.vue
+++ src/components/ProposalResultsShutter.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="mt-3">
-    <span class="text-xs"> {{ $t('poweredBy') }} </span>
+    <div class="-mb-1 text-xs">{{ $t('poweredBy') }}</div>
     <BaseLink link="https://shutter.network/" class="flex items-center gap-1">
-      <i-s-shutter class="-mt-2 -mr-1 h-[36px] text-[70px]" />
+      <i-s-shutter class="-mt-1 -mr-1 h-[36px] text-[70px]" />
     </BaseLink>
   </div>
 </template>
