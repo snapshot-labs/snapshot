0d2d60ea2b7237fc86ec63815a335f0d1a63d2a5
Sam <samuv.eth@gmail.com>
2022-07-03 15:27:44 +0700

    Add header

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { useStorage } from '@vueuse/core';
+
+const visible = useStorage('snapshot.showHomeHeader', true);
+</script>
+
+<template>
+  <BaseContainer v-if="visible" class="mb-4">
+    <div
+      class="relative w-full rounded-xl border border-skin-border p-4 pr-[50px]"
+    >
+      <h3 class="mt-0 font-mono">Where decisions get made</h3>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat.
+      </p>
+      <div class="mt-4 space-x-3">
+        <BaseButton primary>Create a space</BaseButton>
+        <BaseButton>Learn more</BaseButton>
+      </div>
+      <BaseButtonIcon class="absolute right-3 top-3" @click="visible = false">
+        <i-ho-x class="text-base" />
+      </BaseButtonIcon>
+    </div>
+  </BaseContainer>
+</template>
