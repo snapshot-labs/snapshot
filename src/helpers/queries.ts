fffe60799d59e58aa3c4270b2eabfd5aec9022b4
Sam <51686767+samuveth@users.noreply.github.com>
2022-07-10 15:51:54 +0700

    Raise follows query limit (#2784)

--- src/helpers/queries.ts
+++ src/helpers/queries.ts
@@ -136,7 +136,10 @@ export const NOTIFICATION_PROPOSALS_QUERY = gql`
 
 export const FOLLOWS_QUERY = gql`
   query Follows($space_in: [String], $follower_in: [String]) {
-    follows(where: { space_in: $space_in, follower_in: $follower_in }) {
+    follows(
+      where: { space_in: $space_in, follower_in: $follower_in }
+      first: 500
+    ) {
       id
       follower
       space {
