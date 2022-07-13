6697624f765b2a3d4e0639157bbccaf907a2e82a
Sam <samuv.eth@gmail.com>
2022-07-13 19:30:40 +0700

    Fix shutter team feadback

--- src/components/ProposalResultsShutter.vue
+++ src/components/ProposalResultsShutter.vue
@@ -1,8 +1,18 @@
 <template>
-  <div class="mt-3">
-    <div class="-mb-1 text-xs">{{ $t('poweredBy') }}</div>
-    <BaseLink link="https://shutter.network/" class="flex items-center gap-1">
-      <i-s-shutter class="-mt-1 -mr-1 h-[36px] text-[70px]" />
-    </BaseLink>
+  <div>
+    <div class="-mb-2 mt-2 text-xs">{{ $t('poweredBy') }}</div>
+
+    <div class="flex items-center">
+      <div
+        v-tippy="{
+          content:
+            'This proposal has Shutter privacy enabled. All votes will be encrypted until the voting period has ended and the final score is calculated.'
+        }"
+        class="inline-block cursor-help"
+      >
+        <i-s-shutter class="h-[36px] text-[70px]" />
+      </div>
+      <BaseLink link="https://shutter.network/" />
+    </div>
   </div>
 </template>
