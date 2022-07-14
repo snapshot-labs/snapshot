cfa5ce35e7e08cdda3e4bf5634397a3c4ea09861
Sam <samuv.eth@gmail.com>
2022-07-14 17:04:37 +0700

    Fix header

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -9,18 +9,16 @@ const visible = useStorage('snapshot.showHomeHeader', true);
     <div
       class="relative w-full rounded-xl border border-skin-border p-4 pr-[280px]"
     >
-      <h3 class="mt-0 font-mono">For DAOs, governance should be a snap</h3>
+      <h3 class="mt-0 font-mono">{{ $t('header.title') }}</h3>
       <p>
-        Snapshot is the home of your community's governance. It's a place where
-        you can create proposals, weigh voting power in flexable ways, and vote
-        completely off-chain.
+        {{ $t('header.description') }}
       </p>
       <div class="mt-4 space-x-3">
         <BaseButton primary @click="$router.push({ name: 'setup' })">
-          Create a space
+          {{ $t('createASpace') }}
         </BaseButton>
         <BaseButton @click="$router.push({ name: 'about' })">
-          Learn more
+          {{ $t('learnMore') }}
         </BaseButton>
       </div>
       <BaseButtonIcon class="absolute right-3 top-3" @click="visible = false">
