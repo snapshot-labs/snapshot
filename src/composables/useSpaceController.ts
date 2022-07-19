c952c2efc46e4260c49e683bfec4397bc43f3e58
Sam <samuv.eth@gmail.com>
2022-07-19 16:10:08 +0700

    Fix stupidness

--- src/composables/useSpaceController.ts
+++ src/composables/useSpaceController.ts
@@ -104,7 +104,7 @@ export function useSpaceController() {
     if (defaultNetwork === '1') {
       return (uriAddress.value = uriArray[4] ?? '');
     }
-    if (uriArray[4] === '/testnet/') {
+    if (uriArray[4] === 'testnet') {
       return (uriAddress.value = uriArray[5] ?? '');
     }
   }
