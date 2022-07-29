7bf2f3e2d1424fa1b2ca6f576bac929fe68beb52
Sam <samuv.eth@gmail.com>
2022-07-29 19:58:34 +0700

    Add types

--- src/composables/useProposals.ts
+++ src/composables/useProposals.ts
@@ -1,6 +1,18 @@
 import { reactive } from 'vue';
+import { Proposal } from '@/helpers/interfaces';
 
-const store = reactive({
+interface ProposalsStore {
+  space: {
+    proposals: Proposal[];
+    filterBy: string;
+  };
+  timeline: {
+    proposals: Proposal[];
+    filterBy: string;
+  };
+}
+
+const store = reactive<ProposalsStore>({
   space: {
     proposals: [],
     filterBy: 'all'
