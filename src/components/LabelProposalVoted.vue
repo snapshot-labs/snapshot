ba64d3b0ada898ba5fa46213312ffc5ae3cf244a
Sam <samuv.eth@gmail.com>
2022-07-30 15:22:42 +0700

    Fix voted label

--- src/components/LabelProposalVoted.vue
+++ src/components/LabelProposalVoted.vue
@@ -2,9 +2,9 @@
 
 <template>
   <div
-    class="flex items-center gap-1 rounded-3xl border !border-green py-[1px] pl-[7px] pr-[12px] text-skin-heading"
+    class="flex items-center gap-1 rounded-full border py-[1px] pl-[7px] pr-[12px] text-sm text-skin-heading"
   >
     <i-ho-check class="text-green" />
-    Voted
+    {{ $t('voted') }}
   </div>
 </template>
