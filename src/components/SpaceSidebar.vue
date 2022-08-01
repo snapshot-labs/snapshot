9f351dcf22335e838638b92bf8093b2eb10dc781
Sam <samuv.eth@gmail.com>
2022-08-01 12:43:57 +0700

    Hide scrollbar overflow

--- src/components/SpaceSidebar.vue
+++ src/components/SpaceSidebar.vue
@@ -7,11 +7,13 @@ defineProps<{ space: ExtendedSpace }>();
 
 <template>
   <div class="mb-4 lg:fixed lg:mb-0 lg:w-[240px]">
-    <BaseBlock slim class="lg:max-h-[calc(100vh-120px)] lg:overflow-y-auto">
-      <SpaceSidebarHeader :space="space" />
-      <SpaceSidebarNavigation :space="space" class="mt-4 lg:mt-0" />
-      <SpaceSidebarSubspaces :space="space" class="hidden lg:flex" />
-      <SpaceSidebarFooter :space="space" class="hidden lg:flex" />
+    <BaseBlock slim class="overflow-hidden">
+      <div class="lg:max-h-[calc(100vh-120px)] lg:overflow-y-auto">
+        <SpaceSidebarHeader :space="space" />
+        <SpaceSidebarNavigation :space="space" class="mt-4 lg:mt-0" />
+        <SpaceSidebarSubspaces :space="space" class="hidden lg:flex" />
+        <SpaceSidebarFooter :space="space" class="hidden lg:flex" />
+      </div>
     </BaseBlock>
   </div>
 </template>
