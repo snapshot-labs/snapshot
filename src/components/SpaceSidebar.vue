84cca8b0c7b5bb187bead549b01fc6ebf3c450bb
Sam <samuv.eth@gmail.com>
2022-08-01 12:38:23 +0700

    Fix sidebar overflow when large amount of subspaces

--- src/components/SpaceSidebar.vue
+++ src/components/SpaceSidebar.vue
@@ -7,7 +7,7 @@ defineProps<{ space: ExtendedSpace }>();
 
 <template>
   <div class="mb-4 lg:fixed lg:mb-0 lg:w-[240px]">
-    <BaseBlock slim class="overflow-hidden">
+    <BaseBlock slim class="lg:max-h-[calc(100vh-120px)] lg:overflow-y-auto">
       <SpaceSidebarHeader :space="space" />
       <SpaceSidebarNavigation :space="space" class="mt-4 lg:mt-0" />
       <SpaceSidebarSubspaces :space="space" class="hidden lg:flex" />
