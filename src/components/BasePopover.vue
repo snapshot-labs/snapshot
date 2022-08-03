0aac053fe047cc16997077dff4823903c8e01d70
Sam <samuv.eth@gmail.com>
2022-08-03 19:06:02 +0700

    Add floating to BasePopover

--- src/components/BasePopover.vue
+++ src/components/BasePopover.vue
@@ -1,34 +1,45 @@
 <script setup lang="ts">
 import { Popover, PopoverButton, PopoverPanel } from '@headlessui/vue';
+import { Float } from '@headlessui-float/vue';
+import type { Placement } from '@floating-ui/dom';
 
-defineProps<{
-  label?: string;
-}>();
+withDefaults(
+  defineProps<{
+    label?: string;
+    placement?: Placement;
+  }>(),
+  {
+    label: '',
+    placement: 'bottom-end'
+  }
+);
 </script>
 
 <template>
-  <Popover class="relative">
-    <PopoverButton>
-      <slot v-if="$slots.button" name="button" />
-      <div v-else>
-        <span>{{ label }}</span>
-        <i-ho-chevron-down class="ml-2 h-5 w-5" aria-hidden="true" />
-      </div>
-    </PopoverButton>
-
-    <Transition
-      enter-active-class="transition ease-out duration-100"
-      enter-from-class="transform opacity-0 scale-95"
-      enter-to-class="transform opacity-100 scale-100"
-      leave-active-class="transition ease-in duration-75"
-      leave-from-class="transform opacity-100 scale-100"
-      leave-to-class="transform opacity-0 scale-95"
-    >
-      <PopoverPanel class="absolute right-0 z-20 mt-2 w-screen max-w-sm">
-        <div class="overflow-hidden rounded-2xl bg-skin-header-bg shadow-lg">
-          <slot name="content" />
+  <Popover>
+    <Float :placement="placement" :offset="10" :shift="8">
+      <PopoverButton class="outline-none">
+        <slot v-if="$slots.button" name="button" />
+        <div v-else>
+          <span>{{ label }}</span>
+          <i-ho-chevron-down class="ml-2 h-5 w-5" aria-hidden="true" />
         </div>
-      </PopoverPanel>
-    </Transition>
+      </PopoverButton>
+
+      <Transition
+        enter-active-class="transition ease-out duration-100"
+        enter-from-class="transform opacity-0 scale-95"
+        enter-to-class="transform opacity-100 scale-100"
+        leave-active-class="transition ease-in duration-75"
+        leave-from-class="transform opacity-100 scale-100"
+        leave-to-class="transform opacity-0 scale-95"
+      >
+        <PopoverPanel class="z-20 w-screen max-w-xs sm:max-w-sm">
+          <div class="overflow-hidden rounded-2xl bg-skin-header-bg shadow-lg">
+            <slot name="content" />
+          </div>
+        </PopoverPanel>
+      </Transition>
+    </Float>
   </Popover>
 </template>
