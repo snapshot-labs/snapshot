688fd0b9904e4b4c3f604fc2c1c116ee3af824ca
Sam <samuv.eth@gmail.com>
2022-08-03 21:52:19 +0700

    Refactor navbar

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -1,35 +1,9 @@
 <script setup lang="ts">
-import { ref, watchEffect, computed } from 'vue';
-import { shorten } from '@/helpers/utils';
-import { useModal } from '@/composables/useModal';
-import { useApp } from '@/composables/useApp';
-import { useWeb3 } from '@/composables/useWeb3';
-import { useTxStatus } from '@/composables/useTxStatus';
-import { getInstance } from '@snapshot-labs/lock/plugins/vue3';
-import { useProfiles } from '@/composables/useProfiles';
+import { useApp, useWeb3, useTxStatus } from '@/composables';
 
 const { pendingCount } = useTxStatus();
-const { modalAccountOpen } = useModal();
 const { env, showSidebar, domain } = useApp();
-const auth = getInstance();
-const { login, web3, web3Account } = useWeb3();
-const { profiles, loadProfiles, loadingProfiles, reloadingProfile } =
-  useProfiles();
-
-const loading = ref(false);
-
-async function handleLogin(connector) {
-  modalAccountOpen.value = false;
-  loading.value = true;
-  await login(connector);
-  loading.value = false;
-}
-
-const profile = computed(() => profiles.value[web3Account.value]);
-
-watchEffect(() => {
-  loadProfiles([web3Account.value]);
-});
+const { web3Account } = useWeb3();
 </script>
 
 <template>
@@ -56,48 +30,10 @@ watchEffect(() => {
           </router-link>
         </div>
         <div :key="web3Account" class="flex space-x-2">
-          <template v-if="auth.isAuthenticated.value">
-            <MenuAccount
-              :address="web3Account"
-              @switchWallet="modalAccountOpen = true"
-            >
-              <BaseButton
-                :loading="
-                  web3.authLoading || loadingProfiles || reloadingProfile
-                "
-                class="flex items-center"
-              >
-                <AvatarUser
-                  :address="web3Account"
-                  size="18"
-                  class="-mr-1 -ml-1 sm:mr-2 md:mr-2 lg:mr-2 xl:mr-2"
-                />
-                <span
-                  v-if="profile?.name || profile?.ens"
-                  class="hidden sm:block"
-                  v-text="profile.name || profile.ens"
-                />
-                <span
-                  v-else
-                  class="hidden sm:block"
-                  v-text="shorten(web3Account)"
-                />
-              </BaseButton>
-            </MenuAccount>
-          </template>
+          <NavbarAccount />
 
-          <BaseButton
-            v-if="!auth.isAuthenticated.value"
-            :loading="loading || web3.authLoading"
-            :aria-label="$t('connectWallet')"
-            @click="modalAccountOpen = true"
-          >
-            <span class="hidden sm:block" v-text="$t('connectWallet')" />
-            <i-ho-login
-              class="-ml-2 -mr-[11px] block align-text-bottom sm:hidden"
-            />
-          </BaseButton>
           <NavbarNotifications v-if="web3Account && !domain" />
+
           <NavbarExtras />
         </div>
       </div>
@@ -110,12 +46,4 @@ watchEffect(() => {
     <LoadingSpinner fill-white class="mr-2" />
     {{ $tc('delegate.pendingTransaction', pendingCount) }}
   </div>
-  <teleport to="#modal">
-    <ModalAccount
-      :open="modalAccountOpen"
-      :profile="profile"
-      @close="modalAccountOpen = false"
-      @login="handleLogin"
-    />
-  </teleport>
 </template>
