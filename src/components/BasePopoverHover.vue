e9376e612c1a43b91d9a856efa51c776f0f3ff72
Sam <samuv.eth@gmail.com>
2022-08-03 21:01:46 +0700

    Refactor popover hover and remove popperjs

--- src/components/BasePopoverHover.vue
+++ src/components/BasePopoverHover.vue
@@ -1,64 +1,70 @@
 <script setup lang="ts">
-import { ref, computed, watch, onMounted } from 'vue';
-import { usePointer, debouncedWatch } from '@vueuse/core';
-import { createPopper, Placement } from '@popperjs/core';
+import { ref } from 'vue';
+import { Popover, PopoverButton, PopoverPanel } from '@headlessui/vue';
+import { Float } from '@headlessui-float/vue';
+import type { Placement } from '@floating-ui/dom';
 
-const props = defineProps<{
-  options: { placement: Placement; offset: number[] };
-}>();
-
-const open = ref(false);
-const contentHovered = ref(false);
-const itemHovered = ref(false);
-const hovered = computed(() => contentHovered.value || itemHovered.value);
-
-const { pointerType } = usePointer();
-
-debouncedWatch(
-  hovered,
-  () => {
-    open.value = hovered.value && pointerType.value === 'mouse';
-  },
-  { debounce: 500 }
+withDefaults(
+  defineProps<{
+    placement?: Placement;
+  }>(),
+  {
+    placement: 'bottom-end'
+  }
 );
 
-const itemref = ref<HTMLElement | null>(null);
-const contentref = ref(null);
+const show = ref(false);
+const timer = ref<any>(null);
 
-let popperInstance;
+const open = () => {
+  if (timer.value !== null) {
+    clearTimeout(timer.value);
+    timer.value = null;
+  }
+  show.value = true;
+};
 
-onMounted(() => {
-  if (!itemref.value || !contentref.value) return;
-  popperInstance = createPopper(itemref.value, contentref.value, {
-    placement: props.options.placement,
-    modifiers: [
-      {
-        name: 'offset',
-        options: {
-          offset: props.options.offset
-        }
-      }
-    ]
-  });
-});
-
-watch(open, () => {
-  popperInstance.setOptions({ placement: props.options.placement });
-});
+const delayClose = () => {
+  timer.value = setTimeout(() => {
+    show.value = false;
+  }, 150);
+};
 </script>
 
 <template>
-  <div ref="itemref" class="h-full">
-    <div @mouseenter="itemHovered = true" @mouseleave="itemHovered = false">
-      <slot name="item" />
-    </div>
-  </div>
-  <div v-show="open" ref="contentref" class="z-50" @click.prevent.self>
-    <div
-      @mouseenter="contentHovered = true"
-      @mouseleave="contentHovered = false"
-    >
-      <slot name="content" />
-    </div>
-  </div>
+  <Popover>
+    <Float :show="show" :placement="placement" :offset="10" :shift="8">
+      <PopoverButton
+        class="outline-none"
+        @mouseenter="open"
+        @mouseleave="delayClose"
+      >
+        <slot name="button" />
+      </PopoverButton>
+
+      <Transition
+        enter-active-class="transition ease-out duration-100"
+        enter-from-class="transform opacity-0 scale-95"
+        enter-to-class="transform opacity-100 scale-100"
+        leave-active-class="transition ease-in duration-75"
+        leave-from-class="transform opacity-100 scale-100"
+        leave-to-class="transform opacity-0 scale-95"
+      >
+        <PopoverPanel
+          class="z-20 w-screen outline-none sm:max-w-sm"
+          static
+          @mouseenter="open"
+          @mouseleave="delayClose"
+        >
+          <div class="overflow-hidden rounded-2xl bg-skin-header-bg shadow-lg">
+            <div
+              class="no-scrollbar max-h-[85vh] overflow-y-auto overscroll-contain"
+            >
+              <slot name="content" />
+            </div>
+          </div>
+        </PopoverPanel>
+      </Transition>
+    </Float>
+  </Popover>
 </template>
