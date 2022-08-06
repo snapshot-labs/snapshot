05fbdbc7aac3ba434c5b23ea417b8612cbcb4192
Sam <samuv.eth@gmail.com>
2022-08-06 14:37:08 +0700

    Refactor

--- src/components/SpaceSidebarFooter.vue
+++ src/components/SpaceSidebarFooter.vue
@@ -4,32 +4,33 @@ import { sanitizeUrl } from '@braintree/sanitize-url';
 
 const props = defineProps<{ space?: Record<string, any> }>();
 
-type SocialLink = { icon: string; link: string };
-const socials = computed<SocialLink[]>(() => {
-  const socials: SocialLink[] = [];
+type SocialItem = { icon: string; link: string };
+
+const socials = computed<SocialItem[]>(() => {
+  const socialsArray: SocialItem[] = [];
 
   if (props.space?.twitter) {
-    socials.push({
+    socialsArray.push({
       icon: 'twitter',
       link: `https://twitter.com/${props.space?.twitter}`
     });
   }
 
   if (props.space?.github) {
-    socials.push({
+    socialsArray.push({
       icon: 'github',
       link: `https://github.com/${props.space?.github}`
     });
   }
 
   if (props.space?.website) {
-    socials.push({
+    socialsArray.push({
       icon: 'earth',
       link: sanitizeUrl(props.space?.website)
     });
   }
 
-  return socials;
+  return socialsArray;
 });
 </script>
 
