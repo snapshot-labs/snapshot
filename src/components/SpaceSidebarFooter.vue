5d30546477014edba4a91498d0c633102962812c
Sam <samuv.eth@gmail.com>
2022-08-06 14:18:18 +0700

    Fix space sidebar social icons

--- src/components/SpaceSidebarFooter.vue
+++ src/components/SpaceSidebarFooter.vue
@@ -35,11 +35,16 @@ const socials = computed<SocialLink[]>(() => {
 
 <template>
   <div class="my-3 flex items-center space-x-3 px-4">
-    <IconSocial
+    <BaseLink
       v-for="social in socials"
       :key="social.icon"
-      :social="social"
-      class="flex"
-    />
+      :link="social.link"
+      class="text-md text-skin-text hover:text-skin-link"
+      hide-external-icon
+    >
+      <i-s-twitter v-if="social.icon === 'twitter'" class="text-[23px]" />
+      <i-s-github v-if="social.icon === 'github'" />
+      <i-ho-globe v-if="social.icon === 'earth'" class="text-[23px]" />
+    </BaseLink>
   </div>
 </template>
