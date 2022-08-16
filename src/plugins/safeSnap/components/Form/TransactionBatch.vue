8f2da5d766b6bd98356b335ca322bdb5ffafe17e
mktcode <kontakt@markus-kottlaender.de>
2022-08-16 15:28:13 +0200

    converted component

--- src/plugins/safeSnap/components/Form/TransactionBatch.vue
+++ src/plugins/safeSnap/components/Form/TransactionBatch.vue
@@ -1,113 +1,108 @@
-<script>
+<script setup lang="ts">
 import { clone } from '@snapshot-labs/snapshot.js/src/utils';
 import { useSafe } from '@/composables';
-import chevronIcon from '@/assets/icons/chevron.svg';
 import { createBatch } from '@/plugins/safeSnap/utils';
 import { ERC20_ABI, ERC721_ABI } from '@/plugins/safeSnap/constants';
 import { formatEther } from '@ethersproject/units';
 import { FunctionFragment, Interface } from '@ethersproject/abi';
 import SafeSnapFormTransaction from './Transaction.vue';
+import { onMounted, ref } from 'vue';
 
-export default {
-  components: { SafeSnapFormTransaction },
-  props: ['modelValue', 'nonce', 'config'],
-  emits: ['update:modelValue', 'remove'],
-  setup() {
-    const { safesnap } = useSafe();
-    return { safesnap };
-  },
-  data() {
-    return {
-      open: true,
-      hashHidden: true,
-      jsonHidden: true,
-      batch: this.modelValue,
-      transactions: this.modelValue ? clone(this.modelValue.transactions) : [],
-      chevronIcon
-    };
-  },
-  async mounted() {
-    if (!this.config.preview && !this.transactions.length) {
-      // Add transaction if batch is empty
-      this.addTransaction();
-    }
-  },
-  methods: {
-    addTransaction() {
-      this.transactions.push(undefined);
-    },
-    updateTransaction(index, transaction) {
-      if (this.config.preview) return;
-      this.transactions[index] = transaction;
-      this.updateBatch(this.transactions);
-    },
-    removeTransaction(index) {
-      this.transactions.splice(index, 1);
-      this.updateBatch(this.transactions);
-      if (!this.transactions.length) {
-        this.$emit('remove');
-      }
-    },
-    updateBatch(txs) {
-      const batch = this.createBatch(this.nonce, txs);
-      this.$emit('update:modelValue', batch);
-    },
-    createBatch(nonce, txs) {
-      const chainId = parseInt(this.config.network);
-      return createBatch(
-        this.config.realityAddress,
-        chainId,
-        nonce,
-        txs,
-        this.config.multiSendAddress
-      );
-    },
-    formatBatchJson(txs) {
-      const valid = txs.every(tx => tx);
-      if (!valid) {
-        return null;
-      }
-      return txs.map(tx => {
-        const base = {
-          to: tx.to,
-          operation: tx.operation,
-          value: formatEther(tx.value)
-        };
+const props = defineProps(['modelValue', 'nonce', 'config']);
+const emit = defineEmits(['update:modelValue', 'remove']);
 
-        let abi = tx.abi;
-        if (tx.data.length > 2) {
-          switch (tx.type) {
-            case 'transferFunds':
-              abi = ERC20_ABI;
-              break;
-            case 'transferNFT':
-              abi = ERC721_ABI;
-              break;
-            default:
-              base.data = tx.data;
-              break;
-          }
-        }
+const { safesnap } = useSafe();
 
-        if (abi) {
-          const signHash = tx.data.substr(0, 10);
-          const contractInterface = new Interface(abi);
-          const functionFragment = contractInterface.fragments
-            .filter(frag => FunctionFragment.isFunctionFragment(frag))
-            .find(frag => contractInterface.getSighash(frag) === signHash);
-          const func = FunctionFragment.from(functionFragment);
-          const params = contractInterface.decodeFunctionData(func, tx.data);
-          return {
-            ...base,
-            method: func.format(),
-            params: params.map(param => param.toString())
-          };
-        }
-        return base;
-      });
-    }
+const open = ref(true);
+const hashHidden = ref(true);
+const jsonHidden = ref(true);
+const transactions = ref(
+  props.modelValue ? clone(props.modelValue.transactions) : []
+);
+
+const addEmptyTransaction = () => {
+  transactions.value.push(undefined);
+};
+
+const updateBatch = txs => {
+  const batch = createBatch(
+    props.config.realityAddress,
+    parseInt(props.config.network),
+    props.nonce,
+    txs,
+    props.config.multiSendAddress
+  );
+  emit('update:modelValue', batch);
+};
+
+const updateTransaction = (index, transaction) => {
+  if (props.config.preview) return;
+  transactions.value[index] = transaction;
+  updateBatch(transactions.value);
+};
+
+const removeTransaction = index => {
+  transactions.value.splice(index, 1);
+  updateBatch(transactions.value);
+  if (!transactions.value.length) {
+    emit('remove');
   }
 };
+
+const formatBatchJson = txs => {
+  const valid = txs.every(tx => tx);
+  if (!valid) {
+    return null;
+  }
+  return txs.map(tx => {
+    const base = {
+      to: tx.to,
+      operation: tx.operation,
+      value: formatEther(tx.value),
+      data: ''
+    };
+
+    let abi = tx.abi;
+    if (tx.data.length > 2) {
+      switch (tx.type) {
+        case 'transferFunds':
+          abi = ERC20_ABI;
+          break;
+        case 'transferNFT':
+          abi = ERC721_ABI;
+          break;
+        default:
+          base.data = tx.data;
+          break;
+      }
+    }
+
+    if (abi) {
+      const functionSelector = tx.data.substr(0, 10);
+      const contractInterface = new Interface(abi);
+      const functionFragment = contractInterface.fragments
+        .filter(frag => FunctionFragment.isFunctionFragment(frag))
+        .find(frag => contractInterface.getSighash(frag) === functionSelector);
+
+      if (functionFragment) {
+        const func = FunctionFragment.from(functionFragment);
+        const params = contractInterface.decodeFunctionData(func, tx.data);
+        return {
+          ...base,
+          method: func.format(),
+          params: params.map(param => param.toString())
+        };
+      }
+    }
+    return base;
+  });
+};
+
+onMounted(() => {
+  if (!props.config.preview && !transactions.value.length) {
+    addEmptyTransaction();
+  }
+});
 </script>
 
 <template>
@@ -163,7 +158,11 @@ export default {
       <span class="!text-red"> Error: {{ safesnap.batchError.message }}</span>
     </BaseBlock>
 
-    <BaseButton v-if="!config.preview" class="mt-2" @click="addTransaction">
+    <BaseButton
+      v-if="!config.preview"
+      class="mt-2"
+      @click="addEmptyTransaction"
+    >
       {{ $t('safeSnap.addTransaction') }}
     </BaseButton>
   </UiCollapsible>
