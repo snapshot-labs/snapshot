0ceeba751dca40c94968f06ea3d4e732a60cc4ef
Sam <51686767+samuveth@users.noreply.github.com>
2022-08-27 12:45:22 +0700

    Add stargazer (#3058)
    * added stargazer wallet and logo
    
    * Bump @snapshot-labs/lock
    
    * Remove torus
    
    Co-authored-by: Juan David Suarez <juansuarezespitia@gmail.com>
    Co-authored-by: Juan David Suárez <64592107+juandavidkincaid@users.noreply.github.com>

--- src/helpers/auth.ts
+++ src/helpers/auth.ts
@@ -1,18 +1,18 @@
 import injected from '@snapshot-labs/lock/connectors/injected';
 import walletconnect from '@snapshot-labs/lock/connectors/walletconnect';
-import torus from '@snapshot-labs/lock/connectors/torus';
 import portis from '@snapshot-labs/lock/connectors/portis';
 import connectors from '@/helpers/connectors.json';
 import walletlink from '@snapshot-labs/lock/connectors/walletlink';
 import gnosis from '@snapshot-labs/lock/connectors/gnosis';
+import stargazer from '@snapshot-labs/lock/connectors/stargazer';
 
 const options: any = { connectors: [] };
 const lockConnectors = {
   injected,
   walletconnect,
-  torus,
   walletlink,
   portis,
+  stargazer,
   gnosis
 };
 
