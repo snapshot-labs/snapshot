60ff7fc979516022472b3d6b2cb0dafb6cfc73b7
Sam <samuv.eth@gmail.com>
2022-08-27 13:54:09 +0700

    Add check to approval and single choice

--- src/components/SpaceProposalVoteApproval.vue
+++ src/components/SpaceProposalVoteApproval.vue
@@ -27,7 +27,7 @@ function selectChoice(i) {
     <BaseButton
       v-for="(choice, i) in proposal.choices"
       :key="i"
-      class="mb-2 block w-full"
+      class="relative mb-2 block w-full"
       :class="{
         '!border-skin-link': selectedChoices.includes(i + 1),
         'border-skin-border hover:border-skin-link': !selectedChoices.includes(
@@ -36,6 +36,7 @@ function selectChoice(i) {
       }"
       @click="selectChoice(i + 1)"
     >
+      <i-ho-check v-if="selectedChoices.includes(i + 1)" class="absolute" />
       {{ shorten(choice, 32) }}
     </BaseButton>
   </div>
