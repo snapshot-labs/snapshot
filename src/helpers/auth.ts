52d6943f73bf147c5a813053c1eeb9875b9968e9
Sam <samuv.eth@gmail.com>
2022-08-27 12:43:23 +0700

    Remove torus

--- src/helpers/auth.ts
+++ src/helpers/auth.ts
@@ -1,6 +1,5 @@
 import injected from '@snapshot-labs/lock/connectors/injected';
 import walletconnect from '@snapshot-labs/lock/connectors/walletconnect';
-import torus from '@snapshot-labs/lock/connectors/torus';
 import portis from '@snapshot-labs/lock/connectors/portis';
 import connectors from '@/helpers/connectors.json';
 import walletlink from '@snapshot-labs/lock/connectors/walletlink';
@@ -11,7 +10,6 @@ const options: any = { connectors: [] };
 const lockConnectors = {
   injected,
   walletconnect,
-  torus,
   walletlink,
   portis,
   stargazer,
