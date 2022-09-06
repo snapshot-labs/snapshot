b87e8f2f6a31b255eb76974df3b91941934d4116
Sam <samuv.eth@gmail.com>
2022-09-06 15:35:16 +0700

    Make image the link

--- src/components/ProposalResultsShutter.vue
+++ src/components/ProposalResultsShutter.vue
@@ -3,15 +3,16 @@
     <div class="-mb-2 mt-2 text-xs">{{ $t('poweredBy') }}</div>
 
     <div class="flex items-center">
-      <div
+      <BaseLink
         v-tippy="{
           content: $t('privacy.shutter.tooltip')
         }"
-        class="inline-block cursor-help"
+        :link="$t('privacy.shutter.url')"
+        hide-external-icon
+        class="inline-block cursor-pointer"
       >
         <i-s-shutter class="h-[36px] text-[70px]" />
-      </div>
-      <BaseLink :link="$t('privacy.shutter.url')" />
+      </BaseLink>
     </div>
   </div>
 </template>
