f8f80b5160dcd4da30da35b4df2332aaa2231207
mktcode <kontakt@markus-kottlaender.de>
2022-09-29 18:12:19 +0200

    slightly improved transaction display

--- src/components/TransactionBuilderDisplayTransaction.vue
+++ src/components/TransactionBuilderDisplayTransaction.vue
@@ -1,14 +1,33 @@
 <script setup lang="ts">
-import { Transaction } from '@/helpers/transactionBuilder';
+import { computed } from 'vue';
+import { shortenAddress } from '@/helpers/utils';
+import {
+  detectTransactionForm,
+  Transaction,
+  TransactionForms
+} from '@/helpers/transactionBuilder';
 
-defineProps<{
+const props = defineProps<{
   transaction: Transaction;
 }>();
+
+const transactionDisplayType = computed<TransactionForms>(() => {
+  return detectTransactionForm(props.transaction);
+});
 </script>
 
 <template>
-  <div>
-    <div>To: {{ transaction.to }}</div>
-    <div>Value: {{ transaction.value }}</div>
+  <div class="flex">
+    <div class="mr-2 flex w-5 grow items-center justify-center">
+      <i-ho-database v-if="transactionDisplayType === TransactionForms.FUNDS" />
+      <i-ho-photograph v-if="transactionDisplayType === TransactionForms.NFT" />
+      <i-s-smart-contract
+        v-if="transactionDisplayType === TransactionForms.CONTRACT"
+      />
+    </div>
+    <div>
+      <div>To: {{ shortenAddress(transaction.to) }}</div>
+      <div>Value: {{ transaction.value }}</div>
+    </div>
   </div>
 </template>
