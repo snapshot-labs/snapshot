0f454bd69d1caf4cd181a39d3758ab3ecc4df5f2
twodam <twodam@protonmail.com>
2022-10-04 14:27:38 +0800

    Revert "Restore changes from develop branch"
    This reverts commit 9ce757ac9505ce991377554b0e3b593e704b1422.

--- src/components/ModalConfirm.vue
+++ src/components/ModalConfirm.vue
@@ -87,9 +87,9 @@ watch(
   >
     <div class="flex flex-auto flex-col">
       <h4 class="m-4 mb-0 text-center">
-        {{ $tc('voteOverview') }}
+        {{ $tc('proposal.castVote') }}
       </h4>
-      <BaseBlock slim class="m-4 p-4 text-skin-link">
+      <div slim class="m-4 text-skin-link">
         <div class="flex">
           <span class="mr-1 flex-auto text-skin-text" v-text="$t('options')" />
           <span
@@ -159,7 +159,7 @@ watch(
           />
         </div>
         <div v-if="vpLoadingFailed" class="mt-3">{{ t('vpError') }}</div>
-      </BaseBlock>
+      </div>
     </div>
     <template #footer>
       <div class="float-left w-2/4 pr-2">
@@ -176,7 +176,7 @@ watch(
           primary
           @click="handleSubmit"
         >
-          {{ $t('proposal.vote') }}
+          {{ $t('confirm') }}
         </BaseButton>
       </div>
     </template>
