dd8f30962b8c9b55d656041bacc8193cc4bc0c2e
Karamorf <karamorf@proton.me>
2022-11-03 16:59:12 -0700

    Update useWeb3.ts

--- src/composables/useWeb3.ts
+++ src/composables/useWeb3.ts
@@ -40,7 +40,10 @@ export function useWeb3() {
 
   async function loadProvider() {
     try {
-      if (auth.provider.value.removeAllListeners)
+      if (
+        auth.provider.value.removeAllListeners &&
+        !auth.provider.value.isTorus
+        )
         auth.provider.value.removeAllListeners();
       if (auth.provider.value.on) {
         try {
