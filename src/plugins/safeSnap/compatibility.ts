b6ff23c404eebbf97b6c60c5106b09f738a2cc74
mktcode <kontakt@markus-kottlaender.de>
2022-11-09 12:30:29 +0100

    execution data compatibility

--- src/plugins/safeSnap/compatibility.ts
+++ src/plugins/safeSnap/compatibility.ts
@@ -4,6 +4,7 @@ import {
   SafeModuleType,
   SafeType
 } from '@/helpers/safe';
+import { BigNumber } from '@ethersproject/bignumber';
 
 export function mapLegacyConfig(config: any): SafeConfig[] {
   if (config[0]?.safe) {
@@ -30,9 +31,11 @@ export function mapLegacyConfig(config: any): SafeConfig[] {
 
 interface LegacyExecutionData {
   safes: {
-    txs: {
-      transactions: Record<string, any>[];
-    }[];
+    txs:
+      | {
+          transactions: Record<string, any>[];
+        }[]
+      | [Record<string, any>[]];
     network: string;
     realityAddress: string;
   }[];
@@ -69,31 +72,65 @@ function isExecutionData(
   );
 }
 
+/**
+ * Examples:
+ *
+ * https://snapshot.org/#/gnosis.eth/proposal/0x41af607ec45f147b6f0196353f29c5e25951097c64c3a0a703bc519bd1883578
+ * https://snapshot.org/#/gnosis.eth/proposal/0x75bd61f48bb1cc4c9e2dd4938036095d581a99f2c186cb0a0c83914253f45634
+ * https://snapshot.org/#/gnosis.eth/proposal/0x9b12a093e17e92b56d070ed876883d8c2331678ca3945e44f66dd416cfd47a64
+ * https://snapshot.org/#/gnosis.eth/proposal/0x8f8d2118ef3a8fa9f8b1814ce2ae34ae8776548716a45ce69082f3e266465f48
+ */
 export function mapLegacyExecutionData(
   unknownExecutionData: unknown
 ): ExecutionData[] {
   if (isExecutionData(unknownExecutionData)) {
     return unknownExecutionData;
   } else if (isLegacyExecutionData(unknownExecutionData)) {
-    return unknownExecutionData.safes.map(legacyExecutionSet => {
-      return {
+    return unknownExecutionData.safes.map((legacyExecutionSet, index) => {
+      const abis = {};
+      const mappedExecutionSet: ExecutionData = {
         safe: {
-          name: 'Main Safe',
+          name: `Safe #${index + 1}`,
           type: SafeType.GNOSIS,
-          address: '',
+          address: '', // TODO: get address from safe
           network: legacyExecutionSet.network
         },
         module: {
           type: SafeModuleType.REALITY,
           address: legacyExecutionSet.realityAddress
         },
-        batches: legacyExecutionSet.txs.map(tx => {
-          // tx.transactions
-          return []; // TODO: implement encoding
+        batches: legacyExecutionSet.txs.map(batch => {
+          let legacyTransactions;
+          if (Array.isArray(batch)) {
+            legacyTransactions = batch;
+          } else if (Array.isArray(batch.transactions)) {
+            legacyTransactions = batch.transactions;
+          }
+
+          if (!legacyTransactions) {
+            throw new Error('Invalid legacy execution data');
+          }
+
+          return legacyTransactions.map(transaction => {
+            if (transaction.abi) {
+              abis[transaction.to] = transaction.abi;
+            }
+
+            return {
+              to: transaction.to,
+              data: transaction.data,
+              value: BigNumber.from(transaction.value),
+              operation: Number(transaction.operation)
+            };
+          });
         })
       };
+
+      mappedExecutionSet.abis = abis;
+
+      return mappedExecutionSet;
     });
   }
 
-  throw new Error('Invalid execution data');
+  throw new Error('Unknown execution data format.');
 }
