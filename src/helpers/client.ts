2817f7aeec68425c67b56d5c3b1834e0fbbdb450
Sam <51686767+samuveth@users.noreply.github.com>
2022-11-14 16:03:09 +0700

    feat: Remove personal sign  (#3073)
    * Remove personal sign
    
    * Update Lock
    
    * Update lock
    
    * Update lock
    
    * Fix
    
    * Update lock
    
    * Remove personal sign
    
    Co-authored-by: Fabien <bonustrack@users.noreply.github.com>

--- src/helpers/client.ts
+++
@@ -1,6 +0,0 @@
-import Client from '@snapshot-labs/snapshot.js/src/client';
-
-const hubUrl = import.meta.env.VITE_HUB_URL || 'https://testnet.snapshot.org';
-const client = new Client(hubUrl);
-
-export default client;
--- src/helpers/clientGnosisSafe.ts
+++ src/helpers/clientGnosisSafe.ts
@@ -1,4 +1,4 @@
-import Client from '@snapshot-labs/snapshot.js/src/client';
+import Client from '@snapshot-labs/snapshot.js/src/sign';
 
 const relayerUrl =
   import.meta.env.VITE_RELAYER_URL || 'https://testnet.snapshot.org';
