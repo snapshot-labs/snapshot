e9794bed05810f5b38827aa58fd40ba77298fcb2
bonustrack <fabien@bonustrack.co>
2022-12-01 03:47:35 +0700

    hotfix: remove text autolinker

--- src/components/TextAutolinker.vue
+++ src/components/TextAutolinker.vue
@@ -1,23 +1,10 @@
 <script setup lang="ts">
-import { computed } from 'vue';
-import Autolinker from 'autolinker';
-
-interface Props {
+const props = defineProps<{
   text: string;
-  truncate?: number;
-}
-
-const props = withDefaults(defineProps<Props>(), {
-  truncate: 0
-});
-
-const textWithLinks = computed(() =>
-  Autolinker.link(props.text, {
-    truncate: props.truncate
-  })
-);
+  truncate: number;
+}>();
 </script>
 
 <template>
-  <span v-html="textWithLinks" />
+  <span v-text="props.text.slice(0, props.truncate)" />
 </template>
