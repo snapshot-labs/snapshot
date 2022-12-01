9daef51cc0e2f38fd4c3a8cf7bed9466cf2a1c56
Sam <51686767+samuveth@users.noreply.github.com>
2022-12-01 16:47:34 +0700

    Debounce route update on search (#3375)

--- src/components/BaseSearch.vue
+++ src/components/BaseSearch.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import { useRouter } from 'vue-router';
 import debounce from 'lodash/debounce';
 
@@ -11,18 +12,26 @@ const emit = defineEmits(['update:modelValue']);
 
 const router = useRouter();
 
-function handleInput(e) {
-  const input = e.target.value;
+const input = ref(props.modelValue || '');
+
+function updateRouteQuery() {
   if (!props.modal) {
     const { query } = router.currentRoute.value;
     router.push({
-      query: input ? { ...query, q: input } : { ...query, q: undefined }
+      query: input.value
+        ? { ...query, q: input.value }
+        : { ...query, q: undefined }
     });
   }
-  emit('update:modelValue', input);
 }
 
-const handleInputDebounce = debounce(handleInput, 100);
+const debouncedUpdateRouteQuery = debounce(updateRouteQuery, 300);
+
+function handleInput(e) {
+  input.value = e.target.value;
+  debouncedUpdateRouteQuery();
+  emit('update:modelValue', input.value);
+}
 
 function clearInput() {
   if (!props.modal) {
@@ -40,13 +49,13 @@ function clearInput() {
   >
     <i-ho-search class="mr-2 text-[19px]" />
     <input
-      :value="modelValue"
+      :value="input"
       :placeholder="placeholder"
       type="text"
       autocorrect="off"
       autocapitalize="none"
       class="input w-full flex-auto border-none"
-      @input="handleInputDebounce"
+      @input="handleInput"
     />
     <a @click="clearInput">
       <BaseIcon v-if="modelValue" name="close" size="12" class="mb-1" />
