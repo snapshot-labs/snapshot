bed963e4378a4b05624017dad736b7577d06e35e
Sam <samuv.eth@gmail.com>
2022-12-01 15:39:35 +0700

    Fix truncate optional

--- src/components/TextAutolinker.vue
+++ src/components/TextAutolinker.vue
@@ -1,10 +1,16 @@
 <script setup lang="ts">
+import { computed } from 'vue';
+
 const props = defineProps<{
   text: string;
-  truncate: number;
+  truncate?: number;
 }>();
+
+const spanText = computed(() =>
+  props.truncate ? props.text.slice(0, props.truncate) : props.text
+);
 </script>
 
 <template>
-  <span v-text="props.text.slice(0, props.truncate)" />
+  <span v-text="spanText" />
 </template>
