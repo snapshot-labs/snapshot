981002b223ebf675ab9e7379bb77a64c425681e9
Sam <samuv.eth@gmail.com>
2023-02-12 12:29:49 +0700

    Create description in useMeta

--- src/composables/useMeta.ts
+++ src/composables/useMeta.ts
@@ -15,16 +15,25 @@ type metaInfo = {
 export function useMeta(metaInfo: metaInfo) {
   const { t } = useI18n();
 
+  function createMetaDescription() {
+    if (document.querySelector('meta[name="description"]')) return;
+    const meta = document.createElement('meta');
+    meta.name = 'description';
+    document.getElementsByTagName('head')[0].appendChild(meta);
+  }
+
   onMounted(() => {
     if (metaInfo.title.key)
       document.title = t(metaInfo.title.key, metaInfo.title.params || {});
-    if (metaInfo.description.key)
+    if (metaInfo.description.key) {
+      createMetaDescription();
       document
         .querySelector('meta[name="description"]')
         ?.setAttribute(
           'content',
           t(metaInfo.description.key, metaInfo.description.params || {})
         );
+    }
   });
 
   return {};
