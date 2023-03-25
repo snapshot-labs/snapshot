94efec636a35a873ee6a22f3cf1bde0d875e8614
Sam <samuv.eth@gmail.com>
2023-03-25 15:07:38 +0700

    Fixes

--- src/components/FormArrayStrategies.vue
+++ src/components/FormArrayStrategies.vue
@@ -52,47 +52,53 @@ onMounted(() => {
     <div v-if="!strategies.length" class="flex justify-center">
       <LoadingSpinner />
     </div>
-    <div v-else class="mt-3 space-y-3">
-      <div
-        v-for="(property, i) in input"
-        :key="i"
-        class="space-y-2 rounded-md border p-3"
-      >
-        <div class="mb-3 flex items-center justify-between">
-          <BasePill class="text-[18px]">
-            {{ i + 1 }}
-          </BasePill>
-          <BaseButtonIcon v-if="input.length > 1" @click="handleDelete(i)">
-            <i-ho-trash class="text-[18px]" />
-          </BaseButtonIcon>
+    <div v-else class="mt-3">
+      <LabelInput>
+        {{ $t('strategies') }}
+      </LabelInput>
+      <div class="space-y-3">
+        <div
+          v-for="(property, i) in input"
+          :key="i"
+          class="space-y-2 rounded-md border p-3"
+        >
+          <div class="mb-3 flex items-center justify-between">
+            <BasePill class="text-[16px]">
+              {{ i + 1 }}
+            </BasePill>
+            <BaseButtonIcon v-if="input.length > 1" @click="handleDelete(i)">
+              <i-ho-trash class="text-[17px]" />
+            </BaseButtonIcon>
+          </div>
+          <BaseCombobox
+            :label="$t('strategy')"
+            :items="
+              filterStrategies(searchInput).map(s => ({ id: s.id, name: s.id }))
+            "
+            :selected-id="input[i].name"
+            @select="value => (input[i].name = value.id)"
+            @search="value => (searchInput = value)"
+          />
+          <ComboboxNetwork
+            :network="input[i].network"
+            @select="value => (input[i].network = value)"
+          />
+          <FormObjectStrategyParams
+            v-model="input[i].params"
+            :strategy-name="input[i].name"
+            @update:is-valid="strategyValidationStates[i] = $event"
+          />
         </div>
-        <BaseCombobox
-          :label="$t('strategy')"
-          :items="
-            filterStrategies(searchInput).map(s => ({ id: s.id, name: s.id }))
+
+        <BaseButton
+          class="mt-3 w-full"
+          @click="
+            input.push({ name: 'erc20-balance-of', network: '1', params: {} })
           "
-          :selected-id="input[i].name"
-          @select="value => (input[i].name = value.id)"
-          @search="value => (searchInput = value)"
-        />
-        <ComboboxNetwork
-          :network="input[i].network"
-          @select="value => (input[i].network = value)"
-        />
-        <FormObjectStrategyParams
-          v-model="input[i].params"
-          :strategy-name="input[i].name"
-          @update:is-valid="strategyValidationStates[i] = $event"
-        />
+        >
+          {{ $t('addStrategy') }}
+        </BaseButton>
       </div>
-      <BaseButton
-        class="mt-3 w-full"
-        @click="
-          input.push({ name: 'erc20-balance-of', network: '1', params: {} })
-        "
-      >
-        {{ $t('addStrategy') }}
-      </BaseButton>
     </div>
   </div>
 </template>
