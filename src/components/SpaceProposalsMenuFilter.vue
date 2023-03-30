68e164fd3becb20f50aa9c2b1a94e8266a0aa044
j0hnfl0w <ij69@pm.me>
2023-03-30 17:51:49 +0300

    feat: search bar for proposals

--- src/components/SpaceProposalsMenuFilter.vue
+++ src/components/SpaceProposalsMenuFilter.vue
@@ -1,49 +1,77 @@
 <script setup lang="ts">
 const router = useRouter();
 const route = useRoute();
+const { t } = useI18n();
 
-function setStateFilter(name: string) {
+const routeQuery = computed(() => (route.query.q as string) || '');
+const stateFilter = computed(() => route.query.state || 'all');
+const searchOptions = computed(() => [
+  {
+    text: t('proposals.states.all'),
+    action: 'all',
+    extras: { selected: stateFilter.value === 'all' }
+  },
+  {
+    text: t('proposals.states.active'),
+    action: 'active',
+    extras: { selected: stateFilter.value === 'active' }
+  },
+  {
+    text: t('proposals.states.pending'),
+    action: 'pending',
+    extras: { selected: stateFilter.value === 'pending' }
+  },
+  {
+    text: t('proposals.states.closed'),
+    action: 'closed',
+    extras: { selected: stateFilter.value === 'closed' }
+  },
+  {
+    text: t('proposals.states.core'),
+    action: 'core',
+    extras: { selected: stateFilter.value === 'core' }
+  }
+]);
+
+const searchSelectedOption = computed(
+  () =>
+    searchOptions.value.find(option => option.action === route.query.state)
+      ?.text ?? t('proposals.states.all')
+);
+
+function redirectSearch(e: string) {
   router.push({
-    query: {
-      ...route.query,
-      ['state']: name
-    }
+    query: { q: routeQuery.value || undefined, state: e }
   });
 }
-
-const stateFilter = computed(() => route.query.state || 'all');
 </script>
 
 <template>
-  <BaseMenu
-    :items="[
-      {
-        text: $t('proposals.states.all'),
-        action: 'all',
-        extras: { selected: stateFilter === 'all' }
-      },
-      {
-        text: $t('proposals.states.active'),
-        action: 'active',
-        extras: { selected: stateFilter === 'active' }
-      },
-      {
-        text: $t('proposals.states.pending'),
-        action: 'pending',
-        extras: { selected: stateFilter === 'pending' }
-      },
-      {
-        text: $t('proposals.states.closed'),
-        action: 'closed',
-        extras: { selected: stateFilter === 'closed' }
-      },
-      {
-        text: $t('proposals.states.core'),
-        action: 'core',
-        extras: { selected: stateFilter === 'core' }
-      }
-    ]"
-    :selected="$t(`proposals.states.${stateFilter}`)"
-    @select="setStateFilter"
-  />
+  <BaseButton
+    class="w-full pl-3 pr-0 focus-within:!border-skin-link md:max-w-[420px]"
+  >
+    <div class="flex">
+      <BaseSearch
+        :model-value="routeQuery"
+        :placeholder="$t('searchPlaceholder')"
+        class="flex-auto pr-2"
+      />
+      <div class="flex items-center border-l" style="height: 44px">
+        <BaseMenu
+          :items="searchOptions"
+          :selected="$t(`proposals.states.${stateFilter}`)"
+          @select="redirectSearch"
+        >
+          <template #button>
+            <div class="flex h-full flex-grow items-center">
+              <span class="ml-3" v-text="searchSelectedOption" />
+              <i-ho-chevron-down
+                class="ml-1 mr-[12px] text-xs text-skin-text"
+              />
+            </div>
+          </template>
+        </BaseMenu>
+      </div>
+    </div>
+  </BaseButton>
 </template>
