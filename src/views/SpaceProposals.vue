4085788c0cb5bde17fcee2dfc33f1ac7d25a64d6
Sam <samuv.eth@gmail.com>
2023-04-01 14:57:22 +0700

    Fix spacing

--- src/views/SpaceProposals.vue
+++ src/views/SpaceProposals.vue
@@ -147,7 +147,7 @@ onMounted(() => loadProposals());
         class="mt-2"
         :space="space"
       />
-      <div v-else class="my-4 space-y-4">
+      <div v-else class="mb-4 space-y-4">
         <BaseBlock
           v-for="(proposal, i) in spaceProposals"
           :key="i"
