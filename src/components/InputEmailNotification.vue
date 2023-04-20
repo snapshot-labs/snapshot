8aac6c29faafc706de906526f956f748274dc1b2
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2023-04-20 10:33:35 +0400

    feat: show subscription result in modal

--- src/components/InputEmailNotification.vue
+++ src/components/InputEmailNotification.vue
@@ -5,27 +5,48 @@ const props = defineProps<{
   address: string;
 }>();
 
-const email = ref(null);
+const email = ref('');
+const modalOpen = ref(false);
+const modalLevel = ref('info');
+const modalMessage = ref('');
+const loading = ref(false);
 
 async function subscribe() {
-  const response = await fetch('https://envelop.fyi', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      params: {
-        email: email.value,
-        address: props.address
+  loading.value = true;
+
+  try {
+    const response = await fetch('https://envelop.fyi', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
       },
-      method: 'snapshot.subscribe'
-    })
-  });
+      body: JSON.stringify({
+        params: {
+          email: email.value,
+          address: props.address
+        },
+        method: 'snapshot.subscribe'
+      })
+    });
 
-  const result = await response.json();
+    const result = await response.json();
 
-  if (result.result === 'OK') {
-    console.log('OK');
+    if (result.result === 'OK') {
+      modalLevel.value = 'info';
+      modalMessage.value =
+        'You have been subscribed to email notifications. You will receive an email shortly with a verification link in order to confirm your subscription.';
+    } else {
+      modalLevel.value = 'warning';
+      modalMessage.value =
+        'An error occured while adding your email to the subscriptions list';
+    }
+  } catch (e) {
+    modalLevel.value = 'warning';
+    modalMessage.value =
+      'Unable to subscribe to email notifications, please try again later';
+  } finally {
+    loading.value = false;
+    modalOpen.value = true;
   }
 }
 </script>
@@ -37,8 +58,16 @@ async function subscribe() {
         type="submit"
         class="absolute right-0 h-[42px] rounded-r-full px-3"
       >
-        <i-ho-paper-airplane class="rotate-90 text-skin-link" />
+        <LoadingSpinner v-if="loading" />
+        <i-ho-paper-airplane v-else class="rotate-90 text-skin-link" />
       </button>
     </InputEmail>
   </form>
+  <ModalMessage
+    :open="modalOpen"
+    :level="modalLevel"
+    :message="modalMessage"
+    title="Email notifications"
+    @close="modalOpen = false"
+  />
 </template>
