a503daffde48a01f6032fbfdaf263c11636ef9c4
Sam <samuv.eth@gmail.com>
2023-04-22 18:15:55 +0700

    Use Tune for network and symbol

--- src/components/ComboboxNetwork.vue
+++ src/components/ComboboxNetwork.vue
@@ -1,18 +1,16 @@
 <script setup lang="ts">
 defineProps<{
   network: string;
-  information?: string;
-  isDisabled?: boolean;
+  hint?: string;
+  disabled?: boolean;
 }>();
 
 const emit = defineEmits(['select']);
 
-const query = ref('');
-
 const { filterNetworks } = useNetworksFilter();
 
 const networks = computed((): { id: string; name: string }[] => {
-  const filteredNetworks = filterNetworks(query.value).map(_n => ({
+  const filteredNetworks = filterNetworks().map(_n => ({
     id: _n.key,
     name: _n.name
   }));
@@ -22,21 +20,22 @@ const networks = computed((): { id: string; name: string }[] => {
 </script>
 
 <template>
-  <BaseCombobox
+  <TuneCombobox
     :label="$t('settings.network.label')"
     :items="networks"
-    :selected-id="network"
-    :information="information"
-    :is-disabled="isDisabled"
-    @select="value => emit('select', value.id)"
-    @search="value => (query = value)"
+    :model-value="network"
+    :hint="hint"
+    :disabled="disabled"
+    @update:model-value="value => emit('select', value)"
   >
     <template #item="{ item }">
-      <div class="truncate pr-2">
-        {{ item.name }}
-      </div>
+      <div class="flex items-center">
+        <div class="truncate pr-2">
+          {{ item.name }}
+        </div>
 
-      <BasePill class="leading-4"> #{{ item.id }} </BasePill>
+        <BasePill class="leading-4"> #{{ item.id }} </BasePill>
+      </div>
     </template>
-  </BaseCombobox>
+  </TuneCombobox>
 </template>
