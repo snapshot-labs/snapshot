474c748c465aed1ea4aefebef1ad29a44e382334
j0hnfl0w <ij69@pm.me>
2023-04-28 13:43:33 +0300

    fix: menu btn position

--- src/components/SpaceSidebarHeader.vue
+++ src/components/SpaceSidebarHeader.vue
@@ -16,20 +16,9 @@ const {
   subscriptions
 } = useSpaceSubscription(props.space.id);
 
-const { t } = useI18n();
 const { isFollowing } = useFollowSpace(props.space.id);
 
 const notificationIcon = ref('notifications-off');
-
-const threeDotItems = computed(() => {
-  const items = [{ text: t('report'), action: 'report' }];
-  return items;
-});
-
-function handleSelect(e) {
-  if (!props.space) return;
-  if (e === 'report') window.open('https://tally.so/r/mKzXo7', '_blank');
-}
 
 watchEffect(() => {
   if (subscriptions.value === undefined) {
@@ -45,25 +34,6 @@ watchEffect(() => {
   <div
     class="relative block px-4 pt-4 text-center md:flex lg:block lg:px-0 lg:pt-0"
   >
-    <div class="absolute -top-2 right-2">
-      <BaseMenu class="md:ml-2" :items="threeDotItems" @select="handleSelect">
-        <template #button>
-          <div>
-            <BaseButtonIcon :loading="isSending">
-              <i-ho-dots-horizontal />
-            </BaseButtonIcon>
-          </div>
-        </template>
-        <template #item="{ item }">
-          <div class="flex items-center gap-2">
-            <i-ho-document-duplicate v-if="item.action === 'duplicate'" />
-            <i-ho-flag v-if="item.action === 'report'" />
-            <i-ho-trash v-if="item.action === 'delete'" />
-            {{ item.text }}
-          </div>
-        </template>
-      </BaseMenu>
-    </div>
     <div class="flex lg:block">
       <AvatarSpace
         :space="space"
