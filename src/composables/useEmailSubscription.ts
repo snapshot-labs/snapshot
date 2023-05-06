7881d5c5f9411ede2365282cca1098e64e8c5792
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2023-05-07 03:38:23 +0400

    feat: require wallet ownership proof

--- src/composables/useEmailSubscription.ts
+++ src/composables/useEmailSubscription.ts
@@ -1,3 +1,5 @@
+import { getInstance } from '@snapshot-labs/lock/plugins/vue3';
+
 export function useEmailSubscription() {
   enum Status {
     wating,
@@ -11,17 +13,53 @@ export function useEmailSubscription() {
     'warning-red' = 'warning-red'
   }
 
+  const domain = {
+    name: 'snapshot',
+    version: '0.1.4'
+  };
+
   const status: Ref<Status> = ref(Status.wating);
   const postSubscribeLevel: Ref<Level> = ref(Level.info);
   const postSubscribeMessage = ref('');
   const loading = ref(false);
+  const auth = getInstance();
   const { t } = useI18n();
+  const { web3 } = useWeb3();
+
+  async function sign(
+    message: {
+      email: string;
+      from: string;
+      timestamp: number;
+    },
+    types: any
+  ) {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    const signer = auth.web3?.getSigner ? auth.web3.getSigner() : auth.web3;
+    return await signer._signTypedData(domain, types, message);
+  }
 
   async function subscribe(email: string, address: string) {
     loading.value = true;
 
     try {
-      const response = await fetch('https://envelop.fyi', {
+      const signature = await sign(
+        {
+          email,
+          from: web3.value.account,
+          timestamp: parseInt((Date.now() / 1e3).toFixed())
+        },
+        {
+          Subscribe: [
+            { name: 'from', type: 'address' },
+            { name: 'email', type: 'string' },
+            { name: 'timestamp', type: 'uint64' }
+          ]
+        }
+      );
+
+      const response = await fetch(import.meta.env.VITE_ENVELOP_URL, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
@@ -29,7 +67,8 @@ export function useEmailSubscription() {
         body: JSON.stringify({
           params: {
             email,
-            address
+            address,
+            signature
           },
           method: 'snapshot.subscribe'
         })
