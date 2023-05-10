7ae11a36bd2f6497ad58afbfac39030211157e78
j0hnfl0w <ij69@pm.me>
2023-05-10 14:33:40 +0300

    fix: separate list item, translations

--- src/plugins/safeSnap/components/Form/TokensModal.vue
+++ src/plugins/safeSnap/components/Form/TokensModal.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
-import networks from '@snapshot-labs/snapshot.js/src/networks.json';
-import { shorten, explorerUrl } from '@/helpers/utils';
 import { TokenAsset } from '@/helpers/interfaces';
+import TokensModalItem from './TokensModalItem.vue';
 
 const props = defineProps<{
   open: boolean;
@@ -23,31 +22,18 @@ const tokensFiltered = computed(() => {
   });
 });
 
-function addToken() {
-  // TODO actual tally link
-  window.open('https://tally.so/r/mKzXo7', '_blank');
-}
-
 function handleTokenClick(token) {
   console.log(':handleTokenClick', token);
   const isVerified = token.address === 'main' || token.verified !== undefined;
 
   if (!isVerified) {
     // TODO confirm through modal
-    const isApproved = confirm(
-      'Token not verified. Please enter the token address to continue.'
-    );
+    const isApproved = confirm('Token not verified. Confirm to continue.');
     if (!isApproved) return;
   }
 
   emit('tokenAddress', token.address);
   emit('close');
-}
-
-function getTokenExploreUrl(token) {
-  let network = '1';
-  if (token.verified !== undefined) network = String(token.verified.chainId);
-  return explorerUrl(network, token.address);
 }
 </script>
 
@@ -64,62 +50,19 @@ function getTokenExploreUrl(token) {
           modal
           focus-on-mount
           class="min-h-[60px] w-full flex-auto px-3 pb-3"
-        >
-          <template #after>
-            <BaseButton class="whitespace-nowrap" @click="addToken()"
-              >Add token</BaseButton
-            >
-          </template>
-        </BaseSearch>
+        />
       </div>
     </template>
 
     <template #default="{ maxHeight }">
       <div class="flex w-full flex-col gap-y-2 overflow-auto p-4">
-        <button
+        <TokensModalItem
           v-for="token in tokensFiltered"
           :key="token.address"
-          class="flex w-full cursor-pointer flex-row flex-wrap content-center items-center justify-between rounded-xl border border-skin-border p-3 hover:border-skin-link"
-          :class="{
-            '!border-skin-link': token.address === tokenAddress
-          }"
-          @click="handleTokenClick(token)"
-        >
-          <div class="mb-2 flex w-full flex-row justify-between">
-            <div class="flex flex-row gap-x-2">
-              <span class="text-skin-link">{{ token.symbol }}</span>
-              <span class="text-skin-text">{{
-                shorten(token.name, 'choice')
-              }}</span>
-            </div>
-            <div class="flex flex-row content-center items-center gap-x-1">
-              <template v-if="token.verified || token.address === 'main'">
-                <span>Verified</span>
-                <i-ho-check-badge class="mb-1 text-xs text-green" />
-              </template>
-              <template v-else>
-                <span>Unverified</span>
-                <i-ho-question-mark-circle class="text-xs" />
-              </template>
-            </div>
-          </div>
-
-          <div class="flex w-full flex-row items-center justify-between">
-            <div>
-              <span>{{ networks[token?.verified?.chainId || '1']?.name }}</span>
-            </div>
-            <a
-              v-if="token.address !== 'main'"
-              :href="getTokenExploreUrl(token)"
-              target="_blank"
-              rel="noopener noreferrer"
-              class="flex flex-row content-center items-center gap-x-1 text-skin-text hover:!text-skin-link"
-            >
-              <span class="">{{ shorten(token.address) }}</span>
-              <i-ho-arrow-top-right-on-square class="mb-1 text-xs" />
-            </a>
-          </div>
-        </button>
+          :token="token"
+          :is-selected="false"
+          @select="handleTokenClick"
+        />
 
         <div
           v-if="query.length && tokensFiltered.length === 0"
---
+++ src/plugins/safeSnap/components/Form/TokensModalItem.vue
@@ -0,0 +1,112 @@
+<script setup lang="ts">
+import networks from '@snapshot-labs/snapshot.js/src/networks.json';
+import { shorten, explorerUrl } from '@/helpers/utils';
+import { TokenAsset } from '@/helpers/interfaces';
+
+const props = defineProps<{
+  token: TokenAsset;
+  isSelected: boolean;
+}>();
+
+const emit = defineEmits(['select']);
+
+const exploreUrl = computed(() => {
+  let network = '1';
+  if (props.token.verified !== undefined)
+    network = String(props.token.verified.chainId);
+  return explorerUrl(network, props.token.address);
+});
+
+function handleImgError(e: Event) {
+  const img = e.target as HTMLImageElement;
+  img.src =
+    'https://safe-transaction-assets.gnosis-safe.io/chains/1/currency_logo.png';
+}
+</script>
+
+<template>
+  <button
+    class="flex w-full cursor-pointer flex-row flex-wrap content-center items-center justify-between rounded-xl border border-skin-border p-3 hover:border-skin-link"
+    :class="{
+      '!border-skin-link': isSelected
+    }"
+    @click="emit('select', token)"
+  >
+    <div class="mb-2 flex w-full flex-row justify-between">
+      <div class="flex flex-row gap-x-2">
+        <img
+          :src="token.logoUri"
+          alt="token-logo"
+          class="w-[24px] min-w-[24px]"
+          @error="handleImgError"
+        />
+        <span class="text-skin-link">{{ token.symbol }}</span>
+        <span class="text-skin-text">{{ shorten(token.name, 'choice') }}</span>
+      </div>
+      <div class="flex flex-row content-center items-center gap-x-1">
+        <template v-if="token.verified || token.address === 'main'">
+          <BasePopover placement="top-end">
+            <template #button>
+              <div
+                class="flex flex-row items-center gap-x-1 text-skin-text hover:!text-skin-link"
+              >
+                <span>{{ $t('verified') }}</span>
+                <i-ho-check-badge class="text-xs text-green" />
+              </div>
+            </template>
+            <template #content>
+              <div class="m-4">
+                <p>
+                  Information of this token has been verified by Snapshot.
+                  <a href="https://docs.snapshot.org/" target="_blank"
+                    >Click for more info.</a
+                  >
+                </p>
+              </div>
+            </template>
+          </BasePopover>
+        </template>
+
+        <template v-else>
+          <BasePopover placement="top-end">
+            <template #button>
+              <div
+                class="flex flex-row items-center gap-x-1 text-skin-text hover:!text-skin-link"
+              >
+                <span>{{ $t('unverified') }}</span>
+                <i-ho-question-mark-circle class="text-xs" />
+              </div>
+            </template>
+            <template #content>
+              <div class="m-4">
+                <p>
+                  Information of this token has NOT been verified by Snapshot.
+                  <a href="https://tally.so/r/mKzXo7" target="_blank"
+                    >Click to verify token.</a
+                  >
+                </p>
+              </div>
+            </template>
+          </BasePopover>
+        </template>
+      </div>
+    </div>
+
+    <div class="flex w-full flex-row items-center justify-between">
+      <div>
+        <span>{{ networks[token?.verified?.chainId || '1']?.name }}</span>
+      </div>
+      <a
+        v-if="token.address !== 'main'"
+        :href="exploreUrl"
+        target="_blank"
+        rel="noopener noreferrer"
+        class="flex flex-row content-center items-center gap-x-1 text-skin-text hover:!text-skin-link"
+        @click.stop
+      >
+        <span class="">{{ shorten(token.address) }}</span>
+        <i-ho-arrow-top-right-on-square class="mb-1 text-xs" />
+      </a>
+    </div>
+  </button>
+</template>
