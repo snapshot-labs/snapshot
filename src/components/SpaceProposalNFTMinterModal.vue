de8e049738fbcc871f2f72a665e143d7312ea24b
j0hnfl0w <ij69@pm.me>
2023-05-15 14:14:58 +0300

    nft mint wip

--- src/components/SpaceProposalNFTMinterModal.vue
+++ src/components/SpaceProposalNFTMinterModal.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts">
 import { ExtendedSpace, Proposal } from '@/helpers/interfaces';
+import getProvider from '@snapshot-labs/snapshot.js/src/utils/provider';
+import { sendTransaction } from '@snapshot-labs/snapshot.js/src/utils';
+import { getInstance } from '@snapshot-labs/lock/plugins/vue3';
+import { formatBytes32String } from '@ethersproject/strings';
 
 const props = defineProps<{
   space: ExtendedSpace;
@@ -9,18 +13,67 @@ const props = defineProps<{
 
 defineEmits(['close']);
 
+const auth = getInstance();
+const { notify } = useFlashNotification();
+const {
+  createPendingTransaction,
+  updatePendingTransaction,
+  removePendingTransaction
+} = useTxStatus();
+const { validEnsTlds } = useEns();
+const { t } = useI18n();
+const { web3 } = useWeb3();
+
 // watch(
 //   () => props.open,
 //   val => {
 //   }
 // );
 
+const abi = [
+  'function mint(uint256 proposalId, uint256 salt, uint8 v, bytes32 r, bytes32 s)'
+];
+
+const loading = ref(false);
 const mintCurrency = 'MATIC';
 const minMintPrice = ref('0.001');
 
 async function mint() {
   // sign message
   // fetch BE with spaceId, proposalId, salt?
+  // check, approve, WETH flow
+  // TODO move to mint coposable
+  const txPendingId = createPendingTransaction();
+  loading.value = true;
+  try {
+    const tx = await sendTransaction(
+      auth.web3,
+      '0xdDEd2972fB62907723463322b2C709CC9F5466C2',
+      abi,
+      'mint',
+      [
+        props.proposal.id,
+        0,
+        1,
+        formatBytes32String('2'),
+        formatBytes32String('3')
+      ]
+    );
+    console.log(':mint tx', tx);
+    notify(t('notify.transactionSent'));
+    updatePendingTransaction(txPendingId, { hash: tx.hash });
+    loading.value = false;
+    const receipt = await tx.wait();
+    console.log('Receipt', receipt);
+    // await sleep(3e3);
+    // notify(t('notify.delegationSuccess'));
+  } catch (e) {
+    notify(['red', t('notify.somethingWentWrong')]);
+    console.log(e);
+  } finally {
+    loading.value = false;
+    removePendingTransaction(txPendingId);
+  }
 }
 </script>
 
