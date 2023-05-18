d8640e730a614543edde7930c24b005458b9941c
j0hnfl0w <ij69@pm.me>
2023-05-18 17:45:26 +0300

    Added mock collectibles to profile

---
+++ src/components/ProfileCollectiblesItem.vue
@@ -0,0 +1,26 @@
+<script lang="ts" setup>
+import { shorten } from '@/helpers/utils';
+
+defineProps({
+  collectible: { type: Object, required: true }
+});
+</script>
+
+<template>
+  <div class="group flex w-[186px] cursor-pointer flex-col">
+    <div
+      class="flex h-[186px] w-[186px] flex-row items-center justify-center rounded-xl border border-skin-link bg-skin-border"
+    >
+      <BaseIcon
+        name="snapshot"
+        size="50"
+        class="text-primary group-hover:text-snapshot"
+      />
+    </div>
+    <div class="flex w-[186px] flex-row">
+      <span class="text-center text-skin-link">
+        {{ shorten(collectible.title, 32) }}
+      </span>
+    </div>
+  </div>
+</template>
--- src/views/ProfileCollectibles.vue
+++ src/views/ProfileCollectibles.vue
@@ -1,6 +1,11 @@
 <script setup lang="ts">
 // import { FOLLOWS_QUERY } from '@/helpers/queries';
 
+type Collectible = {
+  id: string;
+  title: string;
+};
+
 const props = defineProps<{
   userAddress: string;
   profile?: { about?: string };
@@ -9,10 +14,20 @@ const props = defineProps<{
 // const { apolloQuery } = useApolloQuery();
 
 const loadingOwnedCollectibles = ref(false);
-const collectibles = ref([]);
+const collectibles = ref([]) as Ref<Collectible[]>;
 
 async function loadCollectibles() {
-  collectibles.value = [];
+  collectibles.value = [
+    {
+      id: '0x73021d63f985aba8e42c67165f80c0027a591980a5cc372cda9637bdf2173f95',
+      title: '[Temperature Check] Deploy Uniswap v3 on Moonbeam (2023)'
+    },
+    {
+      id: '0x0f8a85d90759acf3a6fdc018e20b991f3562847122a8142c4e166c71d0e755e3',
+      title:
+        '[Temperature Check] Post-BSL cross-chain deployment process and creation of new uniswap.eth subdomain'
+    }
+  ];
 }
 
 onMounted(async () => {
@@ -24,8 +39,13 @@ onMounted(async () => {
 
 <template>
   <div>
-    <div class="space-y-4">
-      <span>Collectibles not found.</span>
+    <div class="flex w-full flex-row flex-wrap gap-3">
+      <ProfileCollectiblesItem
+        v-for="c in collectibles"
+        :key="c.id"
+        :collectible="c"
+      />
+      <span v-if="collectibles.length === 0">Collectibles not found.</span>
     </div>
   </div>
 </template>
