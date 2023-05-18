b80fe839a13790c7f24096c3c52b60f9eec8bf90
Dmytro Tolok <tolok.d@gmail.com>
2023-05-18 11:18:20 +0200

    feat(subscription management): load current user subscriptions on page loading

--- src/composables/useEmailSubscription.ts
+++ src/composables/useEmailSubscription.ts
@@ -3,6 +3,7 @@ import { getInstance } from '@snapshot-labs/lock/plugins/vue3';
 
 let auth;
 let isInitialized = false;
+let isSubscriptionsLoading = false;
 
 enum Status {
   waiting,
@@ -21,9 +22,46 @@ const status: Ref<Status> = ref(Status.waiting);
 const postSubscribeLevel: Ref<Level> = ref(Level.info);
 const postSubscribeMessage = ref('');
 const loading = ref(false);
+const isSubscribed = ref(false);
 
 const { t } = useI18n();
-const { web3 } = useWeb3();
+const { web3Account } = useWeb3();
+const apiSubscriptions = ref([]);
+
+async function loadEmailSubscriptions() {
+  const address = web3Account.value;
+
+  if (!address) return;
+  if (isSubscriptionsLoading) return;
+
+  isSubscriptionsLoading = true;
+
+  try {
+    const url = `${import.meta.env.VITE_ENVELOP_URL}/subscriber`;
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ address })
+    });
+
+    const result = await response.json();
+
+    if ('error' in result) {
+      isSubscribed.value = false;
+      throw new Error(result.error.message);
+    }
+
+    apiSubscriptions.value = result;
+    isSubscribed.value = true;
+  } catch (e) {
+    isSubscribed.value = false;
+    console.error(e);
+  } finally {
+    isSubscriptionsLoading = false;
+  }
+}
 
 async function subscribe(email: string, address: string) {
   loading.value = true;
@@ -31,7 +69,7 @@ async function subscribe(email: string, address: string) {
   try {
     const signature = await sign(
       auth.web3,
-      web3.value.account,
+      web3Account.value,
       {
         email
       },
@@ -84,16 +122,23 @@ function reset() {
 }
 
 export function useEmailSubscription() {
-  onBeforeMount(() => {
+  onBeforeMount(async () => {
     if (isInitialized) return;
 
     auth = getInstance();
 
-    isInitialized = true;
+    if (!web3Account.value) return;
+
+    try {
+      await loadEmailSubscriptions();
+    } finally {
+      isInitialized = true;
+    }
   });
 
   return {
     subscribe,
+    loadEmailSubscriptions,
     status,
     loading,
     reset,
