017009a0bf1de772a7bea70283eb6c3cedc97dc3
Dmytro Tolok <tolok.d@gmail.com>
2023-05-18 10:40:06 +0200

    refactor(useEmailSubscription): flattify structure

--- src/composables/useEmailSubscription.ts
+++ src/composables/useEmailSubscription.ts
@@ -1,86 +1,96 @@
 import sign, { SubscribeType } from '@/helpers/sign';
 import { getInstance } from '@snapshot-labs/lock/plugins/vue3';
 
-export function useEmailSubscription() {
-  enum Status {
-    waiting,
-    success,
-    error
-  }
+let auth;
+let isInitialized = false;
 
-  enum Level {
-    info = 'info',
-    warning = 'warning',
-    'warning-red' = 'warning-red',
-    success = 'success'
-  }
+enum Status {
+  waiting,
+  success,
+  error
+}
 
-  const status: Ref<Status> = ref(Status.waiting);
-  const postSubscribeLevel: Ref<Level> = ref(Level.info);
-  const postSubscribeMessage = ref('');
-  const loading = ref(false);
+enum Level {
+  info = 'info',
+  warning = 'warning',
+  'warning-red' = 'warning-red',
+  success = 'success'
+}
 
-  const { t } = useI18n();
-  const { web3 } = useWeb3();
-  const auth = getInstance();
+const status: Ref<Status> = ref(Status.waiting);
+const postSubscribeLevel: Ref<Level> = ref(Level.info);
+const postSubscribeMessage = ref('');
+const loading = ref(false);
 
-  async function subscribe(email: string, address: string) {
-    loading.value = true;
+const { t } = useI18n();
+const { web3 } = useWeb3();
 
-    try {
-      const signature = await sign(
-        auth.web3,
-        web3.value.account,
-        {
-          email
+async function subscribe(email: string, address: string) {
+  loading.value = true;
+
+  try {
+    const signature = await sign(
+      auth.web3,
+      web3.value.account,
+      {
+        email
+      },
+      SubscribeType
+    );
+
+    const response = await fetch(import.meta.env.VITE_ENVELOP_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        params: {
+          email,
+          address,
+          signature
         },
-        SubscribeType
-      );
+        method: 'snapshot.subscribe'
+      })
+    });
 
-      const response = await fetch(import.meta.env.VITE_ENVELOP_URL, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          params: {
-            email,
-            address,
-            signature
-          },
-          method: 'snapshot.subscribe'
-        })
-      });
+    const result = await response.json();
 
-      const result = await response.json();
-
-      if (result.result === 'OK') {
-        setPostSubscribeState(Status.success, Level.success, 'success');
-      } else {
-        setPostSubscribeState(Status.error, Level.warning, 'apiError');
-      }
-    } catch (e) {
-      setPostSubscribeState(Status.error, Level.warning, 'unknownError');
-    } finally {
-      loading.value = false;
+    if (result.result === 'OK') {
+      setPostSubscribeState(Status.success, Level.success, 'success');
+    } else {
+      setPostSubscribeState(Status.error, Level.warning, 'apiError');
     }
+  } catch (e) {
+    setPostSubscribeState(Status.error, Level.warning, 'unknownError');
+  } finally {
+    loading.value = false;
   }
+}
 
-  function setPostSubscribeState(
-    newStatus: Status,
-    level: Level,
-    messageKey?: string
-  ) {
-    status.value = newStatus;
-    postSubscribeLevel.value = level;
-    postSubscribeMessage.value = messageKey
-      ? t(`emailSubscription.postSubscribeMessage.${messageKey}`)
-      : '';
-  }
+function setPostSubscribeState(
+  newStatus: Status,
+  level: Level,
+  messageKey?: string
+) {
+  status.value = newStatus;
+  postSubscribeLevel.value = level;
+  postSubscribeMessage.value = messageKey
+    ? t(`emailSubscription.postSubscribeMessage.${messageKey}`)
+    : '';
+}
 
-  function reset() {
-    setPostSubscribeState(Status.waiting, Level.info);
-  }
+function reset() {
+  setPostSubscribeState(Status.waiting, Level.info);
+}
+
+export function useEmailSubscription() {
+  onBeforeMount(() => {
+    if (isInitialized) return;
+
+    auth = getInstance();
+
+    isInitialized = true;
+  });
 
   return {
     subscribe,
