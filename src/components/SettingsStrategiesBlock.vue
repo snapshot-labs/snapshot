f208ada0b5188d3cdad01ef62f100834ee3e5f53
Sam <samuv.eth@gmail.com>
2023-05-26 14:19:07 +0700

    Fixes

--- src/components/SettingsStrategiesBlock.vue
+++ src/components/SettingsStrategiesBlock.vue
@@ -54,75 +54,74 @@ function handleSubmitStrategy(strategy) {
 </script>
 
 <template>
-  <BaseBlock :title="title || $t('settings.strategies.label')">
-    <ContainerParallelInput class="mb-4 w-full">
-      <ComboboxNetwork
-        :network="form.network"
-        :hint="$t('settings.network.information')"
-        :disabled="isViewOnly"
-        @select="value => (form.network = value)"
-      />
-      <TuneInput
-        v-model="form.symbol"
-        :label="$t(`settings.symbol.label`)"
-        :hint="$t(`settings.symbol.information`)"
-        placeholder="e.g. BAL"
-        :error="validationErrors?.symbol"
-        :max-length="schemas.space.properties.symbol.maxLength"
-        :disabled="isViewOnly"
-      />
-    </ContainerParallelInput>
-
-    <div class="flex justify-between">
-      <div>
-        <div class="flex items-center gap-1">
-          <h4>{{ $t('settings.strategiesList') }}</h4>
-          <IconInformationTooltip
-            class="text-sm"
-            :information="$t('settings.strategies.information')"
-          />
-        </div>
-        <div class="-mt-[3px] text-sm">
-          ({{ $t('settings.votingPowerIsCumulative') }})
-        </div>
-      </div>
-      <div>
-        <BaseButton
-          class="flex w-full items-center gap-1"
+  <div>
+    <BaseBlock :title="title || $t('settings.strategies.label')">
+      <ContainerParallelInput class="mb-4 w-full">
+        <ComboboxNetwork
+          :network="form.network"
+          :hint="$t('settings.network.information')"
           :disabled="isViewOnly"
-          @click="handleAddStrategy"
-        >
-          <i-ho-plus class="text-sm" />
-          {{ $t('add') }}
-        </BaseButton>
+          @select="value => (form.network = value)"
+        />
+        <TuneInput
+          v-model="form.symbol"
+          :label="$t(`settings.symbol.label`)"
+          :hint="$t(`settings.symbol.information`)"
+          placeholder="e.g. BAL"
+          :error="validationErrors?.symbol"
+          :max-length="schemas.space.properties.symbol.maxLength"
+          :disabled="isViewOnly"
+        />
+      </ContainerParallelInput>
+
+      <div class="flex justify-between">
+        <div>
+          <div class="flex items-center gap-1">
+            <h4>{{ $t('settings.strategiesList') }}</h4>
+            <IconInformationTooltip
+              class="text-sm"
+              :information="$t('settings.strategies.information')"
+            />
+          </div>
+          <div class="-mt-[3px] text-sm">
+            ({{ $t('settings.votingPowerIsCumulative') }})
+          </div>
+        </div>
+        <div>
+          <BaseButton
+            class="flex w-full items-center gap-1"
+            :disabled="isViewOnly"
+            @click="handleAddStrategy"
+          >
+            <i-ho-plus class="text-sm" />
+            {{ $t('add') }}
+          </BaseButton>
+        </div>
       </div>
-    </div>
-    <div class="mt-3">
-      <StrategiesListItem
-        v-for="(strategy, i) in strategies"
-        :key="i"
-        :strategy="strategy"
-        class="rounded-md !border last:!mb-0"
-        :show-delete="!isViewOnly"
-        :show-edit="!isViewOnly"
-        @edit="handleEditStrategy(i)"
-        @delete="handleRemoveStrategy(i)"
+      <div class="mt-3">
+        <StrategiesListItem
+          v-for="(strategy, i) in strategies"
+          :key="i"
+          :strategy="strategy"
+          class="rounded-md !border last:!mb-0"
+          :show-delete="!isViewOnly"
+          :show-edit="!isViewOnly"
+          @edit="handleEditStrategy(i)"
+          @delete="handleRemoveStrategy(i)"
+        />
+      </div>
+
+      <StrategiesBlockWarning :error="validationErrors?.strategies" />
+    </BaseBlock>
+
+    <teleport to="#modal">
+      <ModalStrategy
+        :open="modalStrategyOpen"
+        :strategy="currentStrategy"
+        :default-network="form.network"
+        @close="modalStrategyOpen = false"
+        @add="handleSubmitStrategy"
       />
-    </div>
-
-    <StrategiesBlockWarning
-      v-if="context === 'settings'"
-      :error="validationErrors?.strategies"
-    />
-  </BaseBlock>
-
-  <teleport to="#modal">
-    <ModalStrategy
-      :open="modalStrategyOpen"
-      :strategy="currentStrategy"
-      :default-network="form.network"
-      @close="modalStrategyOpen = false"
-      @add="handleSubmitStrategy"
-    />
-  </teleport>
+    </teleport>
+  </div>
 </template>
