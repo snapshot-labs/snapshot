47ddcd9b2e20026a12de1df2aec870e686de759d
Sam <samuv.eth@gmail.com>
2023-06-09 06:10:26 +0700

    Update schema

--- src/components/SettingsDelegationBlock.vue
+++ src/components/SettingsDelegationBlock.vue
@@ -1,49 +1,24 @@
 <script setup lang="ts">
+import schemas from '@snapshot-labs/snapshot.js/src/schemas';
+
 const props = defineProps<{
   context: 'setup' | 'settings';
   isViewOnly?: boolean;
 }>();
 
 const { form } = useFormSpaceSettings(props.context);
-
-const def = computed(() => {
-  return {
-    type: 'object',
-    properties: {
-      standard: {
-        type: 'string',
-        title: 'Standard',
-        description: 'The standard used by your delegation contract',
-        anyOf: [{ const: 'compound-governor', title: 'Compound governor' }]
-      },
-      contract: {
-        type: 'string',
-        format: 'address',
-        title: 'Contract address',
-        description: 'The address of your delegation contract',
-        examples: ['0x3901D0fDe202aF1427216b79f5243f8A022d68cf']
-      },
-      subgraphUrl: {
-        type: 'string',
-        format: 'uri',
-        title: 'Subgraph URL',
-        description: 'The URL of your delegation subgraph',
-        examples: [
-          'https://api.thegraph.com/subgraphs/name/arr00/uniswap-governance-v2'
-        ]
-      }
-    },
-    additionalProperties: false
-  };
-});
 </script>
 
 <template>
   <BaseBlock title="Delegates portal">
-    <BaseMessageBlock class="mb-3">
+    <BaseMessageBlock level="info" class="mb-3">
       Please ensure your token adheres to a compatible delegation standard to
       enable delegate discovery and activity within your Snapshot space.
     </BaseMessageBlock>
-    <TuneForm v-model="form.delegation" :definition="def" :error="{}" />
+    <TuneForm
+      v-model="form.delegation"
+      :definition="schemas.space.properties.delegationPortal"
+      :error="{}"
+    />
   </BaseBlock>
 </template>
