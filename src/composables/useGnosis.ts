fc5519c25a55dc691184fc83c1288e50ae3bbc91
Dmytro Tolok <tolok.d@gmail.com>
2023-06-30 14:56:30 +0200

    fix(gnosis safe): add extra check for gnosis contracts

--- src/composables/useGnosis.ts
+++ src/composables/useGnosis.ts
@@ -1,5 +1,7 @@
 import { getInstance } from '@snapshot-labs/lock/plugins/vue3';
 import { ExtendedSpace } from '@/helpers/interfaces';
+import utils from '@snapshot-labs/snapshot.js/src/utils';
+import { computedAsync } from '@vueuse/core';
 
 const defaultNetwork = import.meta.env.VITE_DEFAULT_NETWORK;
 
@@ -9,17 +11,25 @@ export function useGnosis(space?: ExtendedSpace) {
   const auth = getInstance();
   const connectorName = computed(() => auth.provider.value?.connectorName);
 
+  const networkKey = computed(() => web3.value.network.key);
+
+  const spaceNetworkKey = computed(() => space?.network);
+
+  const isContract = computedAsync(async () => {
+    if (!web3.value.account) return false;
+    const provider = utils.getProvider(networkKey.value);
+    const code = await provider.getCode(web3.value.account);
+    return code !== '0x';
+  }, false);
+
   const isGnosisSafe = computed(
     () =>
       web3.value?.walletConnectType === 'Gnosis Safe Multisig' ||
       web3.value?.walletConnectType === 'WalletConnect Safe App' ||
       web3.value?.walletConnectType === 'Den' ||
-      connectorName.value === 'gnosis'
+      connectorName.value === 'gnosis' ||
+      isContract
   );
-
-  const networkKey = computed(() => web3.value.network.key);
-
-  const spaceNetworkKey = computed(() => space?.network);
 
   const isGnosisAndNotDefaultNetwork = computed(() => {
     return isGnosisSafe.value && networkKey.value !== defaultNetwork;
