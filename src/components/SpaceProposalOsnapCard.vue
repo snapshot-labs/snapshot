0483e4c70aa00c97c2159f3249f17a9623200a96
ryanwolhuter <dev@ryanwolhuter.com>
2023-07-31 12:00:44 +0200

    add osnap card component
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

---
+++ src/components/SpaceProposalOsnapCard.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div
+    class="border-y border-skin-border bg-[#FCFCFC] text-base md:rounded-xl md:border"
+  >
+    <div
+      class="min-h-[150px] rounded-t-none border-b border-skin-border bg-[length:65%] bg-right-top bg-no-repeat px-4 pb-[12px] pt-3 md:rounded-t-lg"
+      style="background-image: url('/osnap-card-background.svg')"
+    >
+      <img src="/osnap-logo.svg" alt="osnap-logo" class="mb-3" />
+      <p class="max-w-[400px]">
+        Proposal powered by oSnap to deliver instant, secure and trustless
+        execution.
+      </p>
+    </div>
+    <div class="h-[57px] rounded-t-none px-4 pb-[12px] pt-3 md:rounded-t-lg">
+      <BaseLink link="https://uma.xyz/osnap">Read more about oSnap</BaseLink>
+    </div>
+  </div>
+</template>
