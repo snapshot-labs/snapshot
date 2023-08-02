20b270380f20c1f1f2105d1726ce9897917bf4ec
ryanwolhuter <dev@ryanwolhuter.com>
2023-08-02 11:56:32 +0200

    add dark theme
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- src/components/SpaceProposalOsnapCard.vue
+++ src/components/SpaceProposalOsnapCard.vue
@@ -1,20 +1,26 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const { userTheme } = useSkin();
+const cardBackgroundUrl = computed(() => {
+  return userTheme.value === 'dark'
+    ? 'url("/osnap-card-background-dark.svg")'
+    : 'url("/osnap-card-background-light.svg")';
+});
+</script>
 
 <template>
-  <div
-    class="border-y border-skin-border bg-[#FCFCFC] text-base md:rounded-xl md:border"
-  >
+  <div class="border-y border-skin-border text-base md:rounded-xl md:border">
     <div
       class="min-h-[150px] rounded-t-none border-b border-skin-border bg-[length:65%] bg-right-top bg-no-repeat px-4 pb-[12px] pt-3 md:rounded-t-lg"
-      style="background-image: url('/osnap-card-background.svg')"
+      :style="{ backgroundImage: cardBackgroundUrl }"
     >
       <img src="/osnap-logo.svg" alt="osnap-logo" class="mb-3" />
       <p class="max-w-[400px]">
-        Proposal powered by oSnap to deliver instant, secure and trustless
-        execution.
+        Securely execute approved transactions on chain.
       </p>
     </div>
-    <div class="h-[57px] rounded-t-none px-4 pb-[12px] pt-3 md:rounded-t-lg">
+    <div
+      class="h-[57px] rounded-t-none px-4 pb-[12px] pt-3 md:rounded-t-lg [&_a]:text-skin-heading [&_path]:stroke-skin-heading"
+    >
       <BaseLink link="https://uma.xyz/osnap">Read more about oSnap</BaseLink>
     </div>
   </div>
