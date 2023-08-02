a711c6139a9e9c7f87ef42adf23f84371e0592d4
Sam <samuv.eth@gmail.com>
2023-08-03 06:43:00 +0700

    Fix skeleton

--- src/components/SpaceSidebarSkeleton.vue
+++ src/components/SpaceSidebarSkeleton.vue
@@ -1,23 +1,21 @@
 <template>
-  <div class="mb-4 lg:fixed lg:mb-0 lg:w-[240px]">
-    <BaseBlock :slim="true" class="overflow-hidden">
-      <div class="flex px-4 pt-3 text-center lg:block lg:h-[253px]">
-        <div class="mb-2 flex lg:mb-3 lg:block">
-          <div class="lazy-loading mx-auto h-[80px] w-[80px] rounded-full" />
+  <div class="mb-4">
+    <BaseBlock :slim="true" class="overflow-hidden !border-t-0 md:!border-t">
+      <div class="mx-3 mb-4 items-end text-center md:mt-3 md:flex lg:block">
+        <div>
+          <div class="lazy-loading h-[48px] w-[48px] rounded-full" />
 
-          <div
-            class="ml-3 flex flex-col items-start justify-center lg:ml-0 lg:mt-3 lg:items-center"
-          >
+          <div class="mt-2 flex flex-col items-start justify-center">
             <div
               class="lazy-loading mb-2 h-[28px] w-[130px] rounded-md bg-skin-text"
             />
             <div
-              class="lazy-loading h-[26px] w-[100px] rounded-md bg-skin-text"
+              class="lazy-loading h-[20px] w-[100px] rounded-md bg-skin-text"
             />
           </div>
         </div>
-        <div class="ml-3 flex items-center justify-center gap-x-2 lg:ml-0">
-          <BaseButton class="w-[120px] cursor-wait">
+        <div class="mt-[12px] flex w-full justify-end">
+          <BaseButton class="w-full cursor-wait md:max-w-[180px] lg:max-w-none">
             {{ $t('join') }}
           </BaseButton>
         </div>
