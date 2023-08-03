a59003f672c06825d9b9f0326e1876b5a1010821
Sam <samuv.eth@gmail.com>
2023-08-03 09:46:59 +0700

    Fix navigation hover

--- src/components/BaseSidebarNavigationItem.vue
+++ src/components/BaseSidebarNavigationItem.vue
@@ -11,30 +11,44 @@ withDefaults(defineProps<Props>(), {
 <template>
   <div
     :class="[
-      'group relative block cursor-pointer whitespace-nowrap px-[20px] py-2 text-skin-text hover:bg-skin-bg lg:px-4',
+      'group relative block cursor-pointer whitespace-nowrap px-[20px] py-2 text-skin-text  hover:bg-skin-bg lg:px-4',
       { ' !text-skin-heading': isActive }
     ]"
   >
     <slot />
     <div class="absolute left-0 top-0 flex h-full w-full justify-center">
       <div
-        v-if="isActive"
-        class="lg:nav-left-border max-lg:nav-bottom-border"
+        class="lg:nav-left-border max-lg:nav-bottom-border group-hover:nav-left-border-hovered"
+        :class="[
+          {
+            selected: isActive
+          }
+        ]"
       />
     </div>
   </div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
 @tailwind components;
 
 @layer components {
   .nav-left-border {
-    @apply absolute left-0 top-[4px] h-[32px] w-[4px] rounded-br rounded-tr bg-skin-text;
+    &.selected {
+      @apply absolute left-0 top-[4px] h-[32px] w-[4px] rounded-br rounded-tr bg-skin-text;
+    }
+  }
+
+  .nav-left-border-hovered {
+    &:not(.selected) {
+      @apply absolute left-0 top-[16px] h-[8px] w-[4px] rounded-br rounded-tr bg-skin-text;
+    }
   }
 
   .nav-bottom-border {
-    @apply absolute bottom-[0px] h-[4px] w-4/6 rounded-tl rounded-tr bg-skin-text;
+    &.selected {
+      @apply absolute bottom-[0px] h-[4px] w-4/6 rounded-tl rounded-tr bg-skin-text;
+    }
   }
 }
 </style>
