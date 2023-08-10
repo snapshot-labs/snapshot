6c35d220406145c57dd6dc76fab874d2a45f5bf6
Sam <samuv.eth@gmail.com>
2023-08-10 21:45:33 +0700

    Cleanup divs

--- src/components/BaseNoResults.vue
+++ src/components/BaseNoResults.vue
@@ -1,12 +1,8 @@
 <template>
-  <div class="py-[20px] text-center md:py-5">
-    <div class="text-skin-link">
-      <div>
-        <i-ho-emoji-sad class="mx-auto" />
-        <div class="mt-2 text-base">
-          {{ $t('noResultsFound') }}
-        </div>
-      </div>
+  <div class="py-[20px] text-center text-skin-link md:py-5">
+    <i-ho-emoji-sad class="mx-auto" />
+    <div class="mt-2 text-base">
+      {{ $t('noResultsFound') }}
     </div>
   </div>
 </template>
