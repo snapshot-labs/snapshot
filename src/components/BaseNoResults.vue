4af136ffdca0ec8429831b8cbdfd6b46a7ee86fb
Sam <samuv.eth@gmail.com>
2023-08-10 17:09:37 +0700

    Fix no results message

--- src/components/BaseNoResults.vue
+++ src/components/BaseNoResults.vue
@@ -1,14 +1,12 @@
-<script setup lang="ts">
-const { t } = useI18n();
-defineProps({ useBlock: Boolean });
-const text = computed(() => t('noResultsFound'));
-</script>
-
 <template>
-  <div class="mb-3 text-center">
-    <BaseBlock v-if="useBlock" class="pt-1">
-      {{ text }}
-    </BaseBlock>
-    <div v-else>{{ text }}</div>
+  <div class="py-[20px] text-center md:py-5">
+    <div class="text-skin-link">
+      <div>
+        <i-ho-emoji-sad class="mx-auto" />
+        <div class="mt-2 text-base">
+          {{ $t('noResultsFound') }}
+        </div>
+      </div>
+    </div>
   </div>
 </template>
