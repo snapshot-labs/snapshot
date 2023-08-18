60d3f59dc63fbfe44eb7d15e521e7c9bf081e424
Sam <samuv.eth@gmail.com>
2023-08-18 08:56:15 +0700

    Fix onboarding

--- src/components/SpaceDelegatesAccount.vue
+++ src/components/SpaceDelegatesAccount.vue
@@ -1,25 +1,24 @@
 <script setup lang="ts">
-import { useTippy } from 'vue-tippy';
 import { useStorage } from '@vueuse/core';
 
 const { web3Account } = useWeb3();
 
-const loggedAvatar = ref();
 const showOnboarding = useStorage('snapshot.showOnboardingDelegates', true);
-
-const loggedAvatarTooltip = useTippy(loggedAvatar, {
-  content: 'Delegation profile',
-  placement: 'top-start',
-  trigger: 'manual',
-  showOnCreate: showOnboarding.value,
-  onHide: () => {
-    showOnboarding.value = false;
-  }
-});
 </script>
 
 <template>
-  <div ref="loggedAvatar" @mouseenter="loggedAvatarTooltip.hide()">
+  <div ref="loggedAvatar" class="relative" @click="showOnboarding = false">
+    <div
+      v-if="showOnboarding"
+      class="pointer-events-none absolute bottom-[52px] left-[22px] flex"
+    >
+      <i-s-line-arrow class="text-skin-text opacity-40" />
+      <div
+        class="absolute bottom-[30px] left-[42px] w-[102px] text-xs leading-4 opacity-60"
+      >
+        view and edit your delegator profile
+      </div>
+    </div>
     <BaseButtonRound class="!h-[46px] !w-[46px]">
       <AvatarUser :address="web3Account" size="20" class="cursor-pointer" />
     </BaseButtonRound>
