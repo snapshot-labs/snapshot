694b704763782c1468dcfee1e953d930a08321a0
Sam <samuv.eth@gmail.com>
2023-08-24 19:01:09 +0700

    Fix space heading lg

--- src/components/SpaceSidebarHeader.vue
+++ src/components/SpaceSidebarHeader.vue
@@ -35,7 +35,7 @@ watchEffect(() => {
     <div>
       <AvatarSpace :space="space" symbol-index="space" size="48" class="mr-3" />
       <div class="mt-2 truncate text-left">
-        <h3 class="my-0 flex items-center text-2xl leading-[44px]">
+        <h3 class="my-0 flex items-center text-2xl leading-[44px] lg:text-lg">
           <div
             v-tippy="{
               content: space.name.length > 16 ? space.name : null
@@ -46,7 +46,7 @@ watchEffect(() => {
           </div>
           <IconVerifiedSpace v-if="space.verified" />
         </h3>
-        <div class="text-md text-skin-text">
+        <div class="text-md text-skin-text lg:text-base">
           {{
             $tc('members', space.followersCount, {
               count: formatCompactNumber(space.followersCount)
