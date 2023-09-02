4fe26aace71384fa473215ddb9882e10db50432a
less <fabien@bonustrack.co>
2023-09-02 15:51:54 +0700

    hotfix: cyberconnect proposal

--- src/components/SpaceProposalPage.vue
+++ src/components/SpaceProposalPage.vue
@@ -149,6 +149,13 @@ onMounted(() => setMessageVisibility(props.proposal.flagged));
             :is-admin="isAdmin"
             :is-moderator="isModerator"
           />
+          <div
+            v-if="proposal?.id === '0xb356f9a8bd8aa3210b5cfb7c8c34c950aada63c1d9dc72916730e214e7d380b8'"
+            class="border !border-skin-link bg-skin-block-bg p-4 rounded-lg mb-4"
+          >
+            <i-ho-exclamation-circle class="inline-block"/>
+            The proposal is rejected due to an obvious mistake "Utilizing Cyber Community Treasury’s unlocked CYBER to provide liquidity for bridging. The foundation will try to keep 25k CYBER-ETH, 25k CYBER-BSC, 25k CYBER-OP in the bridge. A total of 7,000,000 CYBER-BSC and 3,888,000 CYBER-ETH can be used to maintain liquidity on the bridging service." Only 1,088,000 CYBER were unlocked to Community Treasury so far, not the 10.888M stated here.
+          </div>
           <SpaceProposalContent :space="space" :proposal="proposal" />
         </div>
         <div class="space-y-4">
