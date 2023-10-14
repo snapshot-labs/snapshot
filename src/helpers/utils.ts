ddf6e3fecc702c82f7f4becd03b54aecfdb10243
Sam <samuv.eth@gmail.com>
2023-10-14 11:39:58 +0700

    Remove old code

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -121,14 +121,6 @@ export async function clearStampCache(id: string, type = 'space') {
     return await fetch(`https://cdn.stamp.fyi/clear/avatar/eth:${id}`);
 }
 
-export function urlify(text: string, target = '_blank') {
-  const urlRegex = /(https?:\/\/[^\s]+)/g;
-  return text.replace(
-    urlRegex,
-    `<a href="$1" target="${target}" rel="noopener">$1</a>`
-  );
-}
-
 export async function resolveEns(handle: string) {
   try {
     const broviderUrl = import.meta.env.VITE_BROVIDER_URL;
