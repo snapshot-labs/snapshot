656e2012c4f6d273adb71cf63480a5d4f0581ad5
Sam <samuv.eth@gmail.com>
2023-10-14 11:39:06 +0700

    Add link preview to TextAutolinker for all links

--- src/components/TextAutolinker.vue
+++ src/components/TextAutolinker.vue
@@ -8,15 +8,47 @@ interface Props {
 const props = withDefaults(defineProps<Props>(), {
   truncate: 0
 });
+
+const textAutolinker = ref();
+const showModal = ref(false);
+const clickedUrl = ref('');
+
 const textWithLinks = computed(() =>
   Autolinker.link(props.text, {
     truncate: props.truncate,
     sanitizeHtml: true
   })
 );
+
+function handleLinkClick(e, url) {
+  e.preventDefault();
+  clickedUrl.value = url;
+  if (url.includes('snapshot.org/#/')) return handleConfirm();
+  showModal.value = true;
+}
+
+function handleConfirm() {
+  window.open(clickedUrl.value, '_blank', 'noopener,noreferrer');
+}
+
+onMounted(() => {
+  textAutolinker.value.querySelectorAll('a[href]').forEach(function (link) {
+    link.addEventListener('click', function (e) {
+      handleLinkClick(e, link.getAttribute('href'));
+    });
+  });
+});
 </script>
 
 <template>
   <!-- eslint-disable-next-line vue/no-v-html -->
-  <span v-html="textWithLinks" />
+  <span ref="textAutolinker" v-html="textWithLinks" />
+  <Teleport to="#modal">
+    <ModalLinkPreview
+      :open="showModal"
+      :clicked-url="clickedUrl"
+      @close="showModal = false"
+      @confirm="handleConfirm"
+    />
+  </Teleport>
 </template>
