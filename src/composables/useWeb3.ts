62e474774a81512d00dbc3f96e70de3e92dc5e8c
Sam <samuv.eth@gmail.com>
2023-11-05 13:54:51 +0700

    Fixes

--- src/composables/useWeb3.ts
+++ src/composables/useWeb3.ts
@@ -1,7 +1,7 @@
 import { useAccount, useConnect, useDisconnect, useNetwork } from 'use-wagmi';
-import { InjectedConnector } from 'use-wagmi/connectors/injected';
 import { Web3Provider } from '@ethersproject/providers';
 import { mainnet, goerli } from 'viem/chains';
+import { Config } from 'use-wagmi';
 
 const defaultChainId: any = import.meta.env.VITE_DEFAULT_NETWORK;
 
@@ -9,22 +9,30 @@ const web3ProviderRef = ref<any>(null);
 const providerRef = ref<any>(null);
 
 export function useWeb3() {
+  const { notify } = useFlashNotification();
+
   const { connect } = useConnect({
-    connector: new InjectedConnector(),
-    onSuccess(connector) {
-      initProviders(connector.connector);
+    onSuccess({ connector }) {
+      initProviders(connector);
+    },
+    onError(e) {
+      notify(['red', e.message]);
     }
   });
   const { disconnect } = useDisconnect();
-  const { address, isConnected, isConnecting } = useAccount();
+  const { address, isConnected, isConnecting } = useAccount({
+    // onConnect({ connector }) {
+    //   connector?.getProvider();
+    // }
+  });
   const { chain } = useNetwork();
 
   const defaultChain = computed(() =>
     defaultChainId === '1' ? mainnet : goerli
   );
 
-  async function initProviders(connector) {
-    if (isConnected.value && connector?.getProvider) {
+  async function initProviders(connector: Config['connector']) {
+    if (connector?.getProvider) {
       providerRef.value = await connector.getProvider();
       web3ProviderRef.value = new Web3Provider(await connector.getProvider());
     } else {
