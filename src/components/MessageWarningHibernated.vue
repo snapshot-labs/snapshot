4f0b6141f7ba05c0312f7928738f9946f00a42ad
Wan <495709+wa0x6e@users.noreply.github.com>
2023-11-16 15:17:50 +0400

    Update src/components/MessageWarningHibernated.vue
    Co-authored-by: Sam <51686767+samuveth@users.noreply.github.com>

--- src/components/MessageWarningHibernated.vue
+++ src/components/MessageWarningHibernated.vue
@@ -10,7 +10,7 @@ const { loadSpaceController, isSpaceController } = useSpaceController();
 
 const isAuthorized = computed(() => {
   const admins = (props.space.admins || []).map(admin => admin.toLowerCase());
-  return admins.includes(web3Account.value?.toLowerCase()) || isSpaceController;
+  return admins.includes(web3Account.value?.toLowerCase()) || isSpaceController.value;
 });
 
 onMounted(async () => {
