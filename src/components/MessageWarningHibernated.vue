fff1e54fb9593c271edf0723786985fb51c4338d
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2023-11-16 17:36:51 +0800

    fix: redirect reactivate space CTA to settings page

--- src/components/MessageWarningHibernated.vue
+++ src/components/MessageWarningHibernated.vue
@@ -6,21 +6,7 @@ const props = defineProps<{
 }>();
 
 const { web3Account } = useWeb3();
-const { send, isSending } = useClient();
-const { reloadSpace } = useExtendedSpaces();
 const { loadSpaceController, isSpaceController } = useSpaceController();
-const { notify } = useFlashNotification();
-const { t } = useI18n();
-const { isValid, prunedForm, populateForm } = useFormSpaceSettings('settings');
-
-async function handleReactivateSpace() {
-  const result = await send(props.space, 'settings', prunedForm.value);
-
-  if (result.id) {
-    await reloadSpace(props.space.id);
-    notify(['green', t('notify.spaceReactivated')]);
-  }
-}
 
 const isAuthorized = computed(() => {
   const admins = (props.space.admins || []).map(admin => admin.toLowerCase());
@@ -29,30 +15,22 @@ const isAuthorized = computed(() => {
 
 onMounted(async () => {
   await loadSpaceController();
-
-  if (isAuthorized.value) {
-    populateForm(props.space);
-  }
 });
 </script>
 
 <template>
   <BaseMessageBlock v-if="space.hibernated" level="warning" is-responsive>
     {{ $t('create.errorSpaceHibernated') }}
-    <BaseLink
-      link="https://docs.snapshot.org/user-guides/spaces/space-hibernation"
-    >
+    <BaseLink link="https://docs.snapshot.org/user-guides/spaces/space-hibernation">
       {{ $t('learnMore') }}
     </BaseLink>
 
     <p v-if="isAuthorized" class="mt-3">
-      <BaseButton
-        :disabled="!isValid"
-        :loading="isSending"
-        @click="handleReactivateSpace"
-      >
-        {{ $t('reactivateSpace') }}
-      </BaseButton>
+      <router-link :to="{ name: 'spaceSettings' }">
+        <BaseButton>
+          {{ $t('reactivateSpace') }}
+        </BaseButton>
+      </router-link>
     </p>
   </BaseMessageBlock>
 </template>
