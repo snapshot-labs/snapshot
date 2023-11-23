8cbb6e28b87eec9aee840ee25ad996ad2575c5e9
Gerhard Steenkamp <51655063+gsteenkamp89@users.noreply.github.com>
2023-11-23 14:58:58 +0200

    feat: uma 1936 add dark mode osnap buttons (#106)

--- src/components/SettingsTreasuryActivateOsnapButton.vue
+++ src/components/SettingsTreasuryActivateOsnapButton.vue
@@ -1,24 +1,58 @@
 <script setup lang="ts">
+const { theme } = useSkin();
+
 defineProps<{
   isOsnapEnabled: boolean;
 }>();
+
+// handling some theming here locally so as not to interfere with the global style.scss file
+const activeStyles = computed(() => {
+  return theme.value === 'light'
+    ? {
+        div: 'bg-[hsla(122,100%,45%,0.13)] text-[hsla(122,100%,21%,1)]',
+        span: 'bg-[hsla(122,100%,45%,1)]'
+      }
+    : {
+        div: 'bg-[hsla(122,100%,45%,0.13)] text-[hsla(122,100%,45%,1)]',
+        span: 'bg-[hsla(122,100%,45%,1)]'
+      };
+});
+
+const inactiveStyles = computed(() => {
+  return theme.value === 'light'
+    ? {
+        div: 'bg-[hsla(0,0%,0%,1)] text-skin-bg',
+        span: 'bg-skin-bg opacity-30'
+      }
+    : {
+        div: 'bg-[hsla(0,0%,100%,1)] text-skin-bg',
+        span: 'bg-skin-bg opacity-30'
+      };
+});
 </script>
 
 <template>
   <button
     v-if="isOsnapEnabled"
-    class="flex items-center gap-2 rounded-full bg-[hsla(122,100%,45%,0.13)] px-3 py-2 text-[hsl(122,100%,21%)]"
+    :class="[
+      'flex items-center gap-2 rounded-full px-3 py-2',
+      activeStyles.div
+    ]"
   >
     <span
-      class="block h-[6px] w-[6px] rounded-full bg-[hsl(122,100%,45%)]"
-    ></span
-    >oSnap activated
+      :class="['block h-[6px] w-[6px] rounded-full', activeStyles.span]"
+    />oSnap activated
   </button>
   <button
     v-else
-    class="flex items-center gap-2 rounded-full bg-black px-3 py-2 text-white"
+    :class="[
+      'flex items-center gap-2 rounded-full px-3 py-2',
+      inactiveStyles.div
+    ]"
   >
-    <span class="block h-[6px] w-[6px] rounded-full bg-white/30"></span>
+    <span
+      :class="['block h-[6px] w-[6px] rounded-full', inactiveStyles.span]"
+    />
     Activate oSnap
   </button>
 </template>
