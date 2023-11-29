6ce82f872d8c003b52591fe7280b0df18f6ab622
ChaituVR <yourchaitu@gmail.com>
2023-11-29 16:21:37 +0530

    fix

--- src/views/SpaceProposals.vue
+++ src/views/SpaceProposals.vue
@@ -65,7 +65,6 @@ const showOnlyCore = computed(() => (route.query.onlyCore as string) || '0');
 const showFlagged = computed(() => (route.query.showFlagged as string) || '0');
 
 async function getProposals(skip = 0) {
-  if (!spaceMembers.value.length) return [];
   return apolloQuery(
     {
       query: PROPOSALS_QUERY,
