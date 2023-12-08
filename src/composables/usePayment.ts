c2274fbca5565e8dc66c489f1be97663318fea84
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2023-12-08 17:06:18 +0700

    feat: implement ETH transfer

---
+++ src/composables/usePayment.ts
@@ -0,0 +1,123 @@
+import getProvider from '@snapshot-labs/snapshot.js/src/utils/provider';
+import { getInstance } from '@snapshot-labs/lock/plugins/vue3';
+import { Contract } from '@ethersproject/contracts';
+import { parseUnits } from '@ethersproject/units';
+import { BigNumber } from '@ethersproject/bignumber';
+
+const BASE_PRICE = 100;
+const BASE_CURRENCY = {
+  name: 'USD',
+  symbol: '$'
+};
+
+const PLANS = [
+  { label: '1 year', factor: 12, discount: 20 },
+  { label: '6 months', factor: 6 },
+  { label: '3 months', factor: 3 }
+];
+const CURRENCIES = {
+  ETH: {
+    name: 'Ethereum',
+    code: 'ETH',
+    decimal: 'ether'
+  },
+  USDC: {
+    name: 'USD Coin',
+    code: 'USDC',
+    decimal: 18,
+    address: {
+      1: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
+      5: '0x07865c6e87b9f70255377e024ace6630c1eaa37f'
+    }
+  },
+  DAI: {
+    name: 'DAI',
+    code: 'DAI',
+    decimal: 18,
+    address: {
+      1: '0x6b175474e89094c44da98b954eedeac495271d0f',
+      5: '0x73967c6a0904aa032c103b4104747e88c566b1a2'
+    }
+  }
+};
+const DEFAULT_CURRENCY = 'ETH';
+const DEFAULT_PLAN = PLANS[0];
+const TRANSFER_ABI = [
+  {
+    name: 'transfer',
+    type: 'function',
+    inputs: [
+      {
+        name: '_to',
+        type: 'address'
+      },
+      {
+        type: 'uint256',
+        name: '_tokens'
+      }
+    ],
+    constant: false,
+    outputs: [],
+    payable: false
+  }
+];
+const SNAPSHOT_WALLET = '0x91FD2c8d24767db4Ece7069AA27832ffaf8590f3';
+const fxRates = reactive({
+  ETH: 0.000424,
+  USDC: 1,
+  DAI: 1
+});
+
+export function usePayment(network: number) {
+  const auth = getInstance();
+  const provider = getProvider(network);
+  const loading = ref(false);
+  const { notify } = useFlashNotification();
+
+  async function transfer(amount: number, currencyCode: string) {
+    loading.value = true;
+    const currency = CURRENCIES[currencyCode];
+    const parsedAmount = parseUnits(amount.toString(), currency.decimal);
+
+    try {
+      if (currencyCode === 'ETH') {
+        await transferEth(parsedAmount);
+      } else {
+        await transferErc20(parsedAmount, currency.address[network]);
+      }
+    } catch (e: any) {
+      if (e.code === 'INSUFFICIENT_FUNDS') {
+        return notify(['red', 'Insufficient funds']);
+      } else {
+        console.error('Transfer error', e);
+      }
+    } finally {
+      loading.value = false;
+    }
+  }
+
+  async function transferEth(amount: BigNumber) {
+    const signer = auth.web3.getSigner();
+    return signer.sendTransaction({
+      to: SNAPSHOT_WALLET,
+      value: amount
+    });
+  }
+
+  async function transferErc20(amount: BigNumber, address: string) {
+    const contract = new Contract(address, TRANSFER_ABI, provider);
+    return contract.transfer(SNAPSHOT_WALLET, amount);
+  }
+
+  return {
+    BASE_PRICE,
+    BASE_CURRENCY,
+    DEFAULT_CURRENCY,
+    DEFAULT_PLAN,
+    CURRENCIES,
+    PLANS,
+    transfer,
+    fxRates,
+    loading
+  };
+}
--- src/views/PaymentNetworkFees.vue
+++ src/views/PaymentNetworkFees.vue
@@ -1,7 +1,124 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const {
+  DEFAULT_CURRENCY,
+  DEFAULT_PLAN,
+  CURRENCIES,
+  PLANS,
+  BASE_PRICE,
+  BASE_CURRENCY,
+  fxRates,
+  transfer,
+  loading
+} = usePayment(import.meta.env.VITE_DEFAULT_NETWORK);
+const { web3Account } = useWeb3();
+
+const data = reactive({
+  tos: true,
+  currency: DEFAULT_CURRENCY,
+  plan: DEFAULT_PLAN
+});
+
+const amount = computed(() => {
+  return computePrice(fxRates[data.currency], data.plan.factor);
+});
+
+function setData(key: string, value: any) {
+  data[key] = value;
+}
+
+function pay() {
+  transfer(amount.value, data.currency);
+}
+
+function computePrice(currencyFactor: number, planFactor: number) {
+  return currencyFactor * BASE_PRICE * planFactor;
+}
+</script>
 
 <template>
   <TheLayout>
-    <h1>Network fees payment</h1>
+    <BaseMessageBlock v-if="!web3Account" level="warning">
+      Connect your wallet
+    </BaseMessageBlock>
+
+    <h1>Network fees</h1>
+    Pay to support your network on Snapshot
+
+    <form class="flex flex-col mt-4 gap-3" @submit="pay">
+      <fieldset>
+        <legend><h2>Select your plan</h2></legend>
+        <ol class="grid grid-flow-col gap-3 justify-stretch">
+          <li
+            v-for="plan in PLANS"
+            :key="plan"
+            class="cursor-pointer border-y border-skin-border bg-skin-block-bg text-base md:rounded-xl md:border p-3"
+            :class="{
+              '!border-skin-primary': plan.label === data.plan.label
+            }"
+            @click="setData('plan', plan)"
+          >
+            <b>{{ plan.label }}</b>
+
+            <h4>
+              {{ BASE_CURRENCY.name }} {{ BASE_CURRENCY.symbol
+              }}{{ (BASE_PRICE * plan.factor).toLocaleString() }}
+            </h4>
+
+            <small
+              >{{ BASE_CURRENCY.name }} {{ BASE_CURRENCY.symbol
+              }}{{ BASE_PRICE.toLocaleString() }}/month</small
+            >
+          </li>
+        </ol>
+      </fieldset>
+
+      <fieldset>
+        <legend><h2>Select you currency</h2></legend>
+
+        <ol class="grid grid-flow-col gap-3 justify-stretch">
+          <li
+            v-for="currency in CURRENCIES"
+            :key="currency"
+            class="cursor-pointer border border-skin-border bg-skin-block-bg text-base md:rounded-xl md:border p-3"
+            :class="{
+              '!border-skin-primary': currency.code === data.currency
+            }"
+            @click="setData('currency', currency.code)"
+          >
+            <div>
+              <b>{{ currency.name }}</b>
+            </div>
+            <small>
+              ~{{
+                computePrice(
+                  fxRates[currency.code],
+                  data.plan.factor
+                ).toLocaleString()
+              }}
+              {{ currency.code }}
+            </small>
+          </li>
+        </ol>
+      </fieldset>
+
+      <TuneCheckbox
+        :id="'tos'"
+        :model-value="data.tos"
+        hint="I agree to the terms and conditions"
+        @update:model-value="setData('tos', $event as boolean)"
+      />
+
+      <TuneButton
+        primary
+        :disabled="!web3Account || !data.tos || loading"
+        :type="'submit'"
+        :loading="loading"
+        @click="pay"
+      >
+        Pay
+        {{ amount.toLocaleString() }}
+        {{ data.currency }} for {{ data.plan.label }}
+      </TuneButton>
+    </form>
   </TheLayout>
 </template>
