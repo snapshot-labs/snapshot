6e5e0efabdd552e3d81b25d7f3d905fe4d4b5426
Sam <samuv.eth@gmail.com>
2023-12-15 15:31:25 +0700

    Add new design

--- src/components/SpaceProposalBoost.vue
+++ src/components/SpaceProposalBoost.vue
@@ -35,38 +35,48 @@ const newBoostLink = computed(() => ({
 </script>
 
 <template>
-  <TuneBlock class="border rounded-xl">
-    <div v-if="SAMPLE_BOOST">
-      <div class="flex justify-between relative">
-        <div>
-          <i-s-boost-logo class="text-xs text-skin-link" />
-          <p class="text-md">Get rewards by voting on this proposal</p>
-        </div>
-        <router-link
-          :to="newBoostLink"
-          class="flex items-center absolute -top-[12px] -right-2 p-2"
-        >
-          <i-ho-plus class="mr-2 text-xs" />
-          <span class="text-md"> New boost </span>
-        </router-link>
-      </div>
-      <div class="mt-3">
-        <SpaceProposalBoostItem :boost="SAMPLE_BOOST" :proposal="proposal" />
-      </div>
+  <TuneBlock slim class="border rounded-xl bg-skin-border p-1 pt-0">
+    <div class="text-skin-heading flex items-center justify-center h-[40px]">
+      <template v-if="[SAMPLE_BOOST].length">
+        <i-ho-fire class="text-xs mr-1" />
+        <span> 1 boost active </span>
+      </template>
+      <template v-else> Try out this feature! </template>
     </div>
-    <div v-else class="py-3">
-      <div
-        class="h-[56px] w-[56px] flex items-center justify-center rounded-[14px] shadow-xl mx-auto"
-      >
-        <i-ho-lightning-bolt class="text-base" />
+    <div class="bg-skin-bg rounded-xl p-3">
+      <div v-if="false">
+        <div class="flex justify-between relative">
+          <div>
+            <i-s-boost-logo class="text-xs text-skin-link" />
+            <p class="text-md">Get rewards by voting on this proposal</p>
+          </div>
+          <router-link
+            :to="newBoostLink"
+            class="flex items-center absolute -top-[12px] -right-2 p-2"
+          >
+            <i-ho-plus class="mr-2 text-xs" />
+            <span class="text-md"> New boost </span>
+          </router-link>
+        </div>
+        <div class="mt-3">
+          <SpaceProposalBoostItem :boost="SAMPLE_BOOST" :proposal="proposal" />
+        </div>
       </div>
-      <div class="mx-auto w-[280px] text-center mt-3">
-        <h4>Boost proposal</h4>
-        <p class="leading-5 mt-[2px]">
-          Incentivize people to vote by rewarding voters with a custom logic.
-        </p>
+      <div v-else class="flex items-center gap-3">
+        <div class="h-[46px] w-[46px] flex-shrink-0 rounded-[14px] shadow-xl">
+          <div class="flex items-center justify-center h-full">
+            <i-s-boost-icon class="text-skin-heading" />
+          </div>
+        </div>
+        <div class="w-full">
+          <h4 class="leading-5">Boost proposal</h4>
+          <p class="">Incentivize people to vote with rewards</p>
+        </div>
         <router-link :to="newBoostLink">
-          <TuneButton class="mt-3 text-skin-text"> Boost proposal </TuneButton>
+          <TuneButton class="flex items-center">
+            <i-ho-fire class="text-sm mr-2" />
+            <div>Boost</div>
+          </TuneButton>
         </router-link>
       </div>
     </div>
