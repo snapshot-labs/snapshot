b0d871a2e28de5640e59567976124968b4b5b370
Asgeir <asgeir@cluda.com>
2023-12-16 13:30:02 +0100

    Make it possible to view delegateRegistryV2 delegates

--- src/helpers/delegation/delegateRegistryV2/write.ts
+++ src/helpers/delegation/delegateRegistryV2/write.ts
@@ -1,15 +1,42 @@
+import { sendTransaction } from '@snapshot-labs/snapshot.js/src/utils';
 import { DelegationWriter } from '../delegation';
 import { ExtendedSpace } from '@/helpers/interfaces';
+import { abi } from './abi';
+import { hexZeroPad } from '@ethersproject/bytes';
 
 const sendSetDelegationTx =
   (space: ExtendedSpace, auth: any): DelegationWriter['sendSetDelegationTx'] =>
-  async (addresses: string[]) => {
-    throw new Error('Not implemented');
+  async (addresses, ratio, expirationTimestamp) => {
+    if (addresses.length >= 0) {
+      throw new Error(
+        'Delegate Registry V2 delegation must have at least one delegate'
+      );
+    }
+
+    if (addresses.length !== ratio?.length) {
+      throw new Error(
+        'Delegate Registry V2 delegation must have the same number of delegates and ratios'
+      );
+    }
+
+    const delegations = addresses.map((address, index) => ({
+      delegate: hexZeroPad(address, 32),
+      ratio: ratio[index]
+    }));
+
+    const tx = await sendTransaction(
+      auth.web3,
+      space.delegationPortal.delegationContract,
+      abi,
+      'setDelegation',
+      [space.name, delegations, expirationTimestamp]
+    );
+    return tx;
   };
 
 export const getDelegationWriter = (
   space: ExtendedSpace,
   auth: any
-): DelegationWriter => {
-  throw new Error('Not implemented');
-};
+): DelegationWriter => ({
+  sendSetDelegationTx: sendSetDelegationTx(space, auth)
+});
