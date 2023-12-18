bac9c5395d7c2ba4d00f655eb2dcbb9015355bbe
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2023-12-18 19:32:03 +0700

    ui: ui fix

--- src/components/ModalPostPayment.vue
+++ src/components/ModalPostPayment.vue
@@ -11,42 +11,40 @@ defineEmits(['close']);
 </script>
 
 <template>
-  <BaseModal :open="open" max-height="550px" @close="$emit('close')">
+  <BaseModal :open="open" @close="$emit('close')">
     <template #header>
       <h3>Transaction submitted</h3>
     </template>
-    <div class="flex w-full flex-col space-y-2 p-4">
-      <BaseBlock slim title="Transaction hash">
-        <template #button>
-          <div class="flex gap-3">
-            <BaseLink
-              v-tippy="{ content: 'View transaction in explorer' }"
-              :link="
-                explorerUrl(tx.network.toString(), tx.hash as string, 'tx')
-              "
-              class="text-skin-text pt-[3px] transition-colors duration-200 hover:text-skin-link"
-              hide-external-icon
-            >
-              <i-ho-external-link />
-            </BaseLink>
-
-            <BaseButtonIcon
-              v-tippy="{ content: 'Copy transaction hash' }"
-              @click="copyToClipboard(tx.hash)"
-            >
-              <i-ho-duplicate />
-            </BaseButtonIcon>
-          </div>
-        </template>
-        <div class="p-3">
-          <b class="my-3 font-mono break-all text-skin-link">{{ tx.hash }}</b>
-        </div>
-      </BaseBlock>
-
-      <p>
+    <div class="mx-4 mb-4">
+      <p class="mt-2 mb-3 text-center">
         Copy the transaction hash above and use on the payment form as proof of
         payment.
       </p>
+      <div class="flex w-full flex-col space-y-2">
+        <BaseBlock slim class="p-3">
+          <b class="font-mono break-all text-skin-link">{{ tx.hash }}</b>
+        </BaseBlock>
+
+        <div class="flex">
+          <BaseLink
+            v-tippy="{ content: 'View transaction in explorer' }"
+            :link="explorerUrl(tx.network.toString(), tx.hash as string, 'tx')"
+            class="text-skin-text"
+          >
+            View in explorer
+          </BaseLink>
+          <div class="flex-grow"></div>
+
+          <button
+            v-tippy="{ content: 'Copy transaction hash' }"
+            class="text-skin-text"
+            @click="copyToClipboard(tx.hash)"
+          >
+            <i-ho-duplicate class="inline-block text-xs" />
+            Copy tx hash
+          </button>
+        </div>
+      </div>
     </div>
   </BaseModal>
 </template>
