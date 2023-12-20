3926e80a7d4499c0a8c7f800817a443a2cbc73f9
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2023-12-20 14:40:55 +0800

    chore: remove unused binding

--- src/views/PaymentView.vue
+++ src/views/PaymentView.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <TheLayout v-bind="$attrs" class="max-w-lg px-4 md:px-0">
+  <TheLayout class="max-w-lg px-4 md:px-0">
     <router-view />
   </TheLayout>
 </template>
