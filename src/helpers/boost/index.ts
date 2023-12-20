e8c22b4b480589a1c6c0a97fcc58d045dfb2881b
Sam <samuv.eth@gmail.com>
2023-12-20 18:38:38 +0700

    Refactor

--- src/helpers/boost/index.ts
+++ src/helpers/boost/index.ts
@@ -1,30 +1,24 @@
 import { Web3Provider } from '@ethersproject/providers';
 import { Contract } from '@ethersproject/contracts';
+import { parseEther } from '@ethersproject/units';
 import { pin } from '@snapshot-labs/pineapple';
 import { BoostStrategy } from '@/helpers/interfaces';
+import ABI from './abi.json';
 
 export const BOOST_CONTRACTS = {
   '1': '0x',
-  '11155111': '0x27b26b007ec742eb8f088ce2ec816db50aee86ae'
+  '11155111': '0x3ef426688D6eF5ca47b4414B9f67E9E582dFc4d3'
 };
 
 export const SUPPORTED_NETWORKS = Object.keys(BOOST_CONTRACTS);
 
-export const ABI = [
-  'function boosts(uint256) view returns (string strategyURI, address token, uint256 balance, address guard, uint256 start, uint256 end, address owner)',
-  'function claimTokens(tuple(uint256 boostId, address recipient, uint256 amount) claim, bytes signature)',
-  'function claimed(address, uint256) view returns (bool)',
-  'function createBoost(tuple(string strategyURI, address token, uint256 balance, address guard, uint256 start, uint256 end, address owner) boost)',
-  'function depositTokens(uint256 boostId, uint256 amount)',
-  'function eip712ClaimStructHash() view returns (bytes32)',
-  'function nextBoostId() view returns (uint256)',
-  'function withdrawRemainingTokens(uint256 boostId, address to)'
-];
+export const SNAPSHOT_GUARD_ADDRESS =
+  '0xF63EB3f569C6cB8F5Cf37caD183790Ed1b251c91';
 
 interface Boost {
   strategyURI: string;
   token: string;
-  balance: string;
+  amount: string;
   guard: string;
   start: number;
   end: number;
@@ -36,10 +30,20 @@ export async function createBoost(
   networkId: string,
   params: Boost
 ): Promise<any> {
+  const { strategyURI, token, amount, guard, start, end, owner } = params;
   const signer = web3.getSigner();
   const contract = new Contract(BOOST_CONTRACTS[networkId], ABI, signer);
-
-  return await contract.createBoost(params);
+  const options = { value: parseEther('0.01') };
+  return await contract.mint(
+    strategyURI,
+    token,
+    amount,
+    owner,
+    guard,
+    start,
+    end,
+    options
+  );
 }
 
 export async function claimTokens(
