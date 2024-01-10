a7a09a39de0f4640e45ac40056b13804975a9e90
Sam <samuv.eth@gmail.com>
2024-01-10 17:45:52 +0700

    Add message for blank approval vote

--- src/components/SpaceProposalVoteApproval.vue
+++ src/components/SpaceProposalVoteApproval.vue
@@ -33,26 +33,34 @@ watch(
 </script>
 
 <template>
-  <div data-testid="approval-choice-list">
-    <template v-for="(choice, i) in proposal.choices" :key="i">
-      <TuneButton
-        v-if="isEditing ? true : userChoice?.includes(i + 1) || !userChoice"
-        class="relative mb-2 last:mb-0 block w-full text-left"
-        :class="{
-          '!border-skin-link': selectedChoices.includes(i + 1),
-          'border-skin-border hover:border-skin-link':
-            !selectedChoices.includes(i + 1),
-          '!cursor-default': !isEditing
-        }"
-        :data-testid="`approval-choice-button-${i}`"
-        @click="isEditing && selectChoice(i + 1)"
-      >
-        <i-ho-check
-          v-if="selectedChoices.includes(i + 1)"
-          class="absolute right-[20px]"
-        />
-        {{ shorten(choice, 32) }}
-      </TuneButton>
-    </template>
+  <div>
+    <div v-if="userChoice?.length === 0 && !isEditing">
+      <BaseMessage level="info">
+        You voted without selecting a choice. Your vote will be counted as a
+        <strong>blank vote</strong>.
+      </BaseMessage>
+    </div>
+    <div data-testid="approval-choice-list">
+      <template v-for="(choice, i) in proposal.choices" :key="i">
+        <TuneButton
+          v-if="isEditing ? true : userChoice?.includes(i + 1) || !userChoice"
+          class="relative mb-2 last:mb-0 block w-full text-left"
+          :class="{
+            '!border-skin-link': selectedChoices.includes(i + 1),
+            'border-skin-border hover:border-skin-link':
+              !selectedChoices.includes(i + 1),
+            '!cursor-default': !isEditing
+          }"
+          :data-testid="`approval-choice-button-${i}`"
+          @click="isEditing && selectChoice(i + 1)"
+        >
+          <i-ho-check
+            v-if="selectedChoices.includes(i + 1)"
+            class="absolute right-[20px]"
+          />
+          {{ shorten(choice, 32) }}
+        </TuneButton>
+      </template>
+    </div>
   </div>
 </template>
