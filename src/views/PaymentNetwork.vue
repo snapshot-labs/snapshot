574c30bd1977cdf4b53bf62e8a61fa499cbf7477
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2024-01-19 01:27:37 +0700

    fix(ui): improve design

--- src/views/PaymentNetwork.vue
+++ src/views/PaymentNetwork.vue
@@ -84,36 +84,49 @@ watch(paymentTx, () => {
 </script>
 
 <template>
-  <h1>Network plan</h1>
-  <p class="text-md mb-4 leading-5">Pay to support your network on Snapshot</p>
+  <h1 class="leading-[44px]">Network plan</h1>
+  <p class="mb-4 leading-[20px]">Pay to support your network on Snapshot</p>
 
   <form v-if="fxLoadStatus === 1" class="flex flex-col gap-4" @submit="pay">
     <fieldset>
       <legend>
-        <h2 class="text-lg mb-2">Select your plan duration</h2>
+        <h2 class="text-[20px] leading-none mb-[12px]">
+          Select your plan duration
+        </h2>
       </legend>
       <ol class="flex flex-col gap-2">
-        <li v-for="(plan, planId) in PLANS" :key="planId">
+        <li
+          v-for="(plan, planId) in PLANS"
+          :key="planId"
+          :class="[{ 'bg-[#384aff]/[0.16] rounded-xl ': plan.discount }]"
+        >
+          <div v-if="plan.discount" class="p-2 text-center text-skin-primary">
+            Save
+            <BasePill class="ml-2 py-1 !bg-skin-primary"
+              >{{ Math.floor(plan.discount) }}%</BasePill
+            >
+            in your package
+          </div>
           <label
-            class="flex gap-3 border border-skin-border hover:border-skin-text rounded-xl p-3 cursor-pointer"
+            class="flex relative bg-skin-bg gap-3 border border-skin-border hover:border-skin-text rounded-xl p-3 cursor-pointer"
             :class="{
               '!border-skin-primary': planId === data.plan
             }"
             @click="setData('plan', planId)"
           >
             <div class="flex-grow">
-              <b class="text-skin-heading">{{ plan.label }}</b>
-              <BasePill v-if="plan.discount" class="ml-2 py-1 !bg-skin-primary"
-                >-{{ Math.floor(plan.discount) }}%</BasePill
-              >
-              <small class="block text-skin-text">
-                {{ formatFiatCurrency(computePlanFiatPrice(plan, BASE_UNIT)) }}
-                per month
-              </small>
+              <div class="leading-none">{{ plan.label }}</div>
+              <b class="text-skin-heading text-lg">{{
+                formatFiatCurrency(computePlanFiatPrice(plan, BASE_UNIT))
+              }}</b
+              >/mo
             </div>
-            <b class="text-skin-heading">
-              {{ formatFiatCurrency(computePlanFiatPrice(plan)) }}
-            </b>
+            <span
+              v-if="planId === data.plan"
+              :class="['absolute right-0 pr-3']"
+            >
+              <i-ho-check class="text-skin-primary text-sm" />
+            </span>
           </label>
         </li>
       </ol>
@@ -121,7 +134,7 @@ watch(paymentTx, () => {
 
     <fieldset>
       <legend>
-        <h2 class="text-lg mb-2">Select your currency</h2>
+        <h2 class="text-[20px] leading-none mb-[12px]">Select your currency</h2>
       </legend>
 
       <ol class="flex gap-2">
@@ -131,51 +144,68 @@ watch(paymentTx, () => {
           class="basis-1/3"
         >
           <label
-            class="flex flex-col sm:flex-row border border-skin-border hover:border-skin-text gap-3 rounded-xl p-3 cursor-pointer"
+            class="flex relative flex-col sm:flex-row border border-skin-border hover:border-skin-text gap-2 rounded-xl p-[12px] pl-3 cursor-pointer"
             :class="{
               '!border-skin-primary': currencyId === data.currency
             }"
             @click="setData('currency', currencyId)"
           >
-            <AvatarToken :address="currency.address[1]" size="30" />
-            <div>
-              <div class="text-skin-heading">
-                {{
-                  computePrice(
-                    currencyId,
-                    PLANS[data.plan].unit,
-                    PLANS[data.plan].discount
-                  ).toLocaleString()
-                }}
-              </div>
-              <small class="leading-none">{{ currency.code }}</small>
-            </div>
+            <AvatarToken :address="currency.address[1]" size="18" />
+
+            <small class="leading-none text-skin-heading text-base">{{
+              currency.code
+            }}</small>
+            <span
+              v-if="currencyId === data.currency"
+              :class="['absolute right-0 pr-3']"
+            >
+              <i-ho-check class="text-skin-primary text-sm" />
+            </span>
           </label>
         </li>
       </ol>
     </fieldset>
 
-    <fieldset class="flex flex-col gap-2">
-      <TuneCheckbox
-        :id="'termsAccepted'"
-        :model-value="data.termsAccepted"
-        hint="By proceeding, I understand that completing the network request form is mandatory after payment."
-        @update:model-value="setData('termsAccepted', $event as boolean)"
-      >
-      </TuneCheckbox>
-    </fieldset>
-
-    <TuneButton
-      primary
-      :disabled="loading || !data.termsAccepted"
-      :type="'submit'"
-      :loading="loading"
-      @click.prevent="pay"
+    <TuneCheckbox
+      :id="'termsAccepted'"
+      :model-value="data.termsAccepted"
+      hint="By proceeding, I understand that completing the network request form is mandatory after payment."
+      @update:model-value="setData('termsAccepted', $event as boolean)"
     >
-      Pay
-      {{ formatCryptoCurrency(amount, CURRENCIES[data.currency]) }} for
-      {{ PLANS[data.plan].label }}
-    </TuneButton>
+    </TuneCheckbox>
+
+    <hr />
+
+    <div class="flex items-center justify-between">
+      <div class="leading-none">
+        Pay now
+        <div class="flex gap-2">
+          <AvatarToken
+            :address="CURRENCIES[data.currency].address[1]"
+            size="36"
+            class="mt-1"
+          />
+          <b class="text-2xl leading-[44px] text-skin-heading"
+            >{{
+              computePrice(
+                data.currency,
+                PLANS[data.plan].unit,
+                PLANS[data.plan].discount
+              ).toLocaleString()
+            }}
+          </b>
+        </div>
+      </div>
+      <TuneButton
+        primary
+        :disabled="loading || !data.termsAccepted"
+        :type="'submit'"
+        :loading="loading"
+        @click.prevent="pay"
+      >
+        Go to payment
+      </TuneButton>
+    </div>
   </form>
 
   <div v-else-if="fxLoadStatus === 0">
