11ecb91e1cbd37937fe064ed4c7d52fb33c63634
Sam <samuv.eth@gmail.com>
2024-03-05 19:43:32 +0700

    Update guard URL

--- src/helpers/boost/api.ts
+++ src/helpers/boost/api.ts
@@ -5,25 +5,24 @@ import {
   BoostSubgraph
 } from '@/helpers/boost/types';
 
+const GUARD_URL = 'https://boost.snapshot.org';
+
 export async function getRewards(
   proposal_id: string,
   voter_address: string,
   boosts: BoostSubgraph[]
 ): Promise<BoostRewardGuard[]> {
-  const results = await fetch(
-    'https://boost-guard-djc2x.ondigitalocean.app/get-rewards',
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        proposal_id,
-        voter_address,
-        boosts: boosts.map(boost => [boost.id, boost.chainId])
-      })
-    }
-  );
+  const results = await fetch(`${GUARD_URL}/get-rewards`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      proposal_id,
+      voter_address,
+      boosts: boosts.map(boost => [boost.id, boost.chainId])
+    })
+  });
 
   if (results.status !== 200) throw new Error('Error fetching rewards');
   return results.json();
@@ -34,20 +33,17 @@ export async function getVouchers(
   voter_address: string,
   boosts: BoostSubgraph[]
 ): Promise<BoostVoucherGuard[]> {
-  const results = await fetch(
-    'https://boost-guard-djc2x.ondigitalocean.app/create-vouchers',
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        proposal_id,
-        voter_address,
-        boosts: boosts.map(boost => [boost.id, boost.chainId])
-      })
-    }
-  );
+  const results = await fetch(`${GUARD_URL}/create-vouchers`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      proposal_id,
+      voter_address,
+      boosts: boosts.map(boost => [boost.id, boost.chainId])
+    })
+  });
 
   if (results.status !== 200) throw new Error('Error fetching vouchers');
   return results.json();
@@ -58,20 +54,17 @@ export async function getWinners(
   boost_id: string,
   chain_id: string
 ): Promise<BoostWinnersGuard> {
-  const results = await fetch(
-    'https://boost-guard-djc2x.ondigitalocean.app/get-lottery-winners',
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        proposal_id,
-        boost_id,
-        chain_id
-      })
-    }
-  );
+  const results = await fetch(`${GUARD_URL}/get-lottery-winners`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      proposal_id,
+      boost_id,
+      chain_id
+    })
+  });
 
   if (results.status !== 200) throw new Error('Error fetching rewards');
   return results.json();
