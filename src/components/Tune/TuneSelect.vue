3c79ee7ac2ac11eab9feac942c16469f79561c18
Sam <samuv.eth@gmail.com>
2024-03-12 18:10:12 +0700

    Use Select instead of Listbox

--- src/components/Tune/TuneSelect.vue
+++ src/components/Tune/TuneSelect.vue
@@ -2,6 +2,9 @@
 type Item = {
   label: string;
   value: string | number;
+  extras: {
+    disabled: boolean;
+  };
 };
 
 defineProps<{
@@ -21,10 +24,15 @@ function handleChange(event: any) {
   <select
     :disabled="disabled"
     :value="modelValue"
-    :class="['tune-select form-select', { disabled: disabled }]"
+    :class="['tune-select', { disabled: disabled }]"
     @change="handleChange($event)"
   >
-    <option v-for="(item, index) in items" :key="index" :value="item.value">
+    <option
+      v-for="(item, index) in items"
+      :key="index"
+      :value="item.value"
+      :disabled="item.extras?.disabled"
+    >
       {{ item.label }}
     </option>
   </select>
