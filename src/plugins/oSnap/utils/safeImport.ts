928fb2283f878be77cde5221e07c7e727f635f8a
Gerhard Steenkamp <gerhard@umaproject.org>
2024-03-20 14:11:26 +0200

    minor bug fix

--- src/plugins/oSnap/utils/safeImport.ts
+++ src/plugins/oSnap/utils/safeImport.ts
@@ -30,11 +30,12 @@ export function isJsonFile(file: File) {
 
 export function getFileFromEvent(event: DragEvent | Event) {
   let _file: File | undefined;
+
   if (event instanceof DragEvent) {
     _file = event.dataTransfer?.files?.[0];
   }
 
-  if (event instanceof Event) {
+  if (event.target && event.target instanceof HTMLInputElement) {
     _file = (event?.currentTarget as HTMLInputElement)?.files?.[0];
   }
   if (!_file) return;
