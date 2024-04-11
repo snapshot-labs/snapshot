d85e979d9f833e1b4a462c7ec2a51cc40bb68d91
Szőke Péter <ps@jpegs.ro>
2024-04-11 05:50:27 +0200

    Add Domino plugin and components (#4665)
    Co-authored-by: Chaitanya <yourchaitu@gmail.com>

---
+++ src/plugins/domino/ProposalSidebar.vue
@@ -0,0 +1,11 @@
+<script setup>
+import DominoCustomBlock from './components/CustomBlock.vue';
+
+defineProps({
+  space: Object
+});
+</script>
+
+<template>
+  <DominoCustomBlock v-if="space.plugins.domino" :space="space" />
+</template>
