e3e0e38e4f89cebe5924113f801eaf59eb78ad0d
Chaitanya <yourchaitu@gmail.com>
2024-06-03 06:48:33 +0530

    Update src/helpers/connectors.ts

--- src/helpers/connectors.ts
+++ src/helpers/connectors.ts
@@ -11,7 +11,7 @@ const connectors = {
       projectId: 'e6454bd61aba40b786e866a69bd4c5c6',
       chains: [],
       optionalChains: [
-        4, 5, 42, 56, 100, 137, 246, 1088, 42161, 73799, 11155111
+        1, 4, 5, 10, 42, 56, 100, 137, 246, 1088, 42161, 73799, 11155111
       ],
       methods: ['eth_sendTransaction', 'personal_sign', 'eth_signTypedData_v4'],
       optionalMethods: ['eth_accounts'],
