789ae2ec76dd8713cd5a5b79a631651235027617
Wan <495709+wa0x6e@users.noreply.github.com>
2024-09-13 10:25:35 +0900

    fix: fix statement not saving (#4872)

--- src/composables/useStatement.ts
+++ src/composables/useStatement.ts
@@ -21,6 +21,9 @@ export function useStatement() {
     statement: {
       about: string;
       statement: string;
+      discourse: string;
+      network: string;
+      status: string;
     }
   ) {
     const result = await send({ id: spaceId }, SET_STATEMENT_ACTION, statement);
@@ -78,8 +81,20 @@ export function useStatement() {
     await loadStatements(spaceId, [id]);
   }
 
-  function getStatement(id: string): { about: string; statement: string } {
-    const defaultStatement = { about: '', statement: '' };
+  function getStatement(id: string): {
+    about: string;
+    statement: string;
+    network: string;
+    status: string;
+    discourse: string;
+  } {
+    const defaultStatement = {
+      about: '',
+      statement: '',
+      network: '',
+      status: 's',
+      discourse: ''
+    };
     return clone(statements.value?.[id?.toLowerCase()] || defaultStatement);
   }
 
